d1d9c00602c7e58d5f25ee3bc56cb2921e99e7ad
rross <rross>
2001-03-06 00:02:41 +0000

    Added return value and prototype.

--- src/mpi/romio/adio/common/status_setb.c
+++ src/mpi/romio/adio/common/status_setb.c
@@ -10,16 +10,14 @@
 #include "mpi.h"
 
 void MPID_Status_set_bytes(MPI_Status *status, int nbytes);
+int MPIR_Status_set_bytes(MPI_Status *status, MPI_Datatype datatype, 
+			  int nbytes);
 
 int MPIR_Status_set_bytes(MPI_Status *status, MPI_Datatype datatype, 
 			  int nbytes)
 {
     MPID_Status_set_bytes(status, nbytes);
-#if 0
-    /* MPID_Status_set_bytes() should do this in the default case */
-    status->count = nbytes;
     return MPI_SUCCESS;
-#endif
 }
 
 #endif
