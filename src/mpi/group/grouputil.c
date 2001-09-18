b953cc69fbafc8ccb150bfd1948bcc04bb6d2d8d
ashton <ashton>
2001-09-18 20:19:31 +0000

    added xxxutil.c files for Groups, Files and Windows

---
+++ src/mpi/group/grouputil.c
@@ -0,0 +1,18 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include "mpiimpl.h"
+
+#ifndef MPID_GROUP_PREALLOC 
+#define MPID_GROUP_PREALLOC 8
+#endif
+
+/* Preallocated group objects */
+MPID_Group MPID_Group_direct[MPID_GROUP_PREALLOC];
+MPIU_Object_alloc_t MPID_Group_mem = { 0, 0, 0, 0, MPID_GROUP, 
+				      sizeof(MPID_Group), MPID_Group_direct,
+                                      MPID_GROUP_PREALLOC, };
+
