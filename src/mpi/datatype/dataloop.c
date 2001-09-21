61e1fd30afb07ed15dbdbd6b57c2b97f1e71f702
gropp <gropp>
2001-09-21 14:29:42 +0000

    Added restrict, removed unnecessary assignments from dataloop code

--- src/mpi/datatype/dataloop.c
+++ src/mpi/datatype/dataloop.c
@@ -9,8 +9,9 @@
   Simple pack routine, using a dataloop description.
  */
 
+#ifdef FOO
 #define VEC_COPY(src,dest,stride,type,nelms,count) \
-{ type *src_t = (type *)src, *dest_t = (type *)dest; int i, j;\
+{ type *restrict src_t = (type *)src, * restrict dest_t = (type *)dest; int i, j;\
   const int l_stride = stride;\
   if (nelms == 1) {\
       for (i=count;i!=0;i--) {\
@@ -20,28 +21,39 @@
           for (j=0; j<nelms; j++) {\
               *dest_t++ = src_t[j];} src_t += l_stride;}}\
   dest=(char *)dest_t;}
-
+#else
+#define VEC_COPY(src,dest,stride,type,nelms,count) \
+{ type *restrict src_t = (type *)src, * restrict dest_t = (type *)dest; int i, j;\
+  if (nelms == 1) {\
+      for (i=count;i!=0;i--) {\
+          *dest_t++ = *src_t; src_t += stride;}}\
+  else {\
+      for (i=count; i!=0;i--) {\
+          for (j=0; j<nelms; j++) {\
+              *dest_t++ = src_t[j];} src_t += stride;}}\
+  dest=(char *)dest_t;}
+#endif
 /* This version should be used when the entire loop fits into the destination
    buffer.  This minimizes the amount of extra processing 
 */
 void MPID_Segment_pack( MPID_Dataloop *loopinfo, 
-			char *src_buf, char *dest_buf )
+			char * restrict src_buf, char * restrict dest_buf )
 {
     int cur_sp = 0, valid_sp = 0;
-    MPID_Dataloop_stackelm stackelm[16], *curstackelm;
+    MPID_Dataloop_stackelm stackelm[16], * restrict curstackelm;
     int kind;
 
     curstackelm = &stackelm[0];
     curstackelm->loopinfo = *loopinfo;
     curstackelm->curcount = 0;
 
-    while (cur_sp >= 0) {
+    do {
 	kind = curstackelm->loopinfo.kind;
 	if (kind & DATALOOP_FINAL_MASK) {
 	    /* This is a simple datatype, such as a vector of 
 	       simple elements or a contiguous block loop */
 	    int      i, nbytes, stride, count, elmsize;
-	    char     *sbuf; /* temporary for loops that update src buf */
+	    char     *restrict sbuf; /* temporary for loops that update src buf */
 	    MPI_Aint *offset_array;
 	    int      *nbytes_array;
 	    switch (kind & DATALOOP_KIND_MASK) {
@@ -55,14 +67,14 @@ void MPID_Segment_pack( MPID_Dataloop *loopinfo,
 		count     = curstackelm->loopinfo.loop_params.v_t.count;
 		nbytes    = curstackelm->loopinfo.loop_params.v_t.blocksize;
 		stride    = curstackelm->loopinfo.loop_params.v_t.stride;
-		sbuf      = src_buf;
 		if (elmsize == 4) {
-		    VEC_COPY(sbuf,dest_buf,stride,int32_t,nbytes,count);
+		    VEC_COPY(src_buf,dest_buf,stride,int32_t,nbytes,count);
 		}
 		else if (elmsize == 2) {
-		    VEC_COPY(sbuf,dest_buf,stride,int16_t,nbytes,count);
+		    VEC_COPY(src_buf,dest_buf,stride,int16_t,nbytes,count);
 		}
 		else {
+		    sbuf      = src_buf;
 		    for (i=0; i<count; i++) {
 			memcpy( dest_buf, sbuf, nbytes );
 			dest_buf += nbytes;
@@ -127,5 +139,5 @@ void MPID_Segment_pack( MPID_Dataloop *loopinfo,
 	    curstackelm++;
 	    curstackelm->curcount = 0;
 	}
-    }
+    } while (cur_sp >= 0);
 }
