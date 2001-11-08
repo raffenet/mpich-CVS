38cc87060cb46cc46104188417dece9b19940edd
toonen <toonen>
2001-11-08 22:32:25 +0000

    Updated to print the rank and size of MPI_COMM_WORLD

--- examples/hellow.c
+++ examples/hellow.c
@@ -3,8 +3,13 @@
 
 int main( int argc, char *argv[] )
 {
+    int rank;
+    int size;
+    
     MPI_Init( 0, 0 );
-    printf( "Hello world\n" );
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf( "Hello world from process %d of %d\n", rank, size );
     MPI_Finalize();
     return 0;
 }
