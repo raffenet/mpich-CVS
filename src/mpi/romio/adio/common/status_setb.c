78c9f5d31563f7293e62604fdf9f8ef59eb1fcac
ashton <ashton>
2001-11-14 19:40:39 +0000

    added NTFS option

--- src/mpi/romio/adio/common/status_setb.c
+++ src/mpi/romio/adio/common/status_setb.c
@@ -16,7 +16,12 @@ int MPIR_Status_set_bytes(MPI_Status *status, MPI_Datatype datatype,
 int MPIR_Status_set_bytes(MPI_Status *status, MPI_Datatype datatype, 
 			  int nbytes)
 {
+#ifdef NTFS
+    status->count = nbytes;
+    return MPI_SUCCESS;
+#else
     MPID_Status_set_bytes(status, nbytes);
+#endif
     return MPI_SUCCESS;
 }
 
