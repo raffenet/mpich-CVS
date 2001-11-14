ec997f1854e1f470702bba7263de77ec2bfb5828
ashton <ashton>
2001-11-14 19:22:54 +0000

    removed tabs

--- src/mpi/romio/adio/ad_ntfs/ad_ntfs.h
+++ src/mpi/romio/adio/ad_ntfs/ad_ntfs.h
@@ -1,26 +1,25 @@
 /* 
- *   $Id$    
+ *
  *
  *   Copyright (C) 1997 University of Chicago. 
  *   See COPYRIGHT notice in top-level directory.
  */
 
-#ifndef AD_UNIX_INCLUDE
-#define AD_UNIX_INCLUDE
+#ifndef AD_NTFS_INCLUDE
+#define AD_NTFS_INCLUDE
 
-//#include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include "adio.h"
 
 #ifdef HAVE_INT64
-#define DWORDLOW(x)			( (DWORD) ( x & (__int64) 0xFFFFFFFF ) )
-#define DWORDHIGH(x)		( (DWORD) ( (x >> 32) & (__int64) 0xFFFFFFFF ) )
-#define DWORDTOINT64(x,y)	( (__int64) ( ( (__int64 x) << 32 ) + (__int64) y ) )
+#define DWORDLOW(x)        ( (DWORD) ( x & (__int64) 0xFFFFFFFF ) )
+#define DWORDHIGH(x)       ( (DWORD) ( (x >> 32) & (__int64) 0xFFFFFFFF ) )
+#define DWORDTOINT64(x,y)  ( (__int64) ( ( (__int64 x) << 32 ) + (__int64) y ) )
 #else
-#define DWORDLOW(x)			x
-#define DWORDHIGH(x)		0
-#define DWORDTOINT64(x,y)	x
+#define DWORDLOW(x)         x
+#define DWORDHIGH(x)        0
+#define DWORDTOINT64(x,y)   x
 #endif
 
 int ADIOI_NTFS_aio(ADIO_File fd, void *buf, int len, ADIO_Offset offset,
