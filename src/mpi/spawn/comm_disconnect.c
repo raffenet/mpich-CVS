e2f8f2f8ba5b955cb2ea41bb0fdd19cbcc5e1773
ashton <ashton>
2002-01-10 20:59:57 +0000

    updated error checking code

--- src/mpi/spawn/comm_disconnect.c
+++ src/mpi/spawn/comm_disconnect.c
@@ -45,10 +45,9 @@ int MPI_Comm_disconnect(MPI_Comm *comm)
     static const char FCNAME[] = "MPI_Comm_disconnect";
     int mpi_errno = MPI_SUCCESS;
     MPID_Comm *comm_ptr = NULL;
+    MPID_MPI_STATE_DECLS;
 
-    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_DISCONNECT);
-    /* Get handles to MPI objects. */
-    MPID_Comm_get_ptr( *comm, comm_ptr );
+    /* Verify that MPI has been initialized */
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
@@ -56,7 +55,21 @@ int MPI_Comm_disconnect(MPI_Comm *comm)
             if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
                 mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
                             "**initialized", 0 );
+                return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
             }
+	}
+        MPID_END_ERROR_CHECKS;
+    }
+#   endif /* HAVE_ERROR_CHECKING */
+
+    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_DISCONNECT);
+
+    /* Get handles to MPI objects. */
+    MPID_Comm_get_ptr( *comm, comm_ptr );
+#   ifdef HAVE_ERROR_CHECKING
+    {
+        MPID_BEGIN_ERROR_CHECKS;
+        {
             /* Validate comm_ptr */
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
 	    /* If comm_ptr is not value, it will be reset to null */
@@ -69,8 +82,14 @@ int MPI_Comm_disconnect(MPI_Comm *comm)
     }
 #   endif /* HAVE_ERROR_CHECKING */
 
-    MPID_Comm_disconnect(comm_ptr);
+    mpi_errno = MPID_Comm_disconnect(comm_ptr);
+
+    if (mpi_errno == MPI_SUCCESS)
+    {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_DISCONNECT);
+	return MPI_SUCCESS;
+    }
 
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_DISCONNECT);
-    return MPI_SUCCESS;
+    return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
 }
--- src/mpi/spawn/comm_spawn.c
+++ src/mpi/spawn/comm_spawn.c
@@ -54,10 +54,9 @@ int MPI_Comm_spawn(char *command, char *argv[], int maxprocs, MPI_Info info, int
     static const char FCNAME[] = "MPI_Comm_spawn";
     int mpi_errno = MPI_SUCCESS;
     MPID_Comm *comm_ptr = NULL;
+    MPID_MPI_STATE_DECLS;
 
-    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_SPAWN);
-    /* Get handles to MPI objects. */
-    MPID_Comm_get_ptr( comm, comm_ptr );
+    /* Verify that MPI has been initialized */
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
@@ -65,7 +64,21 @@ int MPI_Comm_spawn(char *command, char *argv[], int maxprocs, MPI_Info info, int
             if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
                 mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
                             "**initialized", 0 );
+                return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
             }
+	}
+        MPID_END_ERROR_CHECKS;
+    }
+#   endif /* HAVE_ERROR_CHECKING */
+
+    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_SPAWN);
+
+    /* Get handles to MPI objects. */
+    MPID_Comm_get_ptr( comm, comm_ptr );
+#   ifdef HAVE_ERROR_CHECKING
+    {
+        MPID_BEGIN_ERROR_CHECKS;
+        {
             /* Validate comm_ptr */
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
 	    /* If comm_ptr is not value, it will be reset to null */
@@ -83,6 +96,12 @@ int MPI_Comm_spawn(char *command, char *argv[], int maxprocs, MPI_Info info, int
     MPID_Comm_thread_unlock( comm_ptr );
     */
 
+    if (mpi_errno == MPI_SUCCESS)
+    {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_SPAWN);
+	return MPI_SUCCESS;
+    }
+
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_SPAWN);
-    return MPI_SUCCESS;
+    return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
 }
