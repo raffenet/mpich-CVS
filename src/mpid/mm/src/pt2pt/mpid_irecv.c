dbe2b69bdf7b0629a59dfe6181432f97140e37a3
ashton <ashton>
2002-01-10 21:08:06 +0000

    added xfer implementation of irecv

--- src/mpid/mm/src/pt2pt/mpid_irecv.c
+++ src/mpid/mm/src/pt2pt/mpid_irecv.c
@@ -20,6 +20,7 @@
 .  int source - source
 .  int tag - tag
 .  MPID_Comm *comm_ptr - communicator
+.  int mode - communicator mode
 -  MPID_Request **request_pptr - request
 
    Notes:
@@ -29,11 +30,15 @@
 .N Errors
 .N MPI_SUCCESS
 @*/
-int MPID_Irecv(void *buf, int count, MPID_Datatype *datatype_ptr, int source, int tag, MPID_Comm *comm_ptr, MPID_Request **request_pptr)
+int MPID_Irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPID_Comm *comm_ptr, int mode, MPID_Request **request_pptr)
 {
     static const char FCNAME[] = "MPID_Irecv";
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPID_IRECV);
+
+    xfer_scatter_init(source, tag, comm_ptr, request_pptr);
+    xfer_scatter_recv_op(*request_pptr, buf, count, datatype, 0, -1);
+    xfer_scatter_start(*request_pptr);
 
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPID_IRECV);
     return MPI_SUCCESS;
