4b3bad5a5f76d93fec7a45f5bd608da06f78b99b
ashton <ashton>
2002-01-10 21:01:26 +0000

    added xfer function prototypes

--- src/mpid/mm/include/mpidimpl.h
+++ src/mpid/mm/include/mpidimpl.h
@@ -34,4 +34,58 @@ int MM_Send(int, char *, int);
 int MM_Recv(int, char *, int);
 int MM_Close(int);
 
+/*
+What is an xfer block? - A block is defined by an init call, followed by one or more
+xfer operations, and finally a start call.
+Priority in a block is defined by the order of operations issued in a block.
+Lower priority operations may make progress as long as they can be preempted
+by higher priority operations.
+
+Progress on blocks may occur in parallel.  Blocks are independent.
+
+dependencies:
+1) completion
+2) progress
+3) resource (buffer)
+
+Xfer blocks are used to create graphs of cars. Multiple cars within an xfer block 
+may be aggregated in the order issued.
+
+mop = MPI operation
+Do not add xfer_gather/scatter_mop_send_op because it is not necessary at the 
+xfer level.
+
+The xfer...mop functions generate cars like this:
+(recv_mop) - (send)
+(recv) - (mop_send)
+
+The mop_send exists at the car level, not the xfer level.
+
+xfer_gather/scatter_recv_forward_op generates these cars:
+(recv) - (send)
+
+recv_mop can be used for accumulate
+
+send_mop could cause remote operations to occur.  We will not use send_mop 
+currently.
+*/
+int xfer_gather_init(int dest, int tag, MPID_Comm *comm_ptr, MPID_Request **request_pptr);
+int xfer_gather_recv_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int src);
+int xfer_gather_recv_mop_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int src);
+int xfer_gather_recv_forward_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int src);
+int xfer_gather_recv_mop_forward_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int src);
+int xfer_gather_forward_op(MPID_Request *request_ptr, int size);
+int xfer_gather_send_op(MPID_Request *request_ptr, const void *buf, int count, MPI_Datatype dtype, int first, int last);
+int xfer_gather_start(MPID_Request *request_ptr);
+
+int xfer_scatter_init(int src, int tag, MPID_Comm *comm_ptr, MPID_Request **request_pptr);
+int xfer_scatter_recv_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last);
+int xfer_scatter_recv_mop_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last);
+int xfer_scatter_recv_forward_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int dest);
+int xfer_scatter_recv_mop_forward_op(MPID_Request *request_ptr, void *buf, int count, MPI_Datatype dtype, int first, int last, int dest);
+int xfer_scatter_forward_op(MPID_Request *request_ptr, int size, int dest);
+int xfer_scatter_send_op(MPID_Request *request_ptr, const void *buf, int count, MPI_Datatype dtype, int first, int last, int dest);
+int xfer_scatter_replicate_op(MPID_Request *request_ptr, int dest);
+int xfer_scatter_start(MPID_Request *request_ptr);
+
 #endif
