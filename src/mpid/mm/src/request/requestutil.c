352f1c064b038b90e16bc19ad024b7df09b7520b
ashton <ashton>
2002-01-16 16:07:01 +0000

    added allocation code

--- src/mpid/mm/src/request/requestutil.c
+++ src/mpid/mm/src/request/requestutil.c
@@ -15,11 +15,25 @@ MPIU_Object_alloc_t MPID_Request_mem = { 0, 0, 0, 0, 0,
 					 sizeof(MPID_Request), MPID_Request_direct,
 					 MPID_REQUEST_PREALLOC, };
 
-MPID_Request * MPID_Request_new()
+MPID_Request * mm_request_alloc()
 {
-    return NULL;
+    MPID_Request *p;
+    p = MPIU_Handle_obj_alloc(&MPID_Request_mem);
+    if (p == NULL)
+	return p;
+    p->op_valid = 0;
+    p->busy = 0;
+    p->write_list = NULL;
+    p->read_list = NULL;
+    return p;
+}
+
+void mm_request_free(MPID_Request *request_ptr)
+{
+    MPIU_Handle_obj_free(&MPID_Request_mem, request_ptr);
 }
 
 void MPID_Request_free(MPID_Request *request_ptr)
 {
+    mm_request_free(request_ptr);
 }
