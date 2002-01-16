41b4bb76ed1bf75564d2efea2db20dfab07aae07
ashton <ashton>
2002-01-16 16:05:16 +0000

    added mm device stuff to request structure

--- src/mpid/mm/include/mpidpre.h
+++ src/mpid/mm/include/mpidpre.h
@@ -10,4 +10,27 @@ typedef struct MPID_VC
     struct MPID_Request * recv_active;
 } MPID_VC;
 
+typedef struct MM_Car
+{
+    struct MPID_Request *request_ptr;
+    int dest, src;
+    struct MM_Car *next_ptr;
+} MM_Car;
+
+#define MPID_DEV_REQUEST_DECL \
+    int src, dest, tag; \
+    MPID_Comm *comm_ptr; \
+    const void *sbuf; \
+    void *rbuf; \
+    int count; \
+    MPI_Datatype dtype; \
+    int first; \
+    int last; \
+    MM_Car *write_list; \
+    MM_Car *read_list; \
+    MM_Car wcar; \
+    MM_Car rcar; \
+    int op_valid; \
+    struct MPID_Request *next_ptr;
+
 #endif
