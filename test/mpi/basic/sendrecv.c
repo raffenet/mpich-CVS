32515edd28f0e44c1076e1f208cc8c995744308a
ashton <ashton>
2002-03-16 22:52:42 +0000

    added wtime and netpipe to test/mpi/basic

--- test/mpi/basic/sendrecv.c
+++ test/mpi/basic/sendrecv.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     /*MPI_Request request;*/
     MPI_Status status;
     int tag = 1;
-    
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
     if (rank == 0)
     {
-	printf("Rank 0: sending message to process 1.\n");
+      printf("Rank 0: sending message to process 1.\n");fflush(stdout);
 	strcpy(buffer, "Hello process one.");
 	/*MPI_Isend(buffer, 100, MPI_BYTE, 1, tag, MPI_COMM_WORLD, &request);
 	MPI_Wait(&request, &status);*/
@@ -31,15 +31,15 @@ int main(int argc, char *argv[])
     }
     else if (rank == 1)
     {
-	printf("Rank 1: receiving message from process 0.\n");
+      printf("Rank 1: receiving message from process 0.\n");fflush(stdout);
 	/*MPI_Irecv(buffer, 100, MPI_BYTE, 0, tag, MPI_COMM_WORLD, &request);
 	MPI_Wait(&request, &status);*/
 	MPI_Recv(buffer, 100, MPI_BYTE, 0, tag, MPI_COMM_WORLD, &status);
-	printf("Rank 1: received message '%s'\n", buffer);
+	printf("Rank 1: received message '%s'\n", buffer);fflush(stdout);
     }
     else
     {
-	printf("Rank %d, I am not participating.\n", rank);
+      printf("Rank %d, I am not participating.\n", rank);fflush(stdout);
     }
 
     MPI_Finalize();
