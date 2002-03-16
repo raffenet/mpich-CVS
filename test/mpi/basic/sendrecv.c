aba6d454a0e42fa2608dd8676a64af652c172d8b
ashton <ashton>
2002-03-16 21:00:57 +0000

    *** empty log message ***

--- test/mpi/basic/sendrecv.c
+++ test/mpi/basic/sendrecv.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     int size, rank;
     char buffer[100] = "garbage";
-    MPI_Request request;
+    /*MPI_Request request;*/
     MPI_Status status;
     int tag = 1;
     
@@ -25,14 +25,16 @@ int main(int argc, char *argv[])
     {
 	printf("Rank 0: sending message to process 1.\n");
 	strcpy(buffer, "Hello process one.");
-	MPI_Isend(buffer, 100, MPI_BYTE, 1, tag, MPI_COMM_WORLD, &request);
-	MPI_Wait(&request, &status);
+	/*MPI_Isend(buffer, 100, MPI_BYTE, 1, tag, MPI_COMM_WORLD, &request);
+	MPI_Wait(&request, &status);*/
+	MPI_Send(buffer, 100, MPI_BYTE, 1, tag, MPI_COMM_WORLD);
     }
     else if (rank == 1)
     {
 	printf("Rank 1: receiving message from process 0.\n");
-	MPI_Irecv(buffer, 100, MPI_BYTE, 0, tag, MPI_COMM_WORLD, &request);
-	MPI_Wait(&request, &status);
+	/*MPI_Irecv(buffer, 100, MPI_BYTE, 0, tag, MPI_COMM_WORLD, &request);
+	MPI_Wait(&request, &status);*/
+	MPI_Recv(buffer, 100, MPI_BYTE, 0, tag, MPI_COMM_WORLD, &status);
 	printf("Rank 1: received message '%s'\n", buffer);
     }
     else
