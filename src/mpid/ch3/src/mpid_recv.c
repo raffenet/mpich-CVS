d0e9ec660cedf85df048a7941d59988523d3ae11
toonen <toonen>
2002-03-27 17:29:28 +0000

    added handling on non-zero length messages to MPID_Recv()

--- src/mpid/ch3/src/mpid_recv.c
+++ src/mpid/ch3/src/mpid_recv.c
@@ -14,96 +14,99 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype,
 	      int rank, int tag, MPID_Comm * comm, int context_offset,
 	      MPI_Status * status, MPID_Request ** request)
 {
-    MPID_Request * req;
+    MPID_Request * rreq;
     int found;
 
     MPIDI_dbg_printf(10, FCNAME, "entering");
     MPIDI_dbg_printf(15, FCNAME, "rank=%d, tag=%d, context=%d", rank, tag,
 		     comm->context_id + context_offset);
-    req = MPIDI_CH3U_Request_FUOAP(
+    rreq = MPIDI_CH3U_Request_FUOAP(
 	rank, tag, comm->context_id + context_offset, &found);
-    assert(req != NULL);
+    assert(rreq != NULL);
 
     if (found)
     {
 	/* Message was found in the unexepected queue */
 	MPIDI_dbg_printf(15, FCNAME, "request found in unexpected queue");
 
-#if 0	/* only zero length messages supported */
-	
 	/* XXX - this check needs to be thread safe; it may be already assuming
-           req->cc is declared volatile */
-	/* NOTE - req->cc is used here instead of req->cc_ptr.  We are assuming
-	   that for simple sends and receives the request's internal completion
-	   counter will always be used. */
-	if (req->cc == 0)
+           rreq->cc is declared volatile */
+	/* NOTE - rreq->cc is used here instead of rreq->cc_ptr.  We are
+	   assuming that for simple sends and receives the request's internal
+	   completion counter will always be used. */
+	if (MPIDI_Request_get_msg_type(rreq) == MPIDI_REQUEST_EAGER_MSG)
 	{
-	    /* The entire message has arrived */
-
-	    if ((req->mpid_state & MPID_REQUEST_STATE_MSG_MASK) <=
-		MPID_REQUEST_STATE_EAGER_MSG)
+	    /* This is an eager send message.  Assuming all of the data has
+	       arrived, we need to copy the data and then free the buffer and
+	       the request. */
+		
+	    MPIDI_dbg_printf(15, FCNAME, "eager message in the request");
+	    
+	    if (rreq->cc == 0)
 	    {
-		/* This is an eager send message.  We need to copy the data and
-                   then free the buffer and the request. */
-		
-		long dt_sz;
-		int dt_contig;
-    
-		if (HANDLE_GET_KIND(datatype) == HANDLE_KIND_BUILTIN)
+		if (count > 0)
 		{
-		    dt_sz = MPID_Datatype_get_size(datatype);
-		    dt_contig = TRUE;
-		}
-		else
-		{
-		    assert(HANDLE_GET_KIND(datatype) == HANDLE_KIND_BUILTIN);
-		    abort();
+		    rreq->ch3.user_buf = buf;
+		    rreq->ch3.user_count = count;
+		    rreq->ch3.datatype = datatype;
+		    
+		    MPIDI_CH3U_Request_copy_tmp_data(rreq);
 		}
 		
-		if (dt_contig)
-		{
-		    memcpy(buf, req->ch3.recv_buf, req->ch3.recv_sz);
-		}
-		else
-		{
-		    assert(dt_contig);
-		    abort();
-		}
-		
-		*status = req->status;
-		MPIU_Free(req->ch3.recv_buf);
-		MPIDI_CH3_Request_free(req);
+		*status = rreq->status;
+		MPIU_Free(rreq->ch3.tmp_buf);
+		MPID_Request_free(rreq);
 		
 		return status->MPI_ERROR;
 	    }
 	    else
 	    {
-		/* A rendezvous request-to-send (RTS) message has arrived.  We
-		   need to repopulate the reequest with the information
-		   supplied in the arguments and then send a clear-to-send
-		   message to the remote process. */
-		req->ch3.user_buf = buf;
-		req->ch3.user_count = count;
-		req->ch3.datatype = datatype;
-		req->ch3.vc = comm->vcr[rank];
-
-		/* TODO - send Rndv RTS */
-		
-		*request = req;
+		/* The data is still being transfered across the net.  We'll
+		   leave it to the progress engine to handle once the entire
+		   message has arrived. */
+		rreq->ch3.user_buf = buf;
+		rreq->ch3.user_count = count;
+		rreq->ch3.datatype = datatype;
+		rreq->ch3.vc = comm->vcr[rank];
+		rreq->ch3.ca = MPIDI_CH3_CA_COPY_COMPLETE;
+		*request = rreq;
 	    }
 	}
 	else
 	{
-	    /* The data is still being transfered across the net.  We'll leave
-               it to the progress engine to handle once the entire message has
-               arrived. */
-	    req->ch3.user_buf = buf;
-	    req->ch3.user_count = count;
-	    req->ch3.datatype = datatype;
-	    req->ch3.vc = comm->vcr[rank];
-	    *request = req;
+	    MPID_Request * cts_req;
+	    MPIDI_CH3_Pkt_t upkt;
+	    MPIDI_CH3_Pkt_rndv_clr_to_send_t * cts_pkt =
+		&upkt.rndv_clr_to_send;
+		
+	    MPIDI_dbg_printf(15, FCNAME, "rndv RTS in the request, "
+			     "sending rndv CTS");
+	    
+	    /* A rendezvous request-to-send (RTS) message has arrived.  We need
+	       to repopulate the reequest with the information supplied in the
+	       arguments and then send a clear-to-send message to the remote
+	       process. */
+	    rreq->ch3.user_buf = buf;
+	    rreq->ch3.user_count = count;
+	    rreq->ch3.datatype = datatype;
+	    rreq->ch3.vc = comm->vcr[rank];
+
+	    cts_pkt->type = MPIDI_CH3_PKT_RNDV_CLR_TO_SEND;
+	    cts_pkt->req_id_sender = rreq->ch3.rndv_req_id;
+	    cts_pkt->req_id_receiver = rreq->handle;
+	    cts_req = MPIDI_CH3_iStartMsg(rreq->ch3.vc,
+					  cts_pkt, sizeof(*cts_pkt));
+	    if (cts_req != NULL)
+	    {
+		/* XXX: Ideally we could specify that a req not be returned.
+		   This would avoid our having to decrement the reference count
+		   on a req we don't want/need. */
+		MPID_Request_free(cts_req);
+	    }
+	    /* TODO - send Rndv CTS */
+		
+	    *request = rreq;
 	}
-#endif	
     }
     else
     {
@@ -111,12 +114,12 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype,
            of posted receive requests.  The request still needs to be populated
            with information supplied in the arguments. */
 	MPIDI_dbg_printf(15, FCNAME, "request allocated in posted queue");
-	req->comm = comm;
-	req->ch3.user_buf = buf;
-	req->ch3.user_count = count;
-	req->ch3.datatype = datatype;
-	req->ch3.vc = comm->vcr[rank];
-	*request = req;
+	rreq->comm = comm;
+	rreq->ch3.user_buf = buf;
+	rreq->ch3.user_count = count;
+	rreq->ch3.datatype = datatype;
+	rreq->ch3.vc = comm->vcr[rank];
+	*request = rreq;
 	/* XXX - thread safety? message could arrive while populating req */
     }
     
