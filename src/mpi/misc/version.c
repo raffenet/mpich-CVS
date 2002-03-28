0bd765e069ef6222489947838ae6c8c7ba95662e
gropp <gropp>
2002-03-28 20:49:09 +0000

    Add MPI_Get_version

--- src/mpi/misc/version.c
+++ src/mpi/misc/version.c
@@ -7,32 +7,32 @@
 
 #include "mpiimpl.h"
 
-/* -- Begin Profiling Symbol Block for routine MPI_Pcontrol */
+/* -- Begin Profiling Symbol Block for routine MPI_Get_version */
 #if defined(HAVE_PRAGMA_WEAK)
-#pragma weak MPI_Pcontrol = PMPI_Pcontrol
+#pragma weak MPI_Get_version = PMPI_Get_version
 #elif defined(HAVE_PRAGMA_HP_SEC_DEF)
-#pragma _HP_SECONDARY_DEF PMPI_Pcontrol  MPI_Pcontrol
+#pragma _HP_SECONDARY_DEF PMPI_Get_version  MPI_Get_version
 #elif defined(HAVE_PRAGMA_CRI_DUP)
-#pragma _CRI duplicate MPI_Pcontrol as PMPI_Pcontrol
+#pragma _CRI duplicate MPI_Get_version as PMPI_Get_version
 #endif
 /* -- End Profiling Symbol Block */
 
 /* Define MPICH_MPI_FROM_PMPI if weak symbols are not supported to build
    the MPI routines */
 #ifndef MPICH_MPI_FROM_PMPI
-#define MPI_Pcontrol PMPI_Pcontrol
+#define MPI_Get_version PMPI_Get_version
 
 #endif
 
 #undef FUNCNAME
-#define FUNCNAME MPI_Pcontrol
+#define FUNCNAME MPI_Get_version
 
 /*@
-   MPI_Pcontrol - profiling control
+   MPI_Get_version - Return the version number of MPI
 
-   Arguments:
-+  const int level - level
--  ... - other arguments
+   Output Parameters:
++  version - Version of MPI
+-  subversion - Subversion of MPI
 
    Notes:
 
@@ -41,20 +41,20 @@
 .N Errors
 .N MPI_SUCCESS
 @*/
-int MPI_Pcontrol(const int level, ...)
+int MPI_Get_version( int *version, int *subversion )
 {
-    static const char FCNAME[] = "MPI_Pcontrol";
+    static const char FCNAME[] = "MPI_Get_version";
     int mpi_errno = MPI_SUCCESS;
     MPID_MPI_STATE_DECLS;
 
-    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_PCONTROL);
+    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_GET_VERSION);
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
+	    /* Note that this routine may be called before MPI_Init */
             if (mpi_errno) {
-                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_PCONTROL);
+                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_GET_VERSION);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
             }
         }
@@ -63,10 +63,10 @@ int MPI_Pcontrol(const int level, ...)
 #   endif /* HAVE_ERROR_CHECKING */
 
     /* ... body of routine ...  */
-    /* This is a dummy routine that does nothing.  It is intended for 
-       use by the user (or a tool) with the profiling interface */
+    *version    = MPI_VERSION;
+    *subversion = MPI_SUBVERSION;
     /* ... end of body of routine ... */
 
-    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_PCONTROL);
+    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_GET_VERSION);
     return MPI_SUCCESS;
 }
