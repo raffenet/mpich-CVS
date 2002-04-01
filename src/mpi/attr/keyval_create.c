2f7a8cc315e22652f219817ef7539e4f10005ccf
gropp <gropp>
2002-04-01 13:55:19 +0000

    Added MPI-1 keyval create and free

--- src/mpi/attr/keyval_create.c
+++ src/mpi/attr/keyval_create.c
@@ -43,20 +43,20 @@
 .N Errors
 .N MPI_SUCCESS
 @*/
-int MPI_Keyval_create(MPI_Copy_function *copy_fn, MPI_Delete_function *delete_fn, int *keyval, void *extra_state)
+int MPI_Keyval_create(MPI_Copy_function *copy_fn, 
+		      MPI_Delete_function *delete_fn, 
+		      int *keyval, void *extra_state)
 {
     static const char FCNAME[] = "MPI_Keyval_create";
     int mpi_errno = MPI_SUCCESS;
+    MPID_MPI_STATE_DECLS;
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_KEYVAL_CREATE);
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_CREATE);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
@@ -66,6 +66,17 @@ int MPI_Keyval_create(MPI_Copy_function *copy_fn, MPI_Delete_function *delete_fn
     }
 #   endif /* HAVE_ERROR_CHECKING */
 
+    /* ... body of routine ...  */
+    MPIR_Nest_incr();
+    mpi_errno = PMPI_Comm_create_keyval( copy_fn, delete_fn, keyval, 
+					extra_state );
+    MPIR_Nest_decr();
+    if (mpi_errno) {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_CREATE);
+	return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+    }
+
+    /* ... end of body of routine ... */
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_CREATE);
     return MPI_SUCCESS;
 }
--- src/mpi/attr/keyval_free.c
+++ src/mpi/attr/keyval_free.c
@@ -44,16 +44,14 @@ int MPI_Keyval_free(int *keyval)
 {
     static const char FCNAME[] = "MPI_Keyval_free";
     int mpi_errno = MPI_SUCCESS;
+    MPID_MPI_STATE_DECLS;
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_KEYVAL_FREE);
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_FREE);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
@@ -62,6 +60,15 @@ int MPI_Keyval_free(int *keyval)
         MPID_END_ERROR_CHECKS;
     }
 #   endif /* HAVE_ERROR_CHECKING */
+
+    MPIR_Nest_incr();
+    mpi_errno = PMPI_Comm_free_keyval( keyval );
+    MPIR_Nest_decr();
+    if (mpi_errno) {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_FREE);
+	return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+    }
+    /* ... end of body of routine ... */
 
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_KEYVAL_FREE);
     return MPI_SUCCESS;
