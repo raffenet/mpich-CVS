d20353090740ce54d56300c1ccc29ed45e0195e8
toonen <toonen>
2002-04-11 19:36:12 +0000

    (ch3) added implementations of MPID_Isend() and MPID_Irecv()

--- src/mpid/ch3/src/mpid_irecv.c
+++ src/mpid/ch3/src/mpid_irecv.c
@@ -13,13 +13,102 @@
 #define FUNCNAME MPID_Irecv
 #undef FCNAME
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
-
 int MPID_Irecv(void * buf, int count, MPI_Datatype datatype, int rank,
 	       int tag, MPID_Comm * comm, int context_offset,
                MPID_Request ** request)
 {
-    MPIDI_dbg_printf(10, FCNAME, "NOT IMPLEMENTED!");
-    abort();
-    return MPI_ERR_INTERN;
+    MPID_Request * rreq;
+    int found;
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIDI_dbg_printf(10, FCNAME, "entering");
+    MPIDI_dbg_printf(15, FCNAME, "rank=%d, tag=%d, context=%d", rank, tag,
+		     comm->context_id + context_offset);
+    
+    rreq = MPIDI_CH3U_Request_FUOAP(
+	rank, tag, comm->context_id + context_offset, &found);
+    assert(rreq != NULL);
+
+    /* MT - thread safety? message could arrive while populating req */
+    rreq->comm = comm;
+    rreq->ch3.user_buf = buf;
+    rreq->ch3.user_count = count;
+    rreq->ch3.datatype = datatype;
+    rreq->ch3.vc = comm->vcr[rank];
+
+    if (found)
+    {
+	/* Message was found in the unexepected queue */
+	MPIDI_dbg_printf(15, FCNAME, "request found in unexpected queue");
+
+	/* XXX - this check needs to be thread safe; it may be already assuming
+           rreq->cc is declared volatile */
+	/* NOTE - rreq->cc is used here instead of rreq->cc_ptr.  We are
+	   assuming that for simple sends and receives the request's internal
+	   completion counter will always be used. */
+	if (MPIDI_Request_get_msg_type(rreq) == MPIDI_REQUEST_EAGER_MSG)
+	{
+	    /* This is an eager send message.  Assuming all of the data has
+	       arrived, we need to copy the data and then free the buffer and
+	       the request. */
+		
+	    MPIDI_dbg_printf(15, FCNAME, "eager message in the request");
+	    
+	    if (rreq->cc == 0)
+	    {
+		if (count > 0)
+		{
+		    MPIDI_CH3U_Request_copy_tmp_data(rreq);
+		}
+		
+		mpi_errno = rreq->status.MPI_ERROR;
+		goto fn_exit;
+	    }
+	    else
+	    {
+		/* The data is still being transfered across the net.  We'll
+		   leave it to the progress engine to handle once the entire
+		   message has arrived. */
+		rreq->ch3.ca = MPIDI_CH3_CA_COPY_COMPLETE;
+	    }
+	}
+	else
+	{
+	    MPID_Request * cts_req;
+	    MPIDI_CH3_Pkt_t upkt;
+	    MPIDI_CH3_Pkt_rndv_clr_to_send_t * cts_pkt =
+		&upkt.rndv_clr_to_send;
+		
+	    MPIDI_dbg_printf(15, FCNAME, "rndv RTS in the request, "
+			     "sending rndv CTS");
+	    
+	    /* A rendezvous request-to-send (RTS) message has arrived.  We need
+	       to send a clear-to-send message to the remote process. */
+	    cts_pkt->type = MPIDI_CH3_PKT_RNDV_CLR_TO_SEND;
+	    cts_pkt->req_id_sender = rreq->ch3.rndv_req_id;
+	    cts_pkt->req_id_receiver = rreq->handle;
+	    cts_req = MPIDI_CH3_iStartMsg(rreq->ch3.vc,
+					  cts_pkt, sizeof(*cts_pkt));
+	    if (cts_req != NULL)
+	    {
+		/* XXX: Ideally we could specify that a req not be returned.
+		   This would avoid our having to decrement the reference count
+		   on a req we don't want/need. */
+		MPID_Request_free(cts_req);
+	    }
+	}
+    }
+    else
+    {
+	/* Message has yet to arrived.  The request has been placed on the list
+           of posted receive requests and populated with information supplied
+           in the arguments. */
+	MPIDI_dbg_printf(15, FCNAME, "request allocated in posted queue");
+    }
+
+  fn_exit:
+    *request = rreq;
+    MPIDI_dbg_printf(10, FCNAME, "exiting");
+    return mpi_errno;
 }
 
--- src/mpid/ch3/src/mpid_isend.c
+++ src/mpid/ch3/src/mpid_isend.c
@@ -13,13 +13,118 @@
 #define FUNCNAME MPID_Isend
 #undef FCNAME
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
-
 int MPID_Isend(const void * buf, int count, MPI_Datatype datatype, int rank,
 	       int tag, MPID_Comm * comm, int context_offset,
                MPID_Request ** request)
 {
-    MPIDI_dbg_printf(10, FCNAME, "NOT IMPLEMENTED!");
-    abort();
-    return MPI_ERR_INTERN;
+    long dt_sz;
+    int dt_contig;
+    MPID_Request * sreq;
+
+    MPIDI_dbg_printf(10, FCNAME, "entering");
+    MPIDI_dbg_printf(15, FCNAME, "rank=%d, tag=%d, context=%d", rank, tag,
+		     comm->context_id + context_offset);
+    
+    sreq = MPIDI_CH3_Request_create();
+    assert(sreq != NULL);
+    sreq->ref_count = 2;
+    sreq->kind = MPID_REQUEST_SEND;
+    sreq->cc = 1;
+    sreq->cc_ptr = &(sreq->cc);
+    sreq->comm = comm;
+    sreq->ch3.match.rank = rank;
+    sreq->ch3.match.tag = tag;
+    sreq->ch3.match.context_id = comm->context_id + context_offset;
+    sreq->ch3.user_buf = (void *) buf;
+    sreq->ch3.user_count = count;
+    sreq->ch3.datatype = datatype;
+    sreq->ch3.vc = comm->vcr[rank];
+    
+    if (count == 0)
+    {
+	    MPIDI_CH3_Pkt_t upkt;
+	    MPIDI_CH3_Pkt_eager_send_t * const pkt = &upkt.eager_send;
+
+	    sreq->ch3.ca = MPIDI_CH3_CA_COMPLETE;
+	    
+	    MPIDI_dbg_printf(15, FCNAME, "sending zero length message");
+	    pkt->type = MPIDI_CH3_PKT_EAGER_SEND;
+	    pkt->match.rank = comm->rank;
+	    pkt->match.tag = tag;
+	    pkt->match.context_id = comm->context_id + context_offset;
+	    pkt->data_sz = 0;
+
+	    MPIDI_CH3_iSend(comm->vcr[rank], sreq, pkt, sizeof(*pkt));
+
+	    goto fn_exit;
+    }
+    
+    if (HANDLE_GET_KIND(datatype) == HANDLE_KIND_BUILTIN)
+    {
+	dt_sz = MPID_Datatype_get_size(datatype);
+	dt_contig = TRUE;
+    }
+    else
+    {
+	assert(HANDLE_GET_KIND(datatype) == HANDLE_KIND_BUILTIN);
+	abort();
+    }
+
+    /* TODO - flow control: limit number of outstanding eager messsages */
+    
+    if (count * dt_sz + sizeof(MPIDI_CH3_Pkt_eager_send_t) <=
+	MPIDI_CH3_EAGER_MAX_MSG_SIZE)
+    {
+	sreq->ch3.ca = MPIDI_CH3_CA_COMPLETE;
+	
+	/* eager message send */
+	if (dt_contig)
+	{
+	    MPIDI_CH3_Pkt_t upkt;
+	    MPIDI_CH3_Pkt_eager_send_t * const pkt = &upkt.eager_send;
+	    struct iovec iov[2];
+
+	    pkt->type = MPIDI_CH3_PKT_EAGER_SEND;
+	    pkt->match.rank = comm->rank;
+	    pkt->match.tag = tag;
+	    pkt->match.context_id = comm->context_id + context_offset;
+	    pkt->data_sz = count * dt_sz;
+
+	    MPIDI_dbg_printf(15, FCNAME, "sending eager contiguous message, "
+			     "data_sz=%ld", pkt->data_sz);
+	    iov[0].iov_base = pkt;
+	    iov[0].iov_len = sizeof(*pkt);
+	    iov[1].iov_base = (void *) buf;
+	    iov[1].iov_len = count * dt_sz;
+	    MPIDI_CH3_iSendv(comm->vcr[rank], sreq, iov, 2);
+	}
+	else
+	{
+	    assert(dt_contig);
+	    abort();
+	}
+    }
+    else
+    {
+	/* rendezvous protocol */
+	MPIDI_CH3_Pkt_t upkt;
+	MPIDI_CH3_Pkt_rndv_req_to_send_t * const pkt = &upkt.rndv_req_to_send;
+	    
+	sreq->ch3.ca = MPIDI_CH3_CA_NONE;
+	
+	pkt->type = MPIDI_CH3_PKT_RNDV_REQ_TO_SEND;
+	pkt->match.rank = comm->rank;
+	pkt->match.tag = tag;
+	pkt->match.context_id = comm->context_id + context_offset;
+	pkt->data_sz = count * dt_sz;
+	pkt->req_id_sender = sreq->handle;
+
+	MPIDI_CH3_iSend(comm->vcr[rank], sreq, pkt, sizeof(*pkt));
+    }
+
+  fn_exit:
+    MPIDI_dbg_printf(10, FCNAME, "exiting");
+    *request = sreq;
+    return MPI_SUCCESS;
 }
 
