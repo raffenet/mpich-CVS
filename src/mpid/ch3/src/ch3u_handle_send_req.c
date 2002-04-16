1765ddc03736fa22ca277c22fbc7cd8d2b10853f
toonen <toonen>
2002-04-16 18:53:51 +0000

    (ch3) updated comments and renamed the variable "req" to "sreq"

--- src/mpid/ch3/src/ch3u_handle_send_req.c
+++ src/mpid/ch3/src/ch3u_handle_send_req.c
@@ -9,22 +9,23 @@
 /*
  * MPIDI_CH3U_Handle_send_pkt()
  *
- * NOTE: This routine must be reentrant safe.  Routines like MPIDI_CH3_iRead()
- * are allowed to perform up-calls if they complete the requested work
- * immediately. *** Care must be take to avoid deep recursion which with some
- * thread packages can result in overwriting the stack of another thread. ***
+ * NOTE: This routine must be reentrant.  Routines like MPIDI_CH3_iRead() are
+ * allowed to perform additional up-calls if they complete the requested work
+ * immediately. *** Care must be take to avoid deep recursion.  With some
+ * thread packages, exceeding the stack space allocated to a thread can result
+ * in overwriting the stack of another thread. ***
  */
 #undef FUNCNAME
 #define FUNCNAME MPIDI_CH3U_Handle_send_req
 #undef FCNAME
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
-int MPIDI_CH3U_Handle_send_req(MPIDI_VC * vc, MPID_Request * req)
+int MPIDI_CH3U_Handle_send_req(MPIDI_VC * vc, MPID_Request * sreq)
 {
     int completion = FALSE;
     
-    assert(req->ch3.ca < MPIDI_CH3_CA_END_CH3);
+    assert(sreq->ch3.ca < MPIDI_CH3_CA_END_CH3);
     
-    switch(req->ch3.ca)
+    switch(sreq->ch3.ca)
     {
 	case MPIDI_CH3_CA_NONE:
 	{
@@ -34,6 +35,8 @@ int MPIDI_CH3U_Handle_send_req(MPIDI_VC * vc, MPID_Request * req)
 	
 	case MPIDI_CH3_CA_RELOAD_IOV:
 	{
+	    MPIDI_err_printf(FCNAME, "MPIDI_CH3_CA_RELOAD_IOV UMIMPLEMENTED");
+	    abort();
 	    break;
 	}
 	
@@ -41,10 +44,10 @@ int MPIDI_CH3U_Handle_send_req(MPIDI_VC * vc, MPID_Request * req)
 	{
 	    int cc;
 		
-	    MPIDI_CH3U_Request_decrement_cc(req, &cc);
+	    MPIDI_CH3U_Request_decrement_cc(sreq, &cc);
 	    if (cc == 0)
 	    {
-		MPID_Request_free(req);
+		MPID_Request_free(sreq);
 		completion = TRUE;
 	    }
 	    break;
@@ -52,8 +55,7 @@ int MPIDI_CH3U_Handle_send_req(MPIDI_VC * vc, MPID_Request * req)
 	
 	default:
 	{
-	    MPIDI_dbg_printf(30, FCNAME, "action %d not implemented",
-			     req->ch3.ca);
+	    MPIDI_err_printf(FCNAME, "action %d UNIMPLEMENTED", sreq->ch3.ca);
 	    abort();
 	}
     }
