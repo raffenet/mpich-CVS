56720564f28aa01b47ffdf66873f3b5b2ad24e0d
robl <robl>
2002-04-30 22:50:09 +0000

    backed out process-zero optimizations.  seemed to break tests/misc.c

--- src/mpi/romio/adio/ad_pvfs/ad_pvfs_resize.c
+++ src/mpi/romio/adio/ad_pvfs/ad_pvfs_resize.c
@@ -7,34 +7,14 @@
 
 #include "ad_pvfs.h"
 
-/* ADIOI_PVFS_Resize()
- *
- * Assumptions:
- * - called collectively (as defined in the MPI-2 standard)
- *
- * Approach:
- * - Process 0 does the pvfs_ftruncate64(), so only one message goes out
- *   to the file system
- * - Process 0 Bcasts its return value to the rest of the processes
- * - When other processes receive value, they know the
- *   pvfs_ftruncate64() has completed.
- */
 void ADIOI_PVFS_Resize(ADIO_File fd, ADIO_Offset size, int *error_code)
 {
-    int err, rank, procs;
+    int err;
 #ifndef PRINT_ERR_MSG
     static char myname[] = "ADIOI_PVFS_RESIZE";
 #endif
-    MPI_Comm_rank(fd->comm, &rank);
-    if (rank == 0) {
-    	err = pvfs_ftruncate64(fd->fd_sys, size);
-    }
-
-    MPI_Comm_size(fd->comm, &procs);
-    if (procs > 1) {
-    	MPI_Bcast(&err, 1, MPI_INT, 0, fd->comm);
-    }
-
+    
+    err = pvfs_ftruncate64(fd->fd_sys, size);
 #ifdef PRINT_ERR_MSG
     *error_code = (err == 0) ? MPI_SUCCESS : MPI_ERR_UNKNOWN;
 #else
