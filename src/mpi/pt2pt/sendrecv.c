9b6049e8bb767182c4dfa6c302969345cc00e05f
toonen <toonen>
2002-05-01 22:26:32 +0000

    (mpi/pt2pt) filled in MPI_Sendrecv()

--- src/mpi/pt2pt/sendrecv.c
+++ src/mpi/pt2pt/sendrecv.c
@@ -56,21 +56,48 @@ int MPI_Sendrecv(void *sendbuf, int sendcount, MPI_Datatype sendtype, int dest, 
     static const char FCNAME[] = "MPI_Sendrecv";
     int mpi_errno = MPI_SUCCESS;
     MPID_Comm *comm_ptr = NULL;
-
-    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_SENDRECV);
-    /* Get handles to MPI objects. */
-    MPID_Comm_get_ptr( comm, comm_ptr );
+    MPID_Request * reqs[2];
+    MPID_MPI_STATE_DECLS;
+    
+    /* Verify that MPI has been initialized */
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
+            if (mpi_errno) {
+                return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
             }
-            /* Validate comm_ptr */
+	}
+        MPID_END_ERROR_CHECKS;
+    }
+#   endif /* HAVE_ERROR_CHECKING */
+	    
+    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_SENDRECV);
+    
+    /* Convert handles to MPI objects. */
+    MPID_Comm_get_ptr( comm, comm_ptr );
+    
+#   ifdef HAVE_ERROR_CHECKING
+    {
+        MPID_BEGIN_ERROR_CHECKS;
+        {
+	    MPID_Datatype * sendtype_ptr = NULL;
+	    MPID_Datatype * recvtype_ptr = NULL;
+	    
+	    
+	    /* Validate communicator */
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
-	    /* If comm_ptr is not value, it will be reset to null */
+            if (mpi_errno) {
+                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SENDRECV);
+                return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+            }
+	    
+            /* Validate datatypes */
+	    MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
+	    MPID_Datatype_get_ptr(recvtype, sendtype_ptr);
+            MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
+            MPID_Datatype_valid_ptr( recvtype_ptr, mpi_errno );
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SENDRECV);
                 return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
@@ -80,6 +107,45 @@ int MPI_Sendrecv(void *sendbuf, int sendcount, MPI_Datatype sendtype, int dest, 
     }
 #   endif /* HAVE_ERROR_CHECKING */
 
+    mpi_errno = MPID_Irecv(recvbuf, recvcount, recvtype, source, recvtag,
+			   comm_ptr, MPID_CONTEXT_INTRA_PT2PT, &reqs[0]);
+    if (mpi_errno)
+    {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SENDRECV);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+    mpi_errno = MPID_Isend(sendbuf, sendcount, sendtype, dest, sendtag,
+			   comm_ptr, MPID_CONTEXT_INTRA_PT2PT, &reqs[1]);
+    if (mpi_errno)
+    {
+	MPID_Request_release(reqs[0]);
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SENDRECV);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+
+    while(1)
+    {
+	MPID_Progress_start();
+	
+	if (*reqs[0]->cc_ptr != 0 || *reqs[1]->cc_ptr != 0)
+	{
+	    MPID_Progress_wait();
+	}
+	else
+	{
+	    MPID_Progress_end();
+	    break;
+	}
+    }
+
+    if (status != NULL)
+    {
+	*status = reqs[0]->status;
+    }
+
+    MPID_Request_release(reqs[0]);
+    MPID_Request_release(reqs[1]);
+    
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SENDRECV);
     return MPI_SUCCESS;
 }
