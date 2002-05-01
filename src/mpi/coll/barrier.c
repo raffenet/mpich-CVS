deeb7fb32a6aff5bdec57e7a31cb50413f1de97e
toonen <toonen>
2002-05-01 21:08:43 +0000

    (mpi/coll) added MPI_Barrier()

--- src/mpi/coll/barrier.c
+++ src/mpi/coll/barrier.c
@@ -47,22 +47,32 @@ int MPI_Barrier( MPI_Comm comm )
     MPID_Comm *comm_ptr = NULL;
     MPID_MPI_STATE_DECLS;
 
-    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_BARRIER);
-    /* Get handles to MPI objects. */
-    MPID_Comm_get_ptr( comm, comm_ptr );
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
-            /* Validate comm_ptr */
-            MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
-	    /* If comm_ptr is not value, it will be reset to null */
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
             if (mpi_errno) {
-                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_BARRIER);
+                return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+            }
+        }
+        MPID_END_ERROR_CHECKS;
+    }
+#   endif /* HAVE_ERROR_CHECKING */
+
+    MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_BARRIER);
+    
+    /* Convert handles to MPI objects. */
+    MPID_Comm_get_ptr( comm, comm_ptr );
+    
+#   ifdef HAVE_ERROR_CHECKING
+    {
+        MPID_BEGIN_ERROR_CHECKS;
+        {
+	    /* Validate communicator */
+            MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
+            if (mpi_errno) {
+                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_SEND);
                 return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
             }
         }
@@ -70,8 +80,26 @@ int MPI_Barrier( MPI_Comm comm )
     }
 #   endif /* HAVE_ERROR_CHECKING */
 
-    err_printf("ERROR: MPI_Barrier not implemented.\n");
+    if (comm_ptr->coll_fns != NULL && comm_ptr->coll_fns->Barrier != NULL)
+    {
+	mpi_errno = comm_ptr->coll_fns->Barrier(comm_ptr);
+	if (mpi_errno == MPI_SUCCESS)
+	{
+	    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_BARRIER);
+	    return MPI_SUCCESS;
+	}
+	else
+	{
+	    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_BARRIER);
+	    return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+	}
+    }
+    else
+    {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_BARRIER);
+	mpi_errno = MPIR_Err_create_code( MPI_ERR_INTERN,
+					  "**barriernotimpl", 0 );
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
 
-    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_BARRIER);
-    return MPI_SUCCESS;
 }
