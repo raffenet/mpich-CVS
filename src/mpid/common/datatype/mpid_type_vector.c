e6af392283dc9722f6c69f5f15eab37539514fa3
rross <rross>
2002-06-04 22:20:09 +0000

    Added MPIR_Err_return_comm() call to MPID_Type_vector().

--- src/mpid/common/datatype/mpid_type_vector.c
+++ src/mpid/common/datatype/mpid_type_vector.c
@@ -28,11 +28,19 @@ int MPID_Type_vector(int count,
 		     MPI_Datatype oldtype,
 		     MPI_Datatype *newtype)
 {
+    static const char FCNAME[] = "MPID_Type_vector";
+    int mpi_errno = MPI_SUCCESS;
+
     MPID_Datatype *new_dtp;
     struct MPID_Dataloop *dlp;
 
     /* allocate new datatype object and handle */
     new_dtp = (MPID_Datatype *) MPIU_Handle_obj_alloc(&MPID_Datatype_mem);
+    if (!new_dtp) {
+	mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER, "**nomem", 0 );
+	return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+    }
+
     /* Note: handle and ref_count, the first two parameters in the datatype
      * structure, are filled in automatically by the handle allocation
      * function.
