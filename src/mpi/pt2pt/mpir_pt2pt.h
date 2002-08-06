a96bf8ff5ea2f8372d499299dac786b94b8f7572
ashton <ashton>
2002-08-06 21:07:44 +0000

    added copyright

--- src/mpi/pt2pt/mpir_pt2pt.h
+++ src/mpi/pt2pt/mpir_pt2pt.h
@@ -1,3 +1,8 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
 /* Definitions local to src/mpi/pt2pt */
 void MPIR_Wait(MPID_Request *);
 int MPIR_Test(MPID_Request *);
