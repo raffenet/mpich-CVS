e466d2664bb5bea6178aaa3622c769c6ec441c88
gropp <gropp>
2002-08-09 20:31:35 +0000

    Added MPIU_Strncpy

---
+++ src/util/mem/safestr.c
@@ -0,0 +1,45 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  $Id$
+ *
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include "mpiimpl.h"
+
+/* 
+ * This file contains "safe" versions of the various string and printf
+ * operations.  So far, only strncpy is included.
+ */
+
+/*
+ * MPIU_Strncpy - Copy at most n character.  Stop once a null is reached.
+ *
+ * This is different from strncpy, which null pads so that exactly
+ * n characters are copied.  The strncpy behavior is correct for many 
+ * applications because it guarantees that the string has no uninitialized
+ * data.
+ *
+ * If n characters are copied without reaching a null, return an error.
+ * Otherwise, return 0.
+ */
+int MPIU_Strncpy( char *dest, const char *src, size_t n )
+{
+    char * restrict d_ptr = dest;
+    const char * restrict s_ptr = src;
+    register int i;
+
+    i = n;
+    while (i-- > 0 && *s_ptr) {
+	*d_ptr++ = *s_ptr++;
+    }
+    
+    if (i > 0) { 
+	*d_ptr = 0;
+	return 0;
+    }
+    else
+	/* We may want to force an error message here, at least in the
+	   debugging version */
+	return 1;
+}
