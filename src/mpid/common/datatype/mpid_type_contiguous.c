da6788287b6e6d2131e5a815e08662d959bea0d4
rross <rross>
2002-08-16 16:31:42 +0000

    Oops.

---
+++ src/mpid/common/datatype/mpid_type_contiguous.c
@@ -0,0 +1,137 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include <mpiimpl.h>
+#include <mpid_dataloop.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/*@
+  MPID_Type_contiguous - create a contiguous datatype
+ 
+  Input Parameters:
++ count - number of elements in the contiguous block
+- oldtype - type (using handle) of datatype on which vector is based
+
+  Output Parameters:
+. newtype - handle of new vector datatype
+
+  Return Value:
+  0 on success, -1 on failure.
+
+  This routine calls MPID_Dataloop_create_struct() to create the loops for this
+  new datatype.  It calls MPID_Datatype_new() to allocate space for the new
+  datatype.
+@*/
+int MPID_Type_contiguous(int count,
+			 MPI_Datatype oldtype,
+			 MPI_Datatype *newtype)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPID_Datatype *new_dtp;
+    struct MPID_Dataloop *dlp;
+
+    /* allocate new datatype object and handle */
+    new_dtp = (MPID_Datatype *) MPIU_Handle_obj_alloc(&MPID_Datatype_mem);
+    if (!new_dtp) {
+	mpi_errno = MPIR_Err_create_code(MPI_ERR_OTHER, "**nomem", 0);
+	return mpi_errno;
+    }
+
+    /* Note: handle and ref_count, the first two parameters in the datatype
+     * structure, are filled in automatically by the handle allocation
+     * function.
+     */
+
+    new_dtp->combiner     = MPI_COMBINER_CONTIGUOUS;
+    new_dtp->is_permanent = 0;
+    new_dtp->is_committed = 0;
+    new_dtp->attributes   = 0;
+    new_dtp->cache_id     = 0;
+    new_dtp->name[0]      = 0;
+
+    /* NOTE: see mpid_type_vector.c for a more thoroughly commented MPID type fn. */
+    if (HANDLE_GET_KIND(oldtype) == HANDLE_KIND_BUILTIN) {
+	int oldsize = MPID_Datatype_get_basic_size(oldtype);
+
+	new_dtp->size           = count * oldsize;
+	new_dtp->extent         = count * oldsize;
+	new_dtp->has_mpi1_ub    = 0;
+	new_dtp->has_mpi1_lb    = 0;
+	new_dtp->loopinfo_depth = 1;
+	new_dtp->true_lb        = 0;
+	new_dtp->alignsize      = oldsize;
+	new_dtp->n_elements     = count;
+	new_dtp->is_contig      = 1;
+
+	/* allocate dataloop */
+	new_dtp->loopsize       = sizeof(struct MPID_Dataloop);
+	dlp                     = (struct MPID_Dataloop *)MPIU_Malloc(sizeof(struct MPID_Dataloop));
+	if (dlp == NULL) assert(0);
+	new_dtp->opt_loopinfo   = dlp; /* TODO: DO OPTIMIZED LOOPS */
+	new_dtp->loopinfo       = dlp;
+
+	/* fill in dataloop */
+	/* NOTE: element size in kind is probably wrong */
+	dlp->kind                       = DLOOP_KIND_CONTIG | DLOOP_FINAL_MASK |
+	                                  (oldsize << DLOOP_ELMSIZE_SHIFT);
+	dlp->handle                     = new_dtp->handle;
+	dlp->loop_params.c_t.count      = count;
+	dlp->loop_params.c_t.u.handle   = oldtype;
+	dlp->el_extent                  = oldsize;
+	dlp->el_size                    = oldsize;
+    }
+    else /* user-defined base type */ {
+	int new_loopsize;
+	MPID_Datatype *old_dtp;
+	char *curpos;
+
+	/* get pointer to old datatype from handle */
+	MPID_Datatype_get_ptr(oldtype, old_dtp); /* fills in old_dtp */
+
+	/* fill in datatype */
+	new_dtp->size           = count * old_dtp->size;
+	new_dtp->extent         = count * old_dtp->extent;
+	new_dtp->has_mpi1_ub    = old_dtp->has_mpi1_ub; /* ??? */
+	new_dtp->has_mpi1_lb    = old_dtp->has_mpi1_lb; /* ??? */
+	new_dtp->loopinfo_depth = old_dtp->loopinfo_depth + 1;
+	new_dtp->true_lb        = old_dtp->true_lb; /* ??? */
+	new_dtp->alignsize      = old_dtp->alignsize;
+	new_dtp->n_elements     = count * old_dtp->n_elements; /* ??? */
+	new_dtp->is_contig      = old_dtp->is_contig; /* ??? */
+
+	/* allocate space for dataloop */
+	new_loopsize = old_dtp->loopsize + sizeof(struct MPID_Dataloop);
+	dlp = (struct MPID_Dataloop *) MPIU_Malloc(new_loopsize);
+	if (dlp == NULL) assert(0);
+
+	new_dtp->loopinfo       = dlp;
+	new_dtp->opt_loopinfo   = dlp;
+	new_dtp->loopsize       = new_loopsize;
+
+	/* fill in top part of dataloop */
+	/* NOTE: element size in kind is probably wrong */
+	dlp->kind                  = DLOOP_KIND_CONTIG |
+	                             (old_dtp->size << DLOOP_ELMSIZE_SHIFT);
+	/* NOTE: new_dtp->handle is filled in by MPIU_Handle_obj_alloc() */
+	dlp->handle                = new_dtp->handle;
+	dlp->loop_params.c_t.count = count;
+	dlp->el_extent             = old_dtp->extent;
+	dlp->el_size               = old_dtp->size;
+
+	/* copy in old dataloop */
+	curpos = (char *) dlp; /* NEED TO PAD? */
+	curpos += sizeof(struct MPID_Dataloop);
+
+	MPID_Dataloop_copy(curpos, old_dtp->loopinfo, old_dtp->loopsize);
+	dlp->loop_params.c_t.u.dataloop = (struct MPID_Dataloop *) curpos;
+    }
+
+    *newtype = new_dtp->handle;
+    return MPI_SUCCESS;
+}
