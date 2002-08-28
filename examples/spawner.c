c4dcadb2e6f8b86284c754684444bdb685dd4499
lusk <lusk>
2002-08-28 22:41:45 +0000

    added spawner/spawnee examples

---
+++ examples/spawner.c
@@ -0,0 +1,44 @@
+#include "mpi.h"
+#include <stdio.h>
+
+#define MAX_SPAWNED 32
+
+int main( int argc, char * argv[] )
+{
+    int rank;
+    int size;
+    int rc, maxprocs = 1;
+    char *args[5];
+    MPI_Comm newintercomm;
+    int errcodes[MAX_SPAWNED];
+    char *kvsname, executable[128];
+
+    MPI_Init( 0, 0 );
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf( "spawner %d of %d is alive\n", rank, size );  fflush(stdout);
+    kvsname = (char *) malloc(PMI_KVS_Get_name_length_max( ) );
+    PMI_KVS_Get_my_name( kvsname );
+    printf( "spawner %d of %d, kvsname=:%s:\n", rank, size, kvsname );  fflush(stdout);
+
+    if ( argc > 1 ) {
+	maxprocs = atoi( argv[1] );
+	if ( maxprocs > MAX_SPAWNED ) {
+	    printf( "must spawn fewer than %d spawnees\n", MAX_SPAWNED );
+	    exit( -1 );
+	}
+    }
+    strncpy( executable, "/sandbox/lusk/holdit/mpich2-mpd-aug28/examples/spawnee", 128 );
+    args[0] = "argument_1";
+    args[1] = (char *) 0;
+    rc = MPI_Comm_spawn( executable, args, maxprocs, MPI_INFO_NULL, 0, MPI_COMM_WORLD,
+			 &newintercomm, errcodes );
+    if ( rc == MPI_SUCCESS )
+	printf( "spawner %d succeeded\n", rank );
+    else
+	printf( "spawner %d returned %d\n", rank, rc );
+
+    MPI_Finalize();
+    return 0;
+}
+    
