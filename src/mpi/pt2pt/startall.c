0edee70f7cc162f3961fbca7a1f1693e1e38652c
toonen <toonen>
2002-08-29 22:17:51 +0000

    (mpi/pt2pt) added comment about error reporting limitations of MPI_Startall()

--- src/mpi/pt2pt/startall.c
+++ src/mpi/pt2pt/startall.c
@@ -40,6 +40,12 @@
 
    Notes:
 
+   Unlike MPI_Waitall(), MPI_Startall() does not provide a mechanism for
+   returning multiple errors nor pinpointing the request(s) involved.
+   Futhermore, the behavior of MPI_Startall() after an error occurs is not
+   defined by the MPI standard.  If well defined error reporting and behavior
+   are required, multiple calls to MPI_Start() should be used instead.
+
 .N Fortran
 
 .N Errors
