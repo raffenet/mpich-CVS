4fe3a40261a87c20c61c5860b3513bb06242cc67
toonen <toonen>
2002-08-29 17:24:06 +0000

    (mpi/pt2pt) updated MPI_Request_free() to handle active persistent requests.  also rename comreq_free.c to request_free.c.

--- src/mpi/pt2pt/request_free.c
+++ src/mpi/pt2pt/request_free.c
@@ -84,6 +84,17 @@ int MPI_Request_free(MPI_Request *request)
     
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_REQUEST_FREE);
 
+    /* If this is an active persistent request, we must also release the
+       partner request. */
+    if (request_ptr->kind == MPID_PREQUEST_SEND ||
+	request_ptr->kind == MPID_PREQUEST_RECV)
+    {
+	if (request_ptr->partner_request != NULL)
+	{
+	    MPID_Request_release(request_ptr->partner_request);
+	}
+    }
+
     MPID_Request_release(request_ptr);
     *request = MPI_REQUEST_NULL;
     
