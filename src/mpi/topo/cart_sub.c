a1298462a6bde4843d4e7de0f6c684b74d1a5066
gropp <gropp>
2002-09-16 12:39:18 +0000

    Implement cart sub

--- src/mpi/topo/cart_sub.c
+++ src/mpi/topo/cart_sub.c
@@ -6,6 +6,7 @@
  */
 
 #include "mpiimpl.h"
+#include "topo.h"
 
 /* -- Begin Profiling Symbol Block for routine MPI_Cart_sub */
 #if defined(HAVE_PRAGMA_WEAK)
@@ -46,7 +47,9 @@ int MPI_Cart_sub(MPI_Comm comm, int *remain_dims, MPI_Comm *comm_new)
 {
     static const char FCNAME[] = "MPI_Cart_sub";
     int mpi_errno = MPI_SUCCESS;
-    MPID_Comm *comm_ptr = NULL;
+    int ndims, key, color, ndims_in_subcomm, i, j;
+    MPID_Comm *comm_ptr = NULL, *newcomm_ptr;
+    MPIR_Topology *topo_ptr, *toponew_ptr;
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_CART_SUB);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_CART_SUB);
@@ -56,13 +59,10 @@ int MPI_Cart_sub(MPI_Comm comm, int *remain_dims, MPI_Comm *comm_new)
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+            MPIR_ERRTEST_INITIALIZED(mpi_errno);
             /* Validate comm_ptr */
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
-	    /* If comm_ptr is not value, it will be reset to null */
+	    /* If comm_ptr is not valid, it will be reset to null */
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
                 return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
@@ -73,6 +73,94 @@ int MPI_Cart_sub(MPI_Comm comm, int *remain_dims, MPI_Comm *comm_new)
 #   endif /* HAVE_ERROR_CHECKING */
 
     /* ... body of routine ...  */
+    
+    /* Check that the communicator already has a Cartesian topology */
+    topo_ptr = MPIR_Topology_get( comm_ptr );
+
+    if (!topo_ptr) {
+	mpi_errno = MPIR_Err_create_code( MPI_ERR_TOPOLOGY, "**notopology", 0 );
+    }
+    else if (topo_ptr->kind != MPI_CART) {
+	mpi_errno = MPIR_Err_create_code( MPI_ERR_TOPOLOGY, "**notcarttopo", 0 );
+    }
+    if (mpi_errno) {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+
+    /* Determine the number of remaining dimensions */
+    ndims = topo_ptr->topo.cart.ndims;
+    ndims_in_subcomm = 0;
+    for (i=0; i<ndims; i++) {
+	if (remain_dims[i]) {
+	    ndims_in_subcomm ++;
+	}
+    }
+
+    /* Split this communicator.  Do this even if there are no remaining
+       dimensions so that the topology information is attached */
+    key   = 0;
+    color = 0;
+    for (i=0; i<ndims; i++) {
+	if (remain_dims[i]) {
+	    key = (key * topo_ptr->topo.cart.dims[i]) + topo_ptr->topo.cart.position[i];
+	}
+	else {
+	    color = (color * topo_ptr->topo.cart.dims[i]) + topo_ptr->topo.cart.position[i];
+	}
+    }
+    MPIR_Nest_incr();
+    mpi_errno = NMPI_Comm_split( comm, color, key, comm_new );
+    MPIR_Nest_decr();
+    if (mpi_errno) {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+    
+    /* Save the topology of this new communicator */
+    toponew_ptr = (MPIR_Topology *)MPIU_Malloc( sizeof( MPIR_Topology) );
+    if (!toponew_ptr) {
+	mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER, "**nomem", 0 );
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+	
+    toponew_ptr->kind            = MPI_CART;
+    toponew_ptr->topo.cart.ndims = ndims_in_subcomm;
+    if (ndims_in_subcomm) {
+	toponew_ptr->topo.cart.dims     = (int *)MPIU_Malloc( ndims_in_subcomm * sizeof(int) );
+	toponew_ptr->topo.cart.periodic = (int *)MPIU_Malloc( ndims_in_subcomm * sizeof(int) );
+	toponew_ptr->topo.cart.position = (int *)MPIU_Malloc( ndims_in_subcomm * sizeof(int) );
+	if (!toponew_ptr->topo.cart.dims || ! toponew_ptr->topo.cart.periodic ||
+	    !toponew_ptr->topo.cart.position) {
+	    mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER, "**nomem", 0 );
+	    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
+	    return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+	}
+    }
+    else {
+	toponew_ptr->topo.cart.dims     = 0;
+	toponew_ptr->topo.cart.periodic = 0;
+	toponew_ptr->topo.cart.position = 0;
+    }
+
+    j =0;
+    for (i=0; i<ndims; i++) {
+	if (remain_dims[i]) {
+	    toponew_ptr->topo.cart.dims[j] = topo_ptr->topo.cart.dims[i];
+	    toponew_ptr->topo.cart.periodic[j] = topo_ptr->topo.cart.periodic[i];
+	    toponew_ptr->topo.cart.position[j] = topo_ptr->topo.cart.position[i];
+	    j++;
+	}
+    }
+
+    MPID_Comm_get_ptr( *comm_new, newcomm_ptr );
+    mpi_errno = MPIR_Topology_put( newcomm_ptr, toponew_ptr );
+    if (mpi_errno) {
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
+	return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
+    }
+
     /* ... end of body of routine ... */
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_CART_SUB);
     return MPI_SUCCESS;
