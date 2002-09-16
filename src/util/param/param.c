6e4f7b723604ec0b4d913959743c8e06d005c67d
gropp <gropp>
2002-09-16 13:25:47 +0000

    *** empty log message ***

--- src/util/param/param.c
+++ src/util/param/param.c
@@ -11,7 +11,23 @@
 /*
   This file implements the parameter routines.  These routines provide a 
   uniform mechanism to access parameters that are used within the mpich2 code.
+
+  This implements a relatively simple system that stores key/value pairs.
+  Values are normally set at initialization and then not changed, so
+  a simple sorted array of entries can be used.
 */
+
+typedef struct {
+    char *name;
+    enum { MPIU_STRING, MPIU_INT } kind;
+    union {
+	char *string_value;
+	int  int_value;
+    };
+} Param_entry;
+
+static int nentries = 0;
+static Param_entry *param_table = 0;
 
 
 /*@
@@ -33,5 +49,55 @@
   @*/
 int MPIU_Param_init( int *argc_p, char *argv_p[], const char def_file[] )
 {
+    if (def_file && def_file[0]) {
+	/* Read the file */
+	;
+    }
     return 0;
+}
+
+int MPIU_Param_bcast( void )
+{
+    return;
+}
+
+int MPIU_Param_register( const char name[], const char envname[], 
+                         const char description[] )
+{
+    return 0;
+}
+
+int MPIU_Param_get_int( const char name[], int default_val, int *value )
+{
+    int i, cmp;
+    /* Search through the ordered table that is param_table.  
+       Linear search for now; binary tree search is almost as easy */
+    for (i=0; i<nentries; i++) {
+	cmp = strcmp( param_table[i].name, name );
+	if (cmp == 0) {
+	    if (param_table[i].kind == MPIU_INT) {
+		*value = param_table[i].int_value;
+		return 0;
+	    }
+	    else {
+		return 2;
+	    }
+	}
+	else if (cmp < 0) {
+	    *value = default_val;
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+int MPIU_Param_get_string( const char name[], const char *default_val,
+                           char **value )
+{
+    return 0;
+}
+
+void MPIU_Param_finalize( void )
+{
+    return;
 }
