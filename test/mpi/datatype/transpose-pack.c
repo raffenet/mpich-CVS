be4767e0654c1625ebc436cec633ae6a4b2f9a5f
rross <rross>
2002-10-02 14:46:16 +0000

    Added four datatype tests, including those that test packing routines.

---
+++ test/mpi/datatype/transpose-pack.c
@@ -0,0 +1,103 @@
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <malloc.h>
+#include "mpi.h"
+
+static int verbose = 0;
+
+int main(int argc, char *argv[]);
+int parse_args(int argc, char **argv);
+
+int main(int argc, char *argv[])
+{
+    /* Variable declarations */
+    int a[100][100], b[100][100];
+    int row, xpose, sizeofint;
+	
+    int err, errs = 0;
+    int bufsize, position;
+    void *buffer;
+  
+    int i, j;
+  
+    /* Initialize a to some known values. */
+    for(i = 0; i < 100; i++) {
+	for(j = 0; j < 100; j++) {
+	    a[i][j] = i*1000+j;
+	    b[i][j] = -1;
+	}
+    }
+  
+    /* Initialize MPI */
+    MPI_Init(&argc, &argv);
+    parse_args(argc, argv);
+
+    MPI_Type_extent(MPI_INT, &sizeofint);
+	
+    /* Create datatypes. */
+    MPI_Type_vector(100, 1, 100, MPI_INT, &row);
+    MPI_Type_hvector(100, 1, sizeofint, row, &xpose);
+    MPI_Type_commit(&xpose);
+	
+    /* Pack it. */
+    MPI_Pack_size(1, xpose, MPI_COMM_WORLD, &bufsize);
+    buffer = (void *) malloc((unsigned) bufsize);
+
+    err = MPI_Pack(a,
+		   1,
+		   xpose,
+		   buffer,
+		   bufsize,
+		   &position,
+		   MPI_COMM_WORLD);
+	
+    /* Unpack the buffer into b. */
+    position = 0;
+    err = MPI_Unpack(buffer,
+		     bufsize,
+		     &position,
+		     b,
+		     100*100,
+		     MPI_INT,
+		     MPI_COMM_WORLD);
+
+    for (i = 0; i < 100; i++) {
+	for (j = 0; j < 100; j++) {
+	    if(b[i][j] != a[j][i]) {
+		errs++;
+		if (verbose) fprintf(stderr, "b[%d][%d] = %d, should be %d\n",
+				     i, j, b[i][j], a[j][i]);
+	    }
+	}
+    }
+
+    MPI_Type_free(&xpose);
+    MPI_Type_free(&row);
+    
+    /* print message and exit */
+    if (errs) {
+	fprintf(stderr, "Found %d errors\n", errs);
+    }
+    else {
+	printf("No errors\n");
+    }
+    MPI_Finalize();
+    return 0;
+}
+
+
+int parse_args(int argc, char **argv)
+{
+    int ret;
+
+    while ((ret = getopt(argc, argv, "v")) >= 0)
+    {
+	switch (ret) {
+	    case 'v':
+		verbose = 1;
+		break;
+	}
+    }
+    return 0;
+}
