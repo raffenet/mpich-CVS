78ff910a19dbc13abcc5371638466176d53ce096
David <David>
2002-10-09 19:59:49 +0000

    replaced BYU Translate_Error files with newer ones written at Argonne

--- src/mpid/mm/src/mpiexec/mpirun/Translate_Error.cpp
+++ src/mpid/mm/src/mpiexec/mpirun/Translate_Error.cpp
@@ -1,16 +1,10 @@
-/**********************************************************************
-/* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-/* ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-/* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-/* PARTICULAR PURPOSE.
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
 /*
-/* Copyright (C) 1998  Brigham Young University.  All Rights Reserved.
-/*
-/* file: Translate_Error.cpp
-/*
-/**********************************************************************/
+ *
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
 
-// Error message code
 #include "Translate_Error.h"
 #include <stdio.h>
 
@@ -21,21 +15,22 @@
 // Argument         : char *msg
 void Translate_Error(int error, char *msg, char *prepend)
 {
-	HLOCAL str;
-	int num_bytes;
-	num_bytes = FormatMessage(
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		0,
-		error,
-		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
-		(LPTSTR) &str,
-		0,0);
-	if (prepend == NULL)
-		memcpy(msg, str, num_bytes+1);
-	else
-		sprintf(msg, "%s%s", prepend, str);
-	LocalFree(str);
+    HLOCAL str;
+    int num_bytes;
+    num_bytes = FormatMessage(
+	FORMAT_MESSAGE_FROM_SYSTEM |
+	FORMAT_MESSAGE_ALLOCATE_BUFFER,
+	0,
+	error,
+	MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+	(LPTSTR) &str,
+	0,0);
+    if (prepend == NULL)
+	memcpy(msg, str, num_bytes+1);
+    else
+	sprintf(msg, "%s%s", prepend, str);
+    LocalFree(str);
+    strtok(msg, "\r\n");
 }
 
 // Function name	: Translate_HRError
@@ -45,41 +40,43 @@ void Translate_Error(int error, char *msg, char *prepend)
 // Argument         : char *error_msg
 void Translate_HRError(HRESULT hr, char *error_msg, char *prepend)
 {
-	HLOCAL str;
-	FormatMessage(
-		FORMAT_MESSAGE_FROM_SYSTEM | 
-		FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		0,
-		hr,
-		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
-		(LPTSTR) &str,
-		0,0);
-	
-	if (prepend == NULL)
-		sprintf(error_msg, "%s", str);
-	else
-		sprintf(error_msg, "%s%s", prepend, str);
-
-	LocalFree(str);
+    HLOCAL str;
+    FormatMessage(
+	FORMAT_MESSAGE_FROM_SYSTEM | 
+	FORMAT_MESSAGE_ALLOCATE_BUFFER,
+	0,
+	hr,
+	MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+	(LPTSTR) &str,
+	0,0);
+    
+    if (prepend == NULL)
+	sprintf(error_msg, "%s", str);
+    else
+	sprintf(error_msg, "%s%s", prepend, str);
+    
+    LocalFree(str);
+    strtok(error_msg, "\r\n");
 }
 
 void Translate_Error(int error, WCHAR *msg, WCHAR *prepend)
 {
-	HLOCAL str;
-	int num_bytes;
-	num_bytes = FormatMessageW(
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		0,
-		error,
-		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
-		(WCHAR*)&str,
-		0,0);
-	if (prepend == NULL)
-		swprintf(msg, L"%s", str);
-	else
-		swprintf(msg, L"%s%s", prepend, str);
-	LocalFree(str);
+    HLOCAL str;
+    int num_bytes;
+    num_bytes = FormatMessageW(
+	FORMAT_MESSAGE_FROM_SYSTEM |
+	FORMAT_MESSAGE_ALLOCATE_BUFFER,
+	0,
+	error,
+	MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+	(WCHAR*)&str,
+	0,0);
+    if (prepend == NULL)
+	swprintf(msg, L"%s", str);
+    else
+	swprintf(msg, L"%s%s", prepend, str);
+    LocalFree(str);
+    wcstok(msg, L"\r\n");
 }
 
 // Function name	: Translate_HRError
@@ -89,21 +86,21 @@ void Translate_Error(int error, WCHAR *msg, WCHAR *prepend)
 // Argument         : char *error_msg
 void Translate_HRError(HRESULT hr, WCHAR *error_msg, WCHAR *prepend)
 {
-	HLOCAL str;
-	FormatMessageW(
-		FORMAT_MESSAGE_FROM_SYSTEM | 
-		FORMAT_MESSAGE_ALLOCATE_BUFFER,
-		0,
-		hr,
-		MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
-		(WCHAR*) &str,
-		0,0);
-
-	if (prepend == NULL)
-		swprintf(error_msg, L"%s", str);
-	else
-		swprintf(error_msg, L"%s%s", prepend, str);
-
-	LocalFree(str);
+    HLOCAL str;
+    FormatMessageW(
+	FORMAT_MESSAGE_FROM_SYSTEM | 
+	FORMAT_MESSAGE_ALLOCATE_BUFFER,
+	0,
+	hr,
+	MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ),
+	(WCHAR*) &str,
+	0,0);
+    
+    if (prepend == NULL)
+	swprintf(error_msg, L"%s", str);
+    else
+	swprintf(error_msg, L"%s%s", prepend, str);
+    
+    LocalFree(str);
+    wcstok(error_msg, L"\r\n");
 }
-
--- src/mpid/mm/src/mpiexec/mpirun/Translate_Error.h
+++ src/mpid/mm/src/mpiexec/mpirun/Translate_Error.h
@@ -1,19 +1,21 @@
-/**********************************************************************
-/* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-/* ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-/* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-/* PARTICULAR PURPOSE.
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
 /*
-/* Copyright (C) 1998  Brigham Young University.  All Rights Reserved.
-/*
-/* file: Translate_Error.h
-/*
-/**********************************************************************/
+ *
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#ifndef TRANSLATE_ERROR_H
+#define TRANSLATE_ERROR_H
+
 #ifdef WSOCK2_BEFORE_WINDOWS
 #include <winsock2.h>
 #endif
 #include <windows.h>
+
 void Translate_Error(int error, char *msg, char *prepend=NULL);
 void Translate_HRError(HRESULT hr, char *error_msg, char *prepend=NULL);
 void Translate_Error(int error, WCHAR *msg, WCHAR *prepend=NULL);
 void Translate_HRError(HRESULT hr, WCHAR *error_msg, WCHAR *prepend=NULL);
+
+#endif
