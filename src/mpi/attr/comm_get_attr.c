f306c71e4a90d43f1af45172458a7c47dce612c1
gropp <gropp>
2002-11-02 19:24:35 +0000

    Fixed Fortran predefined attributes

--- src/mpi/attr/comm_get_attr.c
+++ src/mpi/attr/comm_get_attr.c
@@ -48,6 +48,8 @@ int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *
     static const char FCNAME[] = "MPI_Comm_get_attr";
     int mpi_errno = MPI_SUCCESS;
     MPID_Comm *comm_ptr = NULL;
+    static PreDefined_attrs attr_copy;    /* Used to provide a copy of the
+					     predefined attributes */
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_COMM_GET_ATTR);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_COMM_GET_ATTR);
@@ -89,33 +91,65 @@ int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *
     if (HANDLE_GET_KIND(comm_keyval) == HANDLE_KIND_BUILTIN) {
 	int attr_idx = comm_keyval & 0x0000000f;
 	void **attr_val_p = (void **)attribute_val;
+#ifdef HAVE_FORTRAN_BINDING
+	MPI_Fint  *attr_int = (MPI_Fint *)attribute_val;
+#endif
 	*flag = 1;
 
 	/* FIXME: We could initialize some of these here; only tag_ub is 
 	 used in the error checking. */
+	/* 
+	 * The C versions of the attributes return the address of a 
+	 * *COPY* of the value (to prevent the user from changing it)
+	 * and the Fortran versions provide the actual value (as an Fint)
+	 */
+	attr_copy = MPIR_Process.attrs;
 	switch (attr_idx) {
 	case 1: /* TAG_UB */
-	    *attr_val_p = &MPIR_Process.attrs.tag_ub;
+	    *attr_val_p = &attr_copy.tag_ub;
 	    break;
-	case 2: /* HOST */
-	    *attr_val_p = &MPIR_Process.attrs.host;
+	case 3: /* HOST */
+	    *attr_val_p = &attr_copy.host;
 	    break;
-	case 3: /* IO */
-	    *attr_val_p = &MPIR_Process.attrs.io;
+	case 5: /* IO */
+	    *attr_val_p = &attr_copy.io;
 	    break;
-	case 4: /* WTIME */
-	    *attr_val_p = &MPIR_Process.attrs.wtime_is_global;
+	case 7: /* WTIME */
+	    *attr_val_p = &attr_copy.wtime_is_global;
 	    break;
-	case 5: /* UNIVERSE */
-	    *attr_val_p = &MPIR_Process.attrs.universe;
+	case 9: /* UNIVERSE */
+	    *attr_val_p = &attr_copy.universe;
 	    break;
-	case 6: /* LASTUSEDCODE */
-	    *attr_val_p = &MPIR_Process.attrs.lastusedcode;
+	case 11: /* LASTUSEDCODE */
+	    *attr_val_p = &attr_copy.lastusedcode;
 	    break;
-	case 7: /* APPNUM */
-	    *attr_val_p = &MPIR_Process.attrs.appnum;
+	case 13: /* APPNUM */
+	    *attr_val_p = &attr_copy.appnum;
+	    break;
+#ifdef HAVE_FORTRAN_BINDING
+	case 2: /* Fortran TAG_UB */
+	    *attr_int = attr_copy.tag_ub;
+	    break;
+	case 4: /* Fortran HOST */
+	    *attr_int = attr_copy.host;
+	    break;
+	case 6: /* Fortran IO */
+	    *attr_int = attr_copy.io;
+	    break;
+	case 8: /* Fortran WTIME */
+	    *attr_int = attr_copy.wtime_is_global;
+	    break;
+	case 10: /* UNIVERSE */
+	    *attr_int = attr_copy.universe;
+	    break;
+	case 12: /* LASTUSEDCODE */
+	    *attr_int = attr_copy.lastusedcode;
+	    break;
+	case 14: /* APPNUM */
+	    *attr_int = attr_copy.appnum;
 	    break;
 	}
+#endif
     }
     else {
 	MPID_Attribute *p = comm_ptr->attributes;
