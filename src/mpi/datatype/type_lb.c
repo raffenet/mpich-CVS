4f26f3838656372ba8f3d83ca493107ea21e111e
David <David>
2002-11-04 21:09:05 +0000

    altered error checking macros

--- src/mpi/datatype/type_lb.c
+++ src/mpi/datatype/type_lb.c
@@ -46,6 +46,7 @@ int MPI_Type_lb(MPI_Datatype datatype, MPI_Aint *displacement)
     static const char FCNAME[] = "MPI_Type_lb";
     int mpi_errno = MPI_SUCCESS;
     MPID_Datatype *datatype_ptr = NULL;
+    MPID_MPI_STATE_DECL(MPID_STATE_MPI_TYPE_LB);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_TYPE_LB);
     /* Get handles to MPI objects. */
@@ -54,9 +55,10 @@ int MPI_Type_lb(MPI_Datatype datatype, MPI_Aint *displacement)
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
+            if (mpi_errno) {
+                MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_TYPE_LB);
+                return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
             }
             /* Validate datatype_ptr */
             MPID_Datatype_valid_ptr( datatype_ptr, mpi_errno );
