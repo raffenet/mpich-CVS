ec32a65afdb3c853c9787cf26bbbc16cd881165b
ashton <ashton>
2002-11-13 06:24:24 +0000

    *** empty log message ***

--- src/mpid/mm/src/ib/ib_make_progress.c
+++ src/mpid/mm/src/ib/ib_make_progress.c
@@ -80,13 +80,14 @@ int ib_make_progress()
     {
     case OP_SEND:
 	g_num_send_posted--;
+	printf("s%d ", g_num_send_posted);
 	ib_handle_written(vc_ptr, mem_ptr, ibu_next_num_written());
 	/* put the send packet back in the pool */
 	BlockFree(vc_ptr->data.ib.info.m_allocator, mem_ptr);
 	break;
     case OP_RECEIVE:
 	g_num_receive_posted--;
-	printf("%d ", g_num_receive_posted);
+	printf("r%d ", g_num_receive_posted);
 	ib_handle_read(vc_ptr, mem_ptr, completion_data.bytes_num);
 	/* put the receive packet back in the pool */
 	BlockFree(vc_ptr->data.ib.info.m_allocator, mem_ptr);
