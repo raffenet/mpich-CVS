7f4398211c5c264a30b18d3b3fce16876fed9af6
toonen <toonen>
2002-11-13 18:34:01 +0000

    (test/mpi/pt2pt) minor enhancements to isendself

--- test/mpi/pt2pt/isendself.c
+++ test/mpi/pt2pt/isendself.c
@@ -6,16 +6,20 @@ int main( int argc, char *argv[] )
     int a[10], b[10], i;
     MPI_Status status;
     MPI_Request request;
+    int rank;
+    
 
     MPI_Init( 0, 0 );
 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
     for (i=0; i<10; i++) a[i] = i+1;
 
-    MPI_Isend( a, 0, MPI_INT, 0, 0, MPI_COMM_WORLD, &request );
+    MPI_Isend( a, 0, MPI_INT, rank, 0, MPI_COMM_WORLD, &request );
     MPI_Recv( b, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
 	      &status );
-    printf ("status = %d %d %d\n", status.MPI_SOURCE, status.MPI_TAG,
-	    status.count );
+    printf ("status = %d %d %d %d\n", status.MPI_SOURCE, status.MPI_TAG,
+	    status.MPI_ERROR, status.count );
     /* printf( "b[0] = %d\n", b[0] );*/
     MPI_Wait( &request, &status );
     MPI_Finalize();
