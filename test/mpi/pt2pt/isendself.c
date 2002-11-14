5a3ab701e3b1aa235f9af8cf32c4996e15adee7d
gropp <gropp>
2002-11-14 18:51:56 +0000

    *** empty log message ***

--- test/mpi/pt2pt/isendself.c
+++ test/mpi/pt2pt/isendself.c
@@ -18,10 +18,18 @@ int main( int argc, char *argv[] )
     MPI_Isend( a, 0, MPI_INT, rank, 0, MPI_COMM_WORLD, &request );
     MPI_Recv( b, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
 	      &status );
-    printf ("status = %d %d %d %d\n", status.MPI_SOURCE, status.MPI_TAG,
+    printf ("1 status = %d %d %d %d\n", status.MPI_SOURCE, status.MPI_TAG,
 	    status.MPI_ERROR, status.count );
     /* printf( "b[0] = %d\n", b[0] );*/
     MPI_Wait( &request, &status );
+
+    MPI_Isend( 0, 0, MPI_INT, rank, 0, MPI_COMM_WORLD, &request );
+    MPI_Recv( 0, 0, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+	      &status );
+    printf ("2 status = %d %d %d %d\n", status.MPI_SOURCE, status.MPI_TAG,
+	    status.MPI_ERROR, status.count );
+    MPI_Wait( &request, &status );
+
     MPI_Finalize();
     return 0;
 }
