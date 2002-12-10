355bad4ac55292eb20867ccfef148ae77511e88c
chan <chan>
2002-12-10 22:06:30 +0000

    Update version ID and history string

--- src/mpe2/src/slog2sdk/src/logformat/slog2/Const.java
+++ src/mpe2/src/slog2sdk/src/logformat/slog2/Const.java
@@ -11,7 +11,7 @@ package logformat.slog2;
 
 public class Const
 {
-           static final String  version_ID     = "SLOG 2.0.2";
+           static final String  version_ID     = "SLOG 2.0.3";
            static final byte    INVALID_byte   = Byte.MIN_VALUE;
            static final short   INVALID_short  = Short.MIN_VALUE;
            static final int     INVALID_int    = Integer.MIN_VALUE;
@@ -28,13 +28,17 @@ public class Const
     public static final int     LEAF_BYTESIZE  = 65536;
 
     public static final String  VERSION_HISTORY =
-                                  "Version SLOG 2.0.0's node employs "
-                                + "decreasing endtime ordering.\n"
-                                + "Version SLOG 2.0.1's node employs "
-                                + "increasing starttime ordering.\n"
-                                + "Version SLOG 2.0.2 added preview "
-                                + "data to shadow states.\n"
-                                + "Using 2.0.1 viewer for 2.0.0 logfile "
-                                + "messes up state nesting stack!\n"
-                                + "2.0.2 viewer cannot read 2.0.1 logfile\n";
+                                  "- Version SLOG 2.0.0's node employs\n"
+                                + "  decreasing endtime ordering.\n"
+                                + "- Version SLOG 2.0.1's node employs\n"
+                                + "  increasing starttime ordering.\n"
+                                + "- Version SLOG 2.0.2 added preview data\n"
+                                + "  in legend order to shadow states.\n"
+                                + "- Version SLOG 2.0.3 changed preview data\n"
+                                + "  from legend order to weight order.\n"
+                                + "- 2.0.1 viewer draws 2.0.0 logfile's\n"
+                                + "  state nesting stack incorrectly!\n"
+                                + "- 2.0.2 viewer cannot read 2.0.1 logfile\n"
+                                + "- 2.0.3 viewer draws 2.0.2 logfile's\n"
+                                + "  preview weight incorrectly\n";
 }
