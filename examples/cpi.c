21c81c0094edd81cd48ba8d567838bff69874b3d
lusk <lusk>
2002-12-18 21:00:44 +0000

    fixed cpi.c to be cleanly non-interactive

--- examples/cpi.c
+++ examples/cpi.c
@@ -23,48 +23,36 @@ int main(int argc,char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Get_processor_name(processor_name,&namelen);
 
-    fprintf(stdout,"Process %d of %d on %s\n",
+    fprintf(stdout,"Process %d of %d is on %s\n",
 	    myid, numprocs, processor_name);
+    fflush(stdout);
 
-    n = 0;
-    while (!done)
+    n = 10000;			/* default # of rectangles */
+    if (myid == 0)
+	startwtime = MPI_Wtime();
+
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    h   = 1.0 / (double) n;
+    sum = 0.0;
+    /* A slightly better approach starts from large i and works back */
+    for (i = myid + 1; i <= n; i += numprocs)
     {
-        if (myid == 0)
-        {
-            printf("Enter the number of intervals: (0 quits) ");
-            scanf("%d",&n);
-
-	    if (n==0) n=10000; else n=0;
-
-	    startwtime = MPI_Wtime();
-        }
-        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if (n == 0)
-            done = 1;
-        else
-        {
-            h   = 1.0 / (double) n;
-            sum = 0.0;
-	    /* A slightly better approach starts from large i and works back */
-            for (i = myid + 1; i <= n; i += numprocs)
-            {
-                x = h * ((double)i - 0.5);
-                sum += f(x);
-            }
-            mypi = h * sum;
-
-            MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-            if (myid == 0)
-	    {
-                printf("pi is approximately %.16f, Error is %.16f\n",
-                       pi, fabs(pi - PI25DT));
-		endwtime = MPI_Wtime();
-		printf("wall clock time = %f\n", endwtime-startwtime);	       
-		fflush( stdout );
-	    }
-        }
+	x = h * ((double)i - 0.5);
+	sum += f(x);
     }
+    mypi = h * sum;
+
+    MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myid == 0) {
+	endwtime = MPI_Wtime();
+	printf("pi is approximately %.16f, Error is %.16f\n",
+	       pi, fabs(pi - PI25DT));
+	printf("wall clock time = %f\n", endwtime-startwtime);	       
+	fflush(stdout);
+    }
+
     MPI_Finalize();
     return 0;
 }
