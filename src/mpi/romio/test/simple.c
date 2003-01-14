a2ced63e86d4a80ee424b0f97934d51d20f7ad41
gropp <gropp>
2003-01-14 14:25:57 +0000

    Added copyright statements to ROMIO test programs

--- src/mpi/romio/test/simple.c
+++ src/mpi/romio/test/simple.c
@@ -1,4 +1,8 @@
 /* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
 #include "mpi.h"
 #include "mpio.h"  /* not necessary with MPICH 1.1.1 or HPMPI 1.4 */
 #include <stdio.h>
