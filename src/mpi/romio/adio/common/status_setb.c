b0ec88ce9940e43cc5822c8f8f5f02908fe06f06
robl <robl>
2003-01-21 22:37:40 +0000

    commenting a bit better what is left to do

--- src/mpi/romio/adio/common/status_setb.c
+++ src/mpi/romio/adio/common/status_setb.c
@@ -11,9 +11,14 @@
 
 #if defined(MPICH2)
 /* Not quite correct, but much closer for MPI2 */
+/* TODO: still needs to handle partial datatypes and situations where the mpi
+ * implementation fills status with something other than bytes (globus2 might
+ * do this) */
 int MPIR_Status_set_bytes(MPI_Status *status, MPI_Datatype datatype, 
 			  int nbytes)
 {
+    /* it's ok that ROMIO stores number-of-bytes in status, not 
+     * count-of-copies, as long as MPI_GET_COUNT knows what to do */
     if (status != MPI_STATUS_IGNORE)
         MPI_Status_set_elements(status, MPI_BYTE, nbytes);
     return MPI_SUCCESS;
