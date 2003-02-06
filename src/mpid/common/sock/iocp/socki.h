cc9d511ac458f01f3e593ccd59b22c8667d77f1d
toonen <toonen>
2003-02-06 18:53:03 +0000

    (sock/iocp) renamed sockimpl.h to socki.h; moved describing of states for interface functions to top-level sock directory

---
+++ src/mpid/common/sock/iocp/socki.h
@@ -0,0 +1,19 @@
+#include <winsock2.h>
+
+#define SOCK_IOV             WSABUF
+#define SOCK_IOV_LEN         len
+#define SOCK_IOV_BUF         buf
+#define SOCK_IOV_MAXLEN      16
+#define SOCK_INFINITE_TIME   INFINITE
+#define SOCK_INVALID_SOCK    NULL
+#define SOCK_SIZE_MAX	     INT_MAX
+
+typedef HANDLE sock_set_t;
+typedef struct sock_state_t * sock_t;
+typedef int sock_size_t;
+
+#define inline __inline
+
+#define SOCKI_STATE_LIST \
+MPID_STATE_SOCK_EASY_RECEIVE, \
+MPID_STATE_SOCK_EASY_SEND,
