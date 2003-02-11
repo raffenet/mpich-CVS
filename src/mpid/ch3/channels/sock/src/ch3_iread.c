ddbdaf220a48a9a3e3b4f2070c15ab081afbd397
toonen <toonen>
2003-02-11 20:18:28 +0000

    (ch3:sock) modified MPIDI_CH3_iRead() to aggressively read

--- src/mpid/ch3/channels/sock/src/ch3_iread.c
+++ src/mpid/ch3/channels/sock/src/ch3_iread.c
@@ -13,15 +13,46 @@
 #define FUNCNAME MPIDI_CH3_iRead
 #undef FCNAME
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
-void MPIDI_CH3_iRead(MPIDI_VC * vc, MPID_Request * req)
+void MPIDI_CH3_iRead(MPIDI_VC * vc, MPID_Request * rreq)
 {
+    int sock_errno;
+    sock_size_t nb;
     MPIDI_STATE_DECL(MPID_STATE_MPIDI_CH3_IREAD);
 
     MPIDI_FUNC_ENTER(MPID_STATE_MPIDI_CH3_IREAD);
     assert(vc->sc.state == MPIDI_CH3I_VC_STATE_CONNECTED);
-    req->sc.iov_offset = 0;
-    
-    MPIDI_CH3I_VC_post_read(vc, req);
 
+    sock_errno = sock_readv(vc->sc.sock, rreq->ch3.iov, rreq->ch3.iov_count, &nb);
+    if (sock_errno == SOCK_SUCCESS)
+    {
+	rreq->sc.iov_offset = 0;
+	while (rreq->sc.iov_offset < rreq->ch3.iov_count)
+	{
+	    if (rreq->ch3.iov[rreq->sc.iov_offset].MPID_IOV_LEN <= nb)
+	    {
+		nb -= rreq->ch3.iov[rreq->sc.iov_offset].MPID_IOV_LEN;
+		rreq->sc.iov_offset += 1;
+	    }
+	    else
+	    {
+		rreq->ch3.iov[rreq->sc.iov_offset].MPID_IOV_BUF = (char *) rreq->ch3.iov[rreq->sc.iov_offset].MPID_IOV_BUF + nb;
+		rreq->ch3.iov[rreq->sc.iov_offset].MPID_IOV_LEN -= nb;
+		MPIDI_CH3I_VC_post_read(vc, rreq);
+		goto fn_exit;
+	    }
+	}
+
+	/* FIXME: excessive recursion... */
+	MPIDI_CH3U_Handle_recv_req(vc, rreq);
+    }
+    else
+    {
+	int mpi_errno;
+
+	mpi_errno = MPIDI_CH3I_sock_errno_to_mpi_errno(sock_errno);
+	MPID_Abort(NULL, mpi_errno);
+    }
+
+  fn_exit:
     MPIDI_FUNC_EXIT(MPID_STATE_MPIDI_CH3_IREAD);
 }
