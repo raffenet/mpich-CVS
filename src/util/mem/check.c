4947daa5216ba6f9e2ce711c982efa9e98c46f12
gropp <gropp>
2003-02-17 14:06:42 +0000

    *** empty log message ***

--- src/util/mem/check.c
+++ src/util/mem/check.c
@@ -1,3 +1,10 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  $Id$
+ *
+ *  (C) 2002 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 #include <stdio.h>
 
 /* This is a test program, so we allow printf */
