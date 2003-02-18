0f72137a553fd23092f98c93bc9a4c824edab4e7
gropp <gropp>
2003-02-18 13:55:25 +0000

    Changed error message for intracomm expected

--- src/mpi/romio/mpi-io/open.c
+++ src/mpi/romio/mpi-io/open.c
@@ -75,7 +75,7 @@ int MPI_File_open(MPI_Comm comm, char *filename, int amode,
     if (flag) {
 #ifdef MPICH2
 			error_code = MPIR_Err_create_code(MPI_ERR_COMM, 
-							"**intercom", "**intercom");
+							"**commnotintra", "**commnotintra");
 			return MPIR_Err_return_file(MPI_FILE_NULL, myname, error_code);
 #elif PRINT_ERR_MSG
 	FPRINTF(stderr, "MPI_File_open: Intercommunicator cannot be passed to MPI_File_open\n");
