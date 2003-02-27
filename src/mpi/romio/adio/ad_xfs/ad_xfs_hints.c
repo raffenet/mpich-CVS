c9b6ab1d029d68211420a5808a5d2b3cb3892265
robl <robl>
2003-02-27 02:15:53 +0000

    protect all calls to MPI_Info_set in an attempt to make the nightly build on IRIX shut up.

--- src/mpi/romio/adio/ad_xfs/ad_xfs_hints.c
+++ src/mpi/romio/adio/ad_xfs/ad_xfs_hints.c
@@ -14,11 +14,15 @@ void ADIOI_XFS_SetInfo(ADIO_File fd, MPI_Info users_info, int *error_code)
     char *value;
     int flag;
 
-    if (!(fd->info)) MPI_Info_create(&(fd->info));
+    if (fd->info == MPI_INFO_NULL) MPI_Info_create(&(fd->info));
 
-    MPI_Info_set(fd->info, "direct_read", "false");
-    MPI_Info_set(fd->info, "direct_write", "false");
-    fd->direct_read = fd->direct_write = 0;
+    /* the nightly builds say somthing is calling MPI_Info_set w/ a null info,
+     * so protect the calls to MPI_Info_set */
+    if (fd->info != MPI_INFO_NULL ) {
+	    MPI_Info_set(fd->info, "direct_read", "false");
+	    MPI_Info_set(fd->info, "direct_write", "false");
+	    fd->direct_read = fd->direct_write = 0;
+    }
 	
     /* has user specified values for keys "direct_read" and "direct wirte"? */
     if (users_info != MPI_INFO_NULL) {
