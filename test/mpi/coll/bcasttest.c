6f8bec074ee0025f8426ede197c2fcfd3211c398
David <David>
2003-02-28 06:50:47 +0000

    added three message sizes to bcasttest

--- test/mpi/coll/bcasttest.c
+++ test/mpi/coll/bcasttest.c
@@ -2,39 +2,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define NINTS     1123
 #define ROOT      0
 #define NUM_REPS  5
+#define NUM_SIZES 3
 
 int main( int argc, char **argv)
 {
     int *buf;
-    int i, rank, reps;
+    int i, rank, reps, n;
+    int sizes[NUM_SIZES] = { 100, 64*1024, 128*1024 };
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-    buf = (int *) malloc(NINTS*sizeof(int));
-    
-    for (reps=0; reps < NUM_REPS; reps++)
+    buf = (int *) malloc(sizes[2]*sizeof(int));
+
+    for (n=0; n<NUM_REPS; n++)
     {
 	if (rank == ROOT)
 	{
-	    for (i=0; i<NINTS; i++) 
-		buf[i] = i;
+	    printf("bcasting %d bytes\n", sizes[n]);
+	    fflush(stdout);
 	}
-	else
+	for (reps=0; reps < NUM_REPS; reps++)
 	{
-	    for (i=0; i<NINTS; i++)
-		buf[i] = -1;
+	    if (rank == ROOT)
+	    {
+		for (i=0; i<sizes[n]; i++) 
+		    buf[i] = i;
+		printf("%d,", reps);
+		fflush(stdout);
+	    }
+	    else
+	    {
+		for (i=0; i<sizes[n]; i++)
+		    buf[i] = -1;
+	    }
+
+	    MPI_Bcast(buf, sizes[n], MPI_INT, ROOT, MPI_COMM_WORLD); 
+
+	    for (i=0; i<sizes[n]; i++)
+	    {
+		if (buf[i] != i)
+		    printf("Error: Rank=%d, i=%d, buf[i]=%d\n", rank, i, buf[i]);
+	    }
 	}
-	
-	MPI_Bcast(buf, NINTS, MPI_INT, ROOT, MPI_COMM_WORLD); 
-	
-	for (i=0; i<NINTS; i++)
+	if (rank == ROOT)
 	{
-	    if (buf[i] != i)
-		printf("Error: Rank=%d, i=%d, buf[i]=%d\n", rank, i, buf[i]);
+	    printf("\n");
+	    fflush(stdout);
 	}
     }
     
@@ -43,7 +59,9 @@ int main( int argc, char **argv)
 	printf(" No Errors\n");
 
     fflush(stdout);
-    
+
+    free(buf);
+
     MPI_Finalize();
     return 0;
 }
