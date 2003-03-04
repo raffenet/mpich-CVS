e08b178ac318231bc0cb955aa2772f26c06cddc9
David <David>
2003-03-04 07:32:56 +0000

    added -novalidate option

--- test/mpi/coll/bcasttest.c
+++ test/mpi/coll/bcasttest.c
@@ -1,6 +1,7 @@
 #include "mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define ROOT      0
 #define NUM_REPS  5
@@ -10,47 +11,54 @@ int main( int argc, char **argv)
 {
     int *buf;
     int i, rank, reps, n;
+    int bVerify = 1;
     int sizes[NUM_SIZES] = { 100, 64*1024, 128*1024 };
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
+    if (argc > 1)
+    {
+	if (strcmp(argv[1], "-novalidate") == 0 || strcmp(argv[1], "-noverify") == 0)
+	    bVerify = 0;
+    }
+
     buf = (int *) malloc(sizes[2]*sizeof(int));
+    memset(buf, 0, sizes[2]*sizeof(int));
 
     for (n=0; n<NUM_SIZES; n++)
     {
 	if (rank == ROOT)
 	{
-	    printf("bcasting %d bytes\n", sizes[n]);
+	    printf("bcasting %d MPI_INTs %d times\n", sizes[n], NUM_REPS);
 	    fflush(stdout);
 	}
 	for (reps=0; reps < NUM_REPS; reps++)
 	{
-	    if (rank == ROOT)
+	    if (bVerify)
 	    {
-		for (i=0; i<sizes[n]; i++) 
-		    buf[i] = i;
-		printf("%d,", reps);
-		fflush(stdout);
-	    }
-	    else
-	    {
-		for (i=0; i<sizes[n]; i++)
-		    buf[i] = -1;
+		if (rank == ROOT)
+		{
+		    for (i=0; i<sizes[n]; i++) 
+			buf[i] = i;
+		}
+		else
+		{
+		    for (i=0; i<sizes[n]; i++)
+			buf[i] = -1;
+		}
 	    }
 
 	    MPI_Bcast(buf, sizes[n], MPI_INT, ROOT, MPI_COMM_WORLD); 
 
-	    for (i=0; i<sizes[n]; i++)
+	    if (bVerify)
 	    {
-		if (buf[i] != i)
-		    printf("Error: Rank=%d, i=%d, buf[i]=%d\n", rank, i, buf[i]);
+		for (i=0; i<sizes[n]; i++)
+		{
+		    if (buf[i] != i)
+			printf("Error: Rank=%d, i=%d, buf[i]=%d\n", rank, i, buf[i]);
+		}
 	    }
-	}
-	if (rank == ROOT)
-	{
-	    printf("\n");
-	    fflush(stdout);
 	}
     }
     
