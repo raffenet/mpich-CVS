bee72522a4adc186b57e2bbcd735172c1966e854
gropp <gropp>
2003-03-08 17:26:27 +0000

    Added the implementation of the name server functions

--- src/mpi/spawn/unpublish_name.c
+++ src/mpi/spawn/unpublish_name.c
@@ -6,6 +6,7 @@
  */
 
 #include "mpiimpl.h"
+#include "namepub.h"
 
 /* -- Begin Profiling Symbol Block for routine MPI_Unpublish_name */
 #if defined(HAVE_PRAGMA_WEAK)
@@ -56,12 +57,11 @@ int MPI_Unpublish_name(char *service_name, MPI_Info info, char *port_name)
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
             /* Validate info_ptr */
             MPID_Info_valid_ptr( info_ptr, mpi_errno );
+	    MPIR_ERRTEST_ARGNULL( service_name, "service_name", mpi_errno );
+	    MPIR_ERRTEST_ARGNULL( port_name, "port_name", mpi_errno );
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_UNPUBLISH_NAME);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
@@ -71,6 +71,20 @@ int MPI_Unpublish_name(char *service_name, MPI_Info info, char *port_name)
     }
 #   endif /* HAVE_ERROR_CHECKING */
 
+#ifdef HAVE_NAMEPUB_SERVICE
+    if (!MPIR_Namepub) {
+	mpi_errno = MPID_NS_Create( info_ptr, &MPIR_Namepub );
+    }
+    if (!mpi_errno) 
+	mpi_errno = MPID_NS_Unpublish( MPIR_Namepub, info_ptr, 
+				       (const char *)service_name );
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_UNPUBLISH_NAME);
+    if (!mpi_errno)
+	return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
     return MPI_SUCCESS;
+#else
+    MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_UNPUBLISH_NAME);
+    mpi_errno = MPIR_Err_create( MPI_ERR_OTHER, "**nonamepub", 0 );
+    return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+#endif    
 }
