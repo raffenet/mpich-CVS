8a68b98dc6bc9b1d2b7ea371598f2d1c496b0dd4
David <David>
2003-03-16 06:35:46 +0000

    compressed packet dbg print into one printf

--- src/mpid/ch3/src/mpidi_printf.c
+++ src/mpid/ch3/src/mpidi_printf.c
@@ -44,73 +44,170 @@ void MPIDI_DBG_Print_packet(MPIDI_CH3_Pkt_t *pkt)
     switch(pkt->type)
     {
     case MPIDI_CH3_PKT_EAGER_SEND:
-	MPIU_DBG_PRINTF((" type ......... EAGER_SEND\n"));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->eager_send.sender_req_id));
-	MPIU_DBG_PRINTF((" context_id ... %d\n", pkt->eager_send.match.context_id));
-	MPIU_DBG_PRINTF((" data_sz ...... %d\n", pkt->eager_send.data_sz));
-	MPIU_DBG_PRINTF((" tag .......... %d\n", pkt->eager_send.match.tag));
-	MPIU_DBG_PRINTF((" rank ......... %d\n", pkt->eager_send.match.rank));
 #ifdef MPID_USE_SEQUENCE_NUMBERS
-	MPIU_DBG_PRINTF((" seqnum ....... %d\n", pkt->eager_send.seqnum));
+	MPIU_DBG_PRINTF((
+	" type ......... EAGER_SEND\n"
+	" sender_reqid . 0x%08x\n"
+	" context_id ... %d\n"
+	" data_sz ...... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" seqnum ....... %d\n",
+	pkt->eager_send.sender_req_id,
+	pkt->eager_send.match.context_id,
+	pkt->eager_send.data_sz,
+	pkt->eager_send.match.tag,
+	pkt->eager_send.match.rank,
+	pkt->eager_send.seqnum
+	));
+#else
+	MPIU_DBG_PRINTF((
+	" type ......... EAGER_SEND\n"
+	" sender_reqid . 0x%08x\n"
+	" context_id ... %d\n"
+	" data_sz ...... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n",
+	pkt->eager_send.sender_req_id,
+	pkt->eager_send.match.context_id,
+	pkt->eager_send.data_sz,
+	pkt->eager_send.match.tag,
+	pkt->eager_send.match.rank
+	));
 #endif
 	break;
     case MPIDI_CH3_PKT_EAGER_SYNC_SEND:
-	MPIU_DBG_PRINTF((" type ......... EAGER_SYNC_SEND\n"));
-	MPIU_DBG_PRINTF((" context_id ... %d\n", pkt->eager_sync_send.match.context_id));
-	MPIU_DBG_PRINTF((" tag .......... %d\n", pkt->eager_sync_send.match.tag));
-	MPIU_DBG_PRINTF((" rank ......... %d\n", pkt->eager_sync_send.match.rank));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->eager_sync_send.sender_req_id));
-	MPIU_DBG_PRINTF((" data_sz ...... %d\n", pkt->eager_sync_send.data_sz));
 #ifdef MPID_USE_SEQUENCE_NUMBERS
-	MPIU_DBG_PRINTF((" seqnum ....... %d\n", pkt->eager_sync_send.seqnum));
+	MPIU_DBG_PRINTF((
+	" type ......... EAGER_SYNC_SEND\n"
+	" context_id ... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" sender_reqid . 0x%08x\n"
+	" data_sz ...... %d\n"
+	" seqnum ....... %d\n",
+	pkt->eager_sync_send.match.context_id,
+	pkt->eager_sync_send.match.tag,
+	pkt->eager_sync_send.match.rank,
+	pkt->eager_sync_send.sender_req_id,
+	pkt->eager_sync_send.data_sz,
+	pkt->eager_sync_send.seqnum
+	));
+#else
+	MPIU_DBG_PRINTF((
+	" type ......... EAGER_SYNC_SEND\n"
+	" context_id ... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" sender_reqid . 0x%08x\n"
+	" data_sz ...... %d\n",
+	pkt->eager_sync_send.match.context_id,
+	pkt->eager_sync_send.match.tag,
+	pkt->eager_sync_send.match.rank,
+	pkt->eager_sync_send.sender_req_id,
+	pkt->eager_sync_send.data_sz
+	));
 #endif
 	break;
     case MPIDI_CH3_PKT_EAGER_SYNC_ACK:
-	MPIU_DBG_PRINTF((" type ......... EAGER_SYNC_ACK\n"));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->eager_sync_ack.sender_req_id));
+	MPIU_DBG_PRINTF((" type ......... EAGER_SYNC_ACK\n sender_reqid . 0x%08x\n", pkt->eager_sync_ack.sender_req_id));
 	break;
     case MPIDI_CH3_PKT_READY_SEND:
-	MPIU_DBG_PRINTF((" type ......... READY_SEND\n"));
-	MPIU_DBG_PRINTF((" context_id ... %d\n", pkt->ready_send.match.context_id));
-	MPIU_DBG_PRINTF((" tag .......... %d\n", pkt->ready_send.match.tag));
-	MPIU_DBG_PRINTF((" rank ......... %d\n", pkt->ready_send.match.rank));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->ready_send.sender_req_id));
-	MPIU_DBG_PRINTF((" data_sz ...... %d\n", pkt->ready_send.data_sz));
 #ifdef MPID_USE_SEQUENCE_NUMBERS
-	MPIU_DBG_PRINTF((" seqnum ....... %d\n", pkt->ready_send.seqnum));
+	MPIU_DBG_PRINTF((
+	" type ......... READY_SEND\n"
+	" context_id ... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" sender_reqid . 0x%08x\n"
+	" data_sz ...... %d\n"
+	" seqnum ....... %d\n",
+	pkt->ready_send.match.context_id,
+	pkt->ready_send.match.tag,
+	pkt->ready_send.match.rank,
+	pkt->ready_send.sender_req_id,
+	pkt->ready_send.data_sz,
+	pkt->ready_send.seqnum
+	));
+#else
+	MPIU_DBG_PRINTF((
+	" type ......... READY_SEND\n"
+	" context_id ... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" sender_reqid . 0x%08x\n"
+	" data_sz ...... %d\n",
+	pkt->ready_send.match.context_id,
+	pkt->ready_send.match.tag,
+	pkt->ready_send.match.rank,
+	pkt->ready_send.sender_req_id,
+	pkt->ready_send.data_sz
+	));
 #endif
 	break;
     case MPIDI_CH3_PKT_RNDV_REQ_TO_SEND:
-	MPIU_DBG_PRINTF((" type ......... REQ_TO_SEND\n"));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->rndv_req_to_send.sender_req_id));
-	MPIU_DBG_PRINTF((" context_id ... %d\n", pkt->rndv_req_to_send.match.context_id));
-	MPIU_DBG_PRINTF((" data_sz ...... %d\n", pkt->rndv_req_to_send.data_sz));
-	MPIU_DBG_PRINTF((" tag .......... %d\n", pkt->rndv_req_to_send.match.tag));
-	MPIU_DBG_PRINTF((" rank ......... %d\n", pkt->rndv_req_to_send.match.rank));
 #ifdef MPID_USE_SEQUENCE_NUMBERS
-	MPIU_DBG_PRINTF((" seqnum ....... %d\n", pkt->rndv_req_to_send.seqnum));
+	MPIU_DBG_PRINTF((
+	" type ......... REQ_TO_SEND\n"
+	" sender_reqid . 0x%08x\n"
+	" context_id ... %d\n"
+	" data_sz ...... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" seqnum ....... %d\n",
+	pkt->rndv_req_to_send.sender_req_id,
+	pkt->rndv_req_to_send.match.context_id,
+	pkt->rndv_req_to_send.data_sz,
+	pkt->rndv_req_to_send.match.tag,
+	pkt->rndv_req_to_send.match.rank,
+	pkt->rndv_req_to_send.seqnum
+	));
+#else
+	MPIU_DBG_PRINTF((
+	" type ......... REQ_TO_SEND\n"
+	" sender_reqid . 0x%08x\n"
+	" context_id ... %d\n"
+	" data_sz ...... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n",
+	pkt->rndv_req_to_send.sender_req_id,
+	pkt->rndv_req_to_send.match.context_id,
+	pkt->rndv_req_to_send.data_sz,
+	pkt->rndv_req_to_send.match.tag,
+	pkt->rndv_req_to_send.match.rank
+	));
 #endif
 	break;
     case MPIDI_CH3_PKT_RNDV_CLR_TO_SEND:
-	MPIU_DBG_PRINTF((" type ......... CLR_TO_SEND\n"));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->rndv_clr_to_send.sender_req_id));
-	MPIU_DBG_PRINTF((" recvr_reqid .. 0x%08x\n", pkt->rndv_clr_to_send.receiver_req_id));
+	MPIU_DBG_PRINTF((
+	" type ......... CLR_TO_SEND\n"
+	" sender_reqid . 0x%08x\n"
+	" recvr_reqid .. 0x%08x\n",
+	pkt->rndv_clr_to_send.sender_req_id,
+	pkt->rndv_clr_to_send.receiver_req_id));
 	break;
     case MPIDI_CH3_PKT_RNDV_SEND:
-	MPIU_DBG_PRINTF((" type ......... RNDV_SEND\n"));
-	MPIU_DBG_PRINTF((" recvr_reqid .. 0x%08x\n", pkt->rndv_send.receiver_req_id));
+	MPIU_DBG_PRINTF((" type ......... RNDV_SEND\n recvr_reqid .. 0x%08x\n", pkt->rndv_send.receiver_req_id));
 	break;
     case MPIDI_CH3_PKT_CANCEL_SEND_REQ:
-	MPIU_DBG_PRINTF((" type ......... CANCEL_SEND\n"));
-	MPIU_DBG_PRINTF((" context_id ... %d\n", pkt->cancel_send_req.match.context_id));
-	MPIU_DBG_PRINTF((" tag .......... %d\n", pkt->cancel_send_req.match.tag));
-	MPIU_DBG_PRINTF((" rank ......... %d\n", pkt->cancel_send_req.match.rank));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->cancel_send_req.sender_req_id));
+	MPIU_DBG_PRINTF((
+	" type ......... CANCEL_SEND\n"
+	" context_id ... %d\n"
+	" tag .......... %d\n"
+	" rank ......... %d\n"
+	" sender_reqid . 0x%08x\n",
+	pkt->cancel_send_req.match.context_id,
+	pkt->cancel_send_req.match.tag,
+	pkt->cancel_send_req.match.rank,
+	pkt->cancel_send_req.sender_req_id));
 	break;
     case MPIDI_CH3_PKT_CANCEL_SEND_RESP:
-	MPIU_DBG_PRINTF((" type ......... CANCEL_SEND_RESP\n"));
-	MPIU_DBG_PRINTF((" sender_reqid . 0x%08x\n", pkt->cancel_send_resp.sender_req_id));
-	MPIU_DBG_PRINTF((" ack .......... %d\n", pkt->cancel_send_resp.ack));
+	MPIU_DBG_PRINTF((
+	" type ......... CANCEL_SEND_RESP\n"
+	" sender_reqid . 0x%08x\n"
+	" ack .......... %d\n",
+	pkt->cancel_send_resp.sender_req_id,
+	pkt->cancel_send_resp.ack));
 	break;
     case MPIDI_CH3_PKT_PUT:
 	MPIU_DBG_PRINTF((" PUT\n"));
