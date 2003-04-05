d4e29ae5b0478d04895b334889110a3a977167ce
David <David>
2003-04-05 08:09:19 +0000

    romio: a few more error code fixes

--- src/mpi/romio/adio/common/ad_seek.c
+++ src/mpi/romio/adio/common/ad_seek.c
@@ -94,9 +94,8 @@ ADIO_Offset ADIOI_GEN_SeekIndividual(ADIO_File fd, ADIO_Offset offset,
 
     if (err == -1) {
 #ifdef MPICH2
-			*error_code = MPIR_Err_create_code(MPI_ERR_IO, "**io",
-							"**io %s", strerror(errno));
-			MPIR_Err_return_file(MPI_FILE_NULL, myname, *error_code);
+	*error_code = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, myname, MPI_ERR_IO, "**io",
+	    "**io %s", strerror(errno));
 #elif defined(PRINT_ERR_MSG)
 			*error_code = MPI_ERR_UNKNOWN;
 #else /* MPICH-1 */
