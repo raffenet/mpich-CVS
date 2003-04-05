07823f96e1b065baddeaeb3e12927f430a3ee902
David <David>
2003-04-05 18:00:35 +0000

    *** empty log message ***

--- src/mpi/romio/adio/ad_sfs/ad_sfs_fcntl.c
+++ src/mpi/romio/adio/ad_sfs/ad_sfs_fcntl.c
@@ -134,9 +134,8 @@ void ADIOI_SFS_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t *fcntl_struct, int *er
 			    &status, error_code);
 	    if (*error_code != MPI_SUCCESS) {
 #ifdef MPICH2
-				*error_code = MPIR_Err_create_code(MPI_ERR_IO, "**io",
-								"**io %s", strerror(errno));
-				MPIR_Err_return_file(fd, myname, *error_code);
+		*error_code = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, myname, MPI_ERR_IO, "**io",
+		    "**io %s", strerror(errno));
 #elif defined(PRINT_ERR_MSG)
 		FPRINTF(stderr, "ADIOI_SFS_Fcntl: To preallocate disk space, ROMIO needs to read the file and write it back, but is unable to read the file. Please give the file read permission and open it with MPI_MODE_RDWR.\n");
 		MPI_Abort(MPI_COMM_WORLD, 1);
