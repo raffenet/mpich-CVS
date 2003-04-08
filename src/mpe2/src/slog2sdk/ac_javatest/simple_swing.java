06e7f860ab4fc9397bbc0e5e297f528a7d83cd23
chan <chan>
2003-04-08 05:15:30 +0000

    Use JPanel instead of JFrame to test the availability of Swing library

--- src/mpe2/src/slog2sdk/ac_javatest/simple_swing.java
+++ src/mpe2/src/slog2sdk/ac_javatest/simple_swing.java
@@ -4,7 +4,8 @@ public class simple_swing
 {
     public static void main( String args[] )
     {
-        JFrame mainframe = new JFrame();
+        // JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
         System.exit( 0 );
     }
 }
