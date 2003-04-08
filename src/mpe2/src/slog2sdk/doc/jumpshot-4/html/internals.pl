c2a3ec8b83e3e1c6bfc67a70a6e33ec71ec65f64
chan <chan>
2003-04-08 04:42:08 +0000

    Added the content of Jumpshot-4 user's guide

---
+++ src/mpe2/src/slog2sdk/doc/jumpshot-4/html/internals.pl
@@ -0,0 +1,118 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:timeline_popup/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_zoom_mode/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_time/;
+$ref_files{$key} = "$dir".q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_popup/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:yaxis_label_panel_simple/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_color_chooser/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_minus_cursor/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_duration/;
+$ref_files{$key} = "$dir".q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:main/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:preference_pview_state/;
+$ref_files{$key} = "$dir".q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_sort_order/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:Zoomable-and-Scrollable/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_plus_cursor/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_preview_state/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_hand_mode/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_popup_finer/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_checkbox_ops/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview_zoomed/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_real_primitive/;
+$ref_files{$key} = "$dir".q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_0/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_1/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_close_cursor/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_2/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_3/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_4/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_open_cursor/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:row_adjustment_panel/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ src/mpe2/src/slog2sdk/doc/jumpshot-4/html/labels.pl
@@ -0,0 +1,125 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:timeline_popup/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_zoom_mode/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_time/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_popup/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:yaxis_label_panel_simple/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_color_chooser/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_minus_cursor/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_duration/;
+$external_labels{$key} = "$URL/" . q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:main/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:preference_pview_state/;
+$external_labels{$key} = "$URL/" . q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_sort_order/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:Zoomable-and-Scrollable/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_plus_cursor/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_preview_state/;
+$external_labels{$key} = "$URL/" . q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_hand_mode/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_popup_finer/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_checkbox_ops/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview_zoomed/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_real_primitive/;
+$external_labels{$key} = "$URL/" . q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_0/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_1/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_close_cursor/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_2/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_3/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_4/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_open_cursor/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:row_adjustment_panel/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.70)
+# labels from external_latex_labels array.
+
+
+1;
+
