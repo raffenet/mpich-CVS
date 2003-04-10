a0181e0839bb2a4fd72e232720499753ea2d5fe4
gropp <gropp>
2003-04-10 20:19:04 +0000

    Added 0 count indexed type to test

--- test/mpi/datatype/zeroparms.c
+++ test/mpi/datatype/zeroparms.c
@@ -5,11 +5,23 @@
 int main( int argc, char *argv[] )
 {
     MPI_Datatype newtype;
+    int b[1], d[1];
 
     MPI_Init( 0, 0 );
 
     MPI_Type_hvector( 0, 1, 10, MPI_DOUBLE, &newtype );
+
     MPI_Type_commit( &newtype );
 
+    MPI_Type_indexed( 0, b, d, MPI_DOUBLE, &newtype );
+
+    MPI_Type_commit( &newtype );
+
+    MPI_Sendrecv( b, 1, newtype, 0, 0, 
+		  d, 0, newtype, 0, 0, 
+		  MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+
     MPI_Finalize();
+
+    return 0;
 }
