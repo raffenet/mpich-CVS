1164e0d0e78af7e4e0e00b5a60872b25e73433ca
chan <chan>
2003-04-18 06:00:16 +0000

    updated

--- src/mpe2/src/slog2sdk/doc/jumpshot-4/html/internals.pl
+++ src/mpe2/src/slog2sdk/doc/jumpshot-4/html/internals.pl
@@ -3,115 +3,115 @@
 
 
 $key = q/fig:timeline_popup/;
-$ref_files{$key} = "$dir".q|node9.html|; 
+$ref_files{$key} = "$dir".q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:mouse_zoom_mode/;
-$ref_files{$key} = "$dir".q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_search_preview/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_infobox_time/;
-$ref_files{$key} = "$dir".q|node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_popup/;
-$ref_files{$key} = "$dir".q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:yaxis_label_panel_simple/;
-$ref_files{$key} = "$dir".q|node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_color_chooser/;
-$ref_files{$key} = "$dir".q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:zoom_minus_cursor/;
-$ref_files{$key} = "$dir".q|node12.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_infobox_duration/;
-$ref_files{$key} = "$dir".q|node18.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:main/;
-$ref_files{$key} = "$dir".q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:preference_pview_state/;
-$ref_files{$key} = "$dir".q|node23.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_sort_order/;
-$ref_files{$key} = "$dir".q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sub:Zoomable-and-Scrollable/;
-$ref_files{$key} = "$dir".q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:zoom_plus_cursor/;
 $ref_files{$key} = "$dir".q|node11.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_infobox_preview_state/;
-$ref_files{$key} = "$dir".q|node16.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:mouse_hand_mode/;
-$ref_files{$key} = "$dir".q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_popup_finer/;
+$key = q/fig:timeline_search_preview/;
 $ref_files{$key} = "$dir".q|node26.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:legend_checkbox_ops/;
+$key = q/fig:timeline_infobox_time/;
+$ref_files{$key} = "$dir".q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_popup/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:yaxis_label_panel_simple/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_color_chooser/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_minus_cursor/;
+$ref_files{$key} = "$dir".q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_duration/;
+$ref_files{$key} = "$dir".q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:main/;
 $ref_files{$key} = "$dir".q|node8.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_search_preview_zoomed/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$key = q/fig:preference_pview_state/;
+$ref_files{$key} = "$dir".q|node24.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_infobox_real_primitive/;
+$key = q/fig:legend_sort_order/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:Zoomable-and-Scrollable/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_plus_cursor/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_preview_state/;
 $ref_files{$key} = "$dir".q|node17.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/fig:mouse_hand_mode/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_popup_finer/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_checkbox_ops/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview_zoomed/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_real_primitive/;
+$ref_files{$key} = "$dir".q|node18.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig:timeline_preview_detail_0/;
-$ref_files{$key} = "$dir".q|node5.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_1/;
-$ref_files{$key} = "$dir".q|node5.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:hand_close_cursor/;
-$ref_files{$key} = "$dir".q|node13.html|; 
+$ref_files{$key} = "$dir".q|node14.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_2/;
-$ref_files{$key} = "$dir".q|node5.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_3/;
-$ref_files{$key} = "$dir".q|node5.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_4/;
-$ref_files{$key} = "$dir".q|node5.html|; 
+$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:hand_open_cursor/;
-$ref_files{$key} = "$dir".q|node13.html|; 
+$ref_files{$key} = "$dir".q|node14.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:row_adjustment_panel/;
-$ref_files{$key} = "$dir".q|node22.html|; 
+$ref_files{$key} = "$dir".q|node23.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- src/mpe2/src/slog2sdk/doc/jumpshot-4/html/labels.pl
+++ src/mpe2/src/slog2sdk/doc/jumpshot-4/html/labels.pl
@@ -3,115 +3,115 @@
 
 
 $key = q/fig:timeline_popup/;
-$external_labels{$key} = "$URL/" . q|node9.html|; 
+$external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:mouse_zoom_mode/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_search_preview/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_infobox_time/;
-$external_labels{$key} = "$URL/" . q|node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_popup/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:yaxis_label_panel_simple/;
-$external_labels{$key} = "$URL/" . q|node21.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_color_chooser/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:zoom_minus_cursor/;
-$external_labels{$key} = "$URL/" . q|node12.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_infobox_duration/;
-$external_labels{$key} = "$URL/" . q|node18.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:main/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:preference_pview_state/;
-$external_labels{$key} = "$URL/" . q|node23.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:legend_sort_order/;
-$external_labels{$key} = "$URL/" . q|node8.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sub:Zoomable-and-Scrollable/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:zoom_plus_cursor/;
 $external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_infobox_preview_state/;
-$external_labels{$key} = "$URL/" . q|node16.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:mouse_hand_mode/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:timeline_popup_finer/;
+$key = q/fig:timeline_search_preview/;
 $external_labels{$key} = "$URL/" . q|node26.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:legend_checkbox_ops/;
+$key = q/fig:timeline_infobox_time/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_popup/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:yaxis_label_panel_simple/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_color_chooser/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_minus_cursor/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_duration/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:main/;
 $external_labels{$key} = "$URL/" . q|node8.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_search_preview_zoomed/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$key = q/fig:preference_pview_state/;
+$external_labels{$key} = "$URL/" . q|node24.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:timeline_infobox_real_primitive/;
+$key = q/fig:legend_sort_order/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:Zoomable-and-Scrollable/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_plus_cursor/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_preview_state/;
 $external_labels{$key} = "$URL/" . q|node17.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/fig:mouse_hand_mode/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_popup_finer/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_checkbox_ops/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview_zoomed/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_real_primitive/;
+$external_labels{$key} = "$URL/" . q|node18.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig:timeline_preview_detail_0/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_1/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:hand_close_cursor/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
+$external_labels{$key} = "$URL/" . q|node14.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_2/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_3/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:timeline_preview_detail_4/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:hand_open_cursor/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
+$external_labels{$key} = "$URL/" . q|node14.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:row_adjustment_panel/;
-$external_labels{$key} = "$URL/" . q|node22.html|; 
+$external_labels{$key} = "$URL/" . q|node23.html|; 
 $noresave{$key} = "$nosave";
 
 1;
@@ -120,6 +120,142 @@ $noresave{$key} = "$nosave";
 # LaTeX2HTML 2002-2-1 (1.70)
 # labels from external_latex_labels array.
 
+
+$key = q/fig:timeline_popup/;
+$external_latex_labels{$key} = q|3.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:preference_data1/;
+$external_latex_labels{$key} = q|3.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_zoom_mode/;
+$external_latex_labels{$key} = q|3.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:preference_data2/;
+$external_latex_labels{$key} = q|3.12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview/;
+$external_latex_labels{$key} = q|4.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_time/;
+$external_latex_labels{$key} = q|3.16|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_popup/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:yaxis_label_panel_simple/;
+$external_latex_labels{$key} = q|3.17|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:selection_rules/;
+$external_latex_labels{$key} = q|3.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:legend_column_ops/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_color_chooser/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_minus_cursor/;
+$external_latex_labels{$key} = q|3.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:main/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_duration/;
+$external_latex_labels{$key} = q|3.15|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_sort_order/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:preference_pview_state/;
+$external_latex_labels{$key} = q|3.19|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:Zoomable-and-Scrollable/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_preview_state/;
+$external_latex_labels{$key} = q|3.13|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zoom_plus_cursor/;
+$external_latex_labels{$key} = q|3.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:main_toolbar/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:timeline_toolbar/;
+$external_latex_labels{$key} = q|3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mouse_hand_mode/;
+$external_latex_labels{$key} = q|3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:legend_checkbox_ops/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_popup_finer/;
+$external_latex_labels{$key} = q|4.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_search_preview_zoomed/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_infobox_real_primitive/;
+$external_latex_labels{$key} = q|3.14|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_0/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_1/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_close_cursor/;
+$external_latex_labels{$key} = q|3.12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_2/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_3/;
+$external_latex_labels{$key} = q|2.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:timeline_preview_detail_4/;
+$external_latex_labels{$key} = q|2.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:row_adjustment_panel/;
+$external_latex_labels{$key} = q|3.18|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hand_open_cursor/;
+$external_latex_labels{$key} = q|3.11|; 
+$noresave{$key} = "$nosave";
 
 1;
 
