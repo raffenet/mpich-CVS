796718af9507a2bc6d3e998b2b33bee098ab6c6e
gropp <gropp>
2003-04-28 13:12:29 +0000

    Added test for comm set/get name and dup of an intercommunicator

---
+++ test/mpi/comm/dupic.c
@@ -0,0 +1,65 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include <stdio.h>
+#include "mpitest.h"
+
+int main( int argc, char *argv[] )
+{
+    int errs = 0;
+    MPI_Comm comm, dupcomm;
+    MPI_Request r1req, r2req;
+    MPI_Status status, status1, status2;
+    int flag, r1buf, r2buf, s1buf, s2buf;
+    int rank;
+
+    MTest_Init( &argc, &argv );
+    
+    while (MTestGetIntercomm( &comm, 2 )) {
+	if (comm == MPI_COMM_NULL) continue;
+
+	MPI_Comm_dup( comm, &dupcomm );
+	
+	/* Check that there are separate contexts.  We do this by setting
+	   up nonblocking received on both communicators, and then
+	   sending to them.  If the contexts are different, tests on the
+	   unstaisfied communicator should indicate no available message */
+	MPI_Comm_rank( comm, &rank );
+	if (rank == 0) {
+	    r1buf = r2buf = -1;
+	    MPI_Irecv( &r1buf, 1, MPI_INT, 0, 0, dupcomm, &r1req );
+	    MPI_Irecv( &r2buf, 1, MPI_INT, 0, 0, comm, &r2req );
+	    s2buf = 17;
+	    MPI_Send( &s2buf, 1, MPI_INT, 0, 0, comm );
+	    MPI_Test( &r1req, &flag, &status );
+	    if (flag) {
+		errs++;
+		printf( "Error in context values for intercomm\n" );
+	    }
+	    s1buf = 456;
+	    MPI_Send( &s1buf, 1, MPI_INT, 0, 0, dupcomm );
+	    MPI_Wait( &r1req, &status1 );
+	    MPI_Wait( &r2req, &status2 );
+	    if (r1buf != s1buf) {
+		errs++;
+		printf( "Wrong value in communication on dupcomm %d != %d\n",
+			r1buf, s1buf );
+	    }
+	    if (r2buf != s2buf) {
+		errs++;
+		printf( "Wrong value in communication on comm %d != %d\n",
+			r2buf, s2buf );
+	    }
+	}
+	MPI_Comm_free( &dupcomm );
+	MPI_Comm_free( &comm );
+    }
+    MTest_Finalize( errs );
+    MPI_Finalize();
+    return 0;
+  
+}
