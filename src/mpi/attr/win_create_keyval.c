2baf2ca1c3562d444de025d8792a711d8e19d2d9
gropp <gropp>
2003-05-02 20:39:31 +0000

    Fix window attribute create and free keyval

--- src/mpi/attr/win_create_keyval.c
+++ src/mpi/attr/win_create_keyval.c
@@ -6,6 +6,7 @@
  */
 
 #include "mpiimpl.h"
+#include "attr.h"
 
 /* -- Begin Profiling Symbol Block for routine MPI_Win_create_keyval */
 #if defined(HAVE_PRAGMA_WEAK)
@@ -47,6 +48,7 @@ int MPI_Win_create_keyval(MPI_Win_copy_attr_function *win_copy_attr_fn, MPI_Win_
 {
     static const char FCNAME[] = "MPI_Win_create_keyval";
     int mpi_errno = MPI_SUCCESS;
+    MPID_Keyval *keyval_ptr;
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_WIN_CREATE_KEYVAL);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_WIN_CREATE_KEYVAL);
@@ -54,10 +56,8 @@ int MPI_Win_create_keyval(MPI_Win_copy_attr_function *win_copy_attr_fn, MPI_Win_
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+            MPIR_ERRTEST_INITIALIZED(mpi_errno);
+
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WIN_CREATE_KEYVAL);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
@@ -66,6 +66,32 @@ int MPI_Win_create_keyval(MPI_Win_copy_attr_function *win_copy_attr_fn, MPI_Win_
         MPID_END_ERROR_CHECKS;
     }
 #   endif /* HAVE_ERROR_CHECKING */
+
+    /* ... body of routine ...  */
+    keyval_ptr = (MPID_Keyval *)MPIU_Handle_obj_alloc( &MPID_Keyval_mem );
+    if (!keyval_ptr) {
+	mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**nomem", 0 );
+	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WIN_CREATE_KEYVAL);
+	return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
+    }
+    /* Initialize the attribute dup function */
+    if (!MPIR_Process.attr_dup) {
+	MPIR_Process.attr_dup  = MPIR_Attr_dup_list;
+	MPIR_Process.attr_free = MPIR_Attr_delete_list;
+    }
+
+    /* The handle encodes the keyval kind.  Modify it to have the correct
+       field */
+    keyval_ptr->handle           = (keyval_ptr->handle & ~(0x03c00000)) |
+	(MPID_WIN << 22);
+    *win_keyval		         = keyval_ptr->handle;
+    MPIU_Object_set_ref(keyval_ptr,1);
+    keyval_ptr->language         = MPID_LANG_C;
+    keyval_ptr->kind	         = MPID_WIN;
+    keyval_ptr->extra_state      = extra_state;
+    keyval_ptr->copyfn.C_CopyFunction  = win_copy_attr_fn;
+    keyval_ptr->delfn.C_DeleteFunction = win_delete_attr_fn;
+    /* ... end of body of routine ... */
 
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WIN_CREATE_KEYVAL);
     return MPI_SUCCESS;
--- src/mpi/attr/win_free_keyval.c
+++ src/mpi/attr/win_free_keyval.c
@@ -6,6 +6,7 @@
  */
 
 #include "mpiimpl.h"
+#include "attr.h"
 
 /* -- Begin Profiling Symbol Block for routine MPI_Win_free_keyval */
 #if defined(HAVE_PRAGMA_WEAK)
@@ -44,17 +45,26 @@ int MPI_Win_free_keyval(int *win_keyval)
 {
     static const char FCNAME[] = "MPI_Win_free_keyval";
     int mpi_errno = MPI_SUCCESS;
+    MPID_Keyval *keyval_ptr = NULL;
+    int          in_use;
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_WIN_FREE_KEYVAL);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_WIN_FREE_KEYVAL);
+
+    MPID_Keyval_get_ptr( *win_keyval, keyval_ptr );
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+            MPIR_ERRTEST_INITIALIZED(mpi_errno);
+
+	    MPID_Keyval_valid_ptr( keyval_ptr, mpi_errno );
+	    if (!mpi_errno) {
+		if (keyval_ptr->kind != MPID_WIN) {
+		    mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_KEYVAL, 
+						    "**keyvalnotwin", 0 );
+		}
+	    }
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WIN_FREE_KEYVAL);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
@@ -63,6 +73,13 @@ int MPI_Win_free_keyval(int *win_keyval)
         MPID_END_ERROR_CHECKS;
     }
 #   endif /* HAVE_ERROR_CHECKING */
+
+    /* ... body of routine ...  */
+    MPIU_Object_release_ref( keyval_ptr, &in_use);
+    if (!in_use) {
+	MPIU_Handle_obj_free( &MPID_Keyval_mem, keyval_ptr );
+    }
+    *win_keyval = MPI_KEYVAL_INVALID;
 
     MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WIN_FREE_KEYVAL);
     return MPI_SUCCESS;
