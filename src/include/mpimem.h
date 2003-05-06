0bc8b5842534ec89a54d40aad17bec706b30a88c
gropp <gropp>
2003-05-06 14:07:02 +0000

    Added style/copyright

--- src/include/mpimem.h
+++ src/include/mpimem.h
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  $Id$
+ *
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
 #ifndef MPIMEM_H_INCLUDED
 #define MPIMEM_H_INCLUDED
 /* ------------------------------------------------------------------------- */
