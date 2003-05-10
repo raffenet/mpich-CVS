86b21889aa49e4efae335e6242b9a83d65114445
David <David>
2003-05-10 07:26:14 +0000

    smpd: added mp_connect_next() to keep track of the next parent and id in the tree, added parsing of : separated sections of the command line

--- src/pm/smpd/mpiexec.c
+++ src/pm/smpd/mpiexec.c
@@ -12,12 +12,38 @@ int g_bDoConsole = 0;
 char g_pszConsoleHost[SMPD_MAX_HOST_LENGTH];
 mp_host_node_t *g_host_list = NULL;
 
+int mp_connect_next(int *parent_ptr, int *id_ptr)
+{
+    static int parent = 1;
+    static int id = 2;
+    int bit, mask, temp;
+
+    *parent_ptr = parent;
+    *id_ptr = id;
+    
+    id++;
+
+    temp = id >> 2;
+    bit = 1;
+    while (temp)
+    {
+	bit <<= 1;
+	temp >>= 1;
+    }
+    mask = bit - 1;
+    parent = bit | (id & mask);
+}
+
 int mp_parse_command_args(int *argcp, char **argvp[])
 {
     mp_host_node_t *node;
     char host[MP_MAX_HOST_LENGTH];
     int found;
     int cur_rank;
+    int n, parent, id;
+    int argc, next_argc;
+    char **next_argv;
+    char *exe_ptr;
 
     mp_enter_fn("mp_parse_command_args");
 
@@ -34,6 +60,16 @@ int mp_parse_command_args(int *argcp, char **argvp[])
     if (smpd_get_opt(argcp, argvp, "-p"))
     {
 	g_bUseProcessSession = 1;
+    }
+    if (smpd_get_opt_int(argcp, argvp, "-tree", &n))
+    {
+	while (n)
+	{
+	    mp_connect_next(&parent, &id);
+	    printf("connect: %d -> %d\n", parent, id);
+	    n--;
+	}
+	exit(0);
     }
     
     /* check for mpi options */
@@ -73,49 +109,82 @@ int mp_parse_command_args(int *argcp, char **argvp[])
      * -priority <class[:level]> - set the process startup priority class and optionally level.
      *            class = 0,1,2,3,4   = idle, below, normal, above, high
      *            level = 0,1,2,3,4,5 = idle, lowest, below, normal, above, highest
+     * -localroot - launch the root process without smpd if the host is local.
+     *              (This allows the root process to create windows and be debugged.)
+     *
+     * Backwards compatibility
+     * -np <numprocs>
+     * -dir <working directory>
      */
 
     cur_rank = 1;
-    while (smpd_get_opt_string(argcp, argvp, "-host", host, MP_MAX_HOST_LENGTH))
+    next_argc = *argcp;
+    next_argv = *argvp + 1;
+    exe_ptr = **argvp;
+    do
     {
-	node = g_host_list;
-	found = 0;
-	while (node)
+	/* calculate the current argc and find the next argv */
+	argc = 1;
+	while ( (*next_argv) != NULL && (**next_argv) != ':')
 	{
-	    if (strcmp(node->host, host) == 0)
-	    {
-		found = 1;
-		break;
-	    }
-	    if (node->next == NULL)
-		break;
-	    node = node->next;
+	    argc++;
+	    next_argc--;
+	    next_argv++;
 	}
-	if (!found)
+	if ( (*next_argv) != NULL && (**next_argv) == ':')
 	{
-	    if (node != NULL)
+	    (*next_argv) = NULL;
+	    next_argv++;
+	}
+	argcp = &argc;
+
+	/* parse the current block */
+	while (smpd_get_opt_string(argcp, argvp, "-host", host, MP_MAX_HOST_LENGTH))
+	{
+	    node = g_host_list;
+	    found = 0;
+	    while (node)
 	    {
-		node->next = (mp_host_node_t *)malloc(sizeof(mp_host_node_t));
+		if (strcmp(node->host, host) == 0)
+		{
+		    found = 1;
+		    break;
+		}
+		if (node->next == NULL)
+		    break;
 		node = node->next;
 	    }
-	    else
+	    if (!found)
 	    {
-		node = (mp_host_node_t *)malloc(sizeof(mp_host_node_t));
-		g_host_list = node;
+		if (node != NULL)
+		{
+		    node->next = (mp_host_node_t *)malloc(sizeof(mp_host_node_t));
+		    node = node->next;
+		}
+		else
+		{
+		    node = (mp_host_node_t *)malloc(sizeof(mp_host_node_t));
+		    g_host_list = node;
+		}
+		if (node == NULL)
+		{
+		    mp_err_printf("malloc failed to allocate a host node structure of size %d\n", sizeof(mp_host_node_t));
+		    mp_exit_fn("mp_parse_command_args");
+		    return SMPD_FAIL;
+		}
+		strcpy(node->host, host);
+		node->ip_str[0] = '\0';
+		node->id = cur_rank;
+		cur_rank++;
+		node->next = NULL;
 	    }
-	    if (node == NULL)
-	    {
-		mp_err_printf("malloc failed to allocate a host node structure of size %d\n", sizeof(mp_host_node_t));
-		mp_exit_fn("mp_parse_command_args");
-		return SMPD_FAIL;
-	    }
-	    strcpy(node->host, host);
-	    node->ip_str[0] = '\0';
-	    node->id = cur_rank;
-	    cur_rank++;
-	    node->next = NULL;
 	}
-    }
+
+	/* move to the next block */
+	*argvp = next_argv - 1;
+	if (*next_argv)
+	    **argvp = exe_ptr;
+    } while (*next_argv);
 
     mp_exit_fn("mp_parse_command_args");
     return SMPD_SUCCESS;
