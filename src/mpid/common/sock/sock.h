2064e62ab438e887df9ac1a8ddb2651c96009021
David <David>
2003-05-19 05:47:44 +0000

    sock: added SOCK_ERR_OP_ABORTED

--- src/mpid/common/sock/sock.h
+++ src/mpid/common/sock/sock.h
@@ -56,7 +56,8 @@ typedef enum sock_op
 #define SOCK_ERR_BAD_SOCK       1005
 #define SOCK_ERR_BAD_BUFFER     1006
 #define SOCK_ERR_OP_IN_PROGRESS 1007
-#define SOCK_ERR_OS_SPECIFIC    1008
+#define SOCK_ERR_OP_ABORTED     1008
+#define SOCK_ERR_OS_SPECIFIC    1009
 
 /*
  * structures
