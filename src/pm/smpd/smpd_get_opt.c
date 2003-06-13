2b397945eb07115ce2dd4956bfed2ea017cde6ea
David <David>
2003-06-13 22:39:50 +0000

    smpd: added better string encoding and decoding

--- src/pm/smpd/smpd_get_opt.c
+++ src/pm/smpd/smpd_get_opt.c
@@ -4,99 +4,239 @@
 #include <string.h>
 #endif
 
-int smpd_get_string_arg(char *str, char *flag, char *val, int maxlen)
+static const char * first_token(const char *str)
 {
-    char *pszFirst, *pszDelimLoc, *pszLast;
-    char *pszDelim = "=";
-    int bFirst = SMPD_TRUE;
-
-    if (str == NULL || flag == NULL || val == NULL)
-	return SMPD_FALSE;
-
-    while (SMPD_TRUE)
-    {
-	/* Find the name */
-	pszFirst = strstr(str, flag);
-	if (pszFirst == NULL)
-	    return SMPD_FALSE;
-
-	/* Check to see if we have matched a sub-string */
-	if (bFirst)
-	{
-	    bFirst = SMPD_FALSE;
-	    if ((pszFirst != str) && (!isspace(*(pszFirst-1))))
-	    {
-		str = pszFirst + strlen(flag);
-		continue;
-	    }
-	}
-	else
-	{
-	    if (!isspace(*(pszFirst-1)))
-	    {
-		str = pszFirst + strlen(flag);
-		continue;
-	    }
-	}
-
-	/* Skip over any white space after the name */
-	pszDelimLoc = &pszFirst[strlen(flag)];
-	while (isspace(*pszDelimLoc))
-	    pszDelimLoc++;
-
-	/* Find the deliminator */
-	if (strncmp(pszDelimLoc, pszDelim, strlen(pszDelim)) != 0)
-	{
-	    /*str = &pszDelimLoc[strlen(pszDelim)]; */
-	    str = pszDelimLoc;
-	    continue;
-	}
-	
-	/* Skip over the deliminator and any white space */
-	pszFirst = &pszDelimLoc[strlen(pszDelim)];
-	while (isspace(*pszFirst))
-	    pszFirst++;
-
-	if (*pszFirst == '\'')
-	{
-	    pszFirst++;
-	    while (*pszFirst != '\'' && *pszFirst != '\0')
-	    {
-		*val++ = *pszFirst++;
-	    }
-	    *val = '\0';
-	    break;
-	}
-	else
-	{
-	    /* Find the next deliminator */
-	    pszLast = strstr(pszFirst, pszDelim);
-	    if (pszLast == NULL)
-	    {
-		strcpy(val, pszFirst);
-		break;
-	    }
-	    
-	    /* Back up over any white space and name preceding the second deliminator */
-	    pszLast--;
-	    while (pszLast > pszFirst && isspace(*pszLast))
-		pszLast--;
-	    while (pszLast > pszFirst && !isspace(*pszLast))
-		pszLast--;
-	    while (pszLast > pszFirst && isspace(*pszLast))
-		pszLast--;
-	    
-	    /* Copy the data between first and last */
-	    pszLast++;
-	    strncpy(val, pszFirst, pszLast-pszFirst);
-	    val[pszLast-pszFirst] = '\0';
-	}
-	break;
-    }
-    return SMPD_TRUE;
+    if (str == NULL)
+	return NULL;
+    while (isspace(*str))
+	str++;
+    if (*str == '\0')
+	return NULL;
+    return str;
 }
 
-int smpd_get_int_arg(char *str, char *flag, int *val_ptr)
+static const char * next_token(const char *str)
+{
+    if (str == NULL)
+	return NULL;
+    str = first_token(str);
+    if (str == NULL)
+	return NULL;
+    if (*str == SMPD_QUOTE_CHAR)
+    {
+	/* move over string */
+	str++; /* move over the first quote */
+	if (*str == '\0')
+	    return NULL;
+	while (*str != SMPD_QUOTE_CHAR)
+	{
+	    /* move until the last quote, ignoring escaped quotes */
+	    if (*str == SMPD_ESCAPE_CHAR)
+	    {
+		str++;
+		if (*str == SMPD_QUOTE_CHAR)
+		    str++;
+	    }
+	    else
+	    {
+		str++;
+	    }
+	    if (*str == '\0')
+		return NULL;
+	}
+	str++; /* move over the last quote */
+    }
+    else
+    {
+	if (*str == SMPD_DELIM_CHAR)
+	{
+	    /* move over the DELIM token */
+	    str++;
+	}
+	else
+	{
+	    /* move over literal */
+	    while (!isspace(*str) && *str != SMPD_DELIM_CHAR && *str != '\0')
+		str++;
+	}
+    }
+    return first_token(str);
+}
+
+static int compare_token(const char *token, const char *str)
+{
+    if (token == NULL || str == NULL)
+	return -1;
+
+    if (*token == SMPD_QUOTE_CHAR)
+    {
+	/* compare quoted strings */
+	token++; /* move over the first quote */
+	/* compare characters until reaching the end of the string or the end quote character */
+	do
+	{
+	    if (*token == SMPD_ESCAPE_CHAR)
+	    {
+		if (*(token+1) == SMPD_QUOTE_CHAR)
+		{
+		    /* move over the escape character if the next character is a quote character */
+		    token++;
+		}
+		if (*token != *str)
+		    break;
+	    }
+	    else
+	    {
+		if (*token != *str || *token == SMPD_QUOTE_CHAR)
+		    break;
+	    }
+	    if (*str == '\0')
+		break;
+	    token++;
+	    str++;
+	} while (1);
+	if (*str == '\0' && *token == SMPD_QUOTE_CHAR)
+	    return 0;
+	if (*token == SMPD_QUOTE_CHAR)
+	    return 1;
+	if (*str < *token)
+	    return -1;
+	return 1;
+    }
+
+    /* compare DELIM token */
+    if (*token == SMPD_DELIM_CHAR)
+    {
+	if (*str == SMPD_DELIM_CHAR)
+	{
+	    str++;
+	    if (*str == '\0')
+		return 0;
+	    return 1;
+	}
+	if (*token < *str)
+	    return -1;
+	return 1;
+    }
+
+    /* compare literals */
+    while (*token == *str && *str != '\0' && *token != SMPD_DELIM_CHAR && !isspace(*token))
+    {
+	token++;
+	str++;
+    }
+    if ( (*str == '\0') && (*token == SMPD_DELIM_CHAR || isspace(*token) || *token == '\0') )
+	return 0;
+    if (*token == SMPD_DELIM_CHAR || isspace(*token) || *token < *str)
+	return -1;
+    return 1;
+}
+
+static void token_copy(const char *token, char *str, int maxlen)
+{
+    /* check parameters */
+    if (token == NULL || str == NULL)
+	return;
+
+    /* check special buffer lengths */
+    if (maxlen < 1)
+	return;
+    if (maxlen == 1)
+    {
+	*str = '\0';
+	return;
+    }
+
+    /* cosy up to the token */
+    token = first_token(token);
+    if (token == NULL)
+	return;
+
+    if (*token == SMPD_DELIM_CHAR)
+    {
+	/* copy the special deliminator token */
+	str[0] = SMPD_DELIM_CHAR;
+	str[1] = '\0';
+	return;
+    }
+
+    if (*token == SMPD_QUOTE_CHAR)
+    {
+	/* quoted copy */
+	token++; /* move over the first quote */
+	do
+	{
+	    if (*token == SMPD_ESCAPE_CHAR)
+	    {
+		if (*(token+1) == SMPD_QUOTE_CHAR)
+		    token++;
+		*str = *token;
+	    }
+	    else
+	    {
+		if (*token == SMPD_QUOTE_CHAR)
+		{
+		    *str = '\0';
+		    return;
+		}
+		*str = *token;
+	    }
+	    str++;
+	    token++;
+	    maxlen--;
+	} while (maxlen);
+	/* we've run out of destination characters so back up and null terminate the string */
+	str--;
+	*str = '\0';
+	return;
+    }
+
+    /* literal copy */
+    while (*token != SMPD_DELIM_CHAR && !isspace(*token) && *token != '\0' && maxlen)
+    {
+	*str = *token;
+	str++;
+	token++;
+	maxlen--;
+    }
+    if (maxlen)
+	*str = '\0';
+}
+
+int smpd_get_string_arg(const char *str, char *flag, char *val, int maxlen)
+{
+    if (maxlen < 1)
+	return SMPD_FALSE;
+
+    /* line up with the first token */
+    str = first_token(str);
+    if (str == NULL)
+	return SMPD_FALSE;
+
+    /* This loop will match the first instance of "flag = value" in the string. */
+    do
+    {
+	if (compare_token(str, flag) == 0)
+	{
+	    str = next_token(str);
+	    if (compare_token(str, SMPD_DELIM_STR) == 0)
+	    {
+		str = next_token(str);
+		if (str == NULL)
+		    return SMPD_FALSE;
+		token_copy(str, val, maxlen);
+		return SMPD_TRUE;
+	    }
+	}
+	else
+	{
+	    str = next_token(str);
+	}
+    } while (str);
+    return SMPD_FALSE;
+}
+
+int smpd_get_int_arg(const char *str, char *flag, int *val_ptr)
 {
     char int_str[12];
 
@@ -108,34 +248,110 @@ int smpd_get_int_arg(char *str, char *flag, int *val_ptr)
     return SMPD_FALSE;
 }
 
+static int quoted_printf(char *str, int maxlen, char *val)
+{
+    int count = 0;
+    if (maxlen < 1)
+	return 0;
+    *str = SMPD_QUOTE_CHAR;
+    str++;
+    maxlen--;
+    count++;
+    while (maxlen)
+    {
+	if (*val == '\0')
+	    break;
+	if (*val == SMPD_QUOTE_CHAR)
+	{
+	    *str = SMPD_ESCAPE_CHAR;
+	    str++;
+	    maxlen--;
+	    count++;
+	    if (maxlen == 0)
+		return count;
+	}
+	*str = *val;
+	str++;
+	maxlen--;
+	count++;
+	val++;
+    }
+    if (maxlen)
+    {
+	*str = SMPD_QUOTE_CHAR;
+	str++;
+	maxlen--;
+	count++;
+	if (maxlen == 0)
+	    return count;
+	*str = '\0';
+    }
+    return count;
+}
+
 int smpd_add_string_arg(char **str_ptr, int *maxlen_ptr, char *flag, char *val)
 {
     int num_chars;
 
+    if (*maxlen_ptr < 1)
+	return SMPD_FAIL;
+
+    /* add the flag */
     if (strstr(flag, " "))
     {
-	smpd_err_printf("invalid flag, spaces not allowed: %s\n", flag);
+	num_chars = quoted_printf(*str_ptr, *maxlen_ptr, flag);
+    }
+    else
+    {
+	num_chars = snprintf(*str_ptr, *maxlen_ptr, "%s", flag);
+    }
+    *maxlen_ptr = *maxlen_ptr - num_chars;
+    if (*maxlen_ptr < 1)
+    {
+	(*str_ptr)[num_chars-1] = '\0';
+	smpd_dbg_printf("partial argument added to string: '%s'\n", *str_ptr);
 	return SMPD_FAIL;
     }
-    num_chars = snprintf(*str_ptr, *maxlen_ptr, "%s='%s' ", flag, val);
+    *str_ptr = *str_ptr + num_chars;
+
+    /* add the deliminator character */
+    **str_ptr = SMPD_DELIM_CHAR;
+    *str_ptr = *str_ptr + 1;
+    *maxlen_ptr = *maxlen_ptr - 1;
+
+    /* add the value string */
+    if (strstr(val, " "))
+    {
+	num_chars = quoted_printf(*str_ptr, *maxlen_ptr, val);
+    }
+    else
+    {
+	num_chars = snprintf(*str_ptr, *maxlen_ptr, "%s", val);
+    }
     *str_ptr = *str_ptr + num_chars;
     *maxlen_ptr = *maxlen_ptr - num_chars;
+    if (*maxlen_ptr < 2)
+    {
+	*str_ptr = *str_ptr - 1;
+	**str_ptr = '\0';
+	smpd_dbg_printf("partial argument added to string: '%s'\n", *str_ptr);
+	return SMPD_FAIL;
+    }
+    
+    /* add the trailing space */
+    **str_ptr = ' ';
+    *str_ptr = *str_ptr + 1;
+    **str_ptr = '\0';
+    *maxlen_ptr = *maxlen_ptr - 1;
+
     return SMPD_SUCCESS;
 }
 
 int smpd_add_int_arg(char **str_ptr, int *maxlen_ptr, char *flag, int val)
 {
-    int num_chars;
-
-    if (strstr(flag, " "))
-    {
-	smpd_err_printf("invalid flag, spaces not allowed: %s\n", flag);
-	return SMPD_FAIL;
-    }
-    num_chars = snprintf(*str_ptr, *maxlen_ptr, "%s=%d ", flag, val);
-    *str_ptr = *str_ptr + num_chars;
-    *maxlen_ptr = *maxlen_ptr - num_chars;
-    return SMPD_SUCCESS;
+    char val_str[12];
+    sprintf(val_str, "%d", val);
+    return smpd_add_string_arg(str_ptr, maxlen_ptr, flag, val_str);
 }
 
 int smpd_get_opt(int *argc, char ***argv, char * flag)
