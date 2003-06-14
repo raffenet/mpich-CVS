11abf7ce4e222ff7f18bbe57c12f84278a689422
David <David>
2003-06-14 22:51:13 +0000

    smpd: added smpd_signal

--- src/pm/smpd/smpd_session.c
+++ src/pm/smpd/smpd_session.c
@@ -6,6 +6,29 @@
 
 #include "smpd.h"
 
+#ifndef HAVE_WINDOWS_H
+smpd_sig_fn_t *smpd_signal( int signo, smpd_sig_fn_t func )
+{
+    struct sigaction act, oact;
+
+    act.sa_handler = func;
+    sigemptyset( &act.sa_mask );
+    act.sa_flags = 0;
+    if ( signo == SIGALRM ) {
+#ifdef  SA_INTERRUPT
+        act.sa_flags |= SA_INTERRUPT;   /* SunOS 4.x */
+#endif
+    } else {
+#ifdef SA_RESTART
+        act.sa_flags |= SA_RESTART;     /* SVR4, 4.4BSD */
+#endif
+    }
+    if ( sigaction( signo,&act, &oact ) < 0 )
+        return ( SIG_ERR );
+    return( oact.sa_handler );
+}
+#endif
+
 int smpd_create_process_struct(int rank, smpd_process_t **process_ptr)
 {
     int result;
