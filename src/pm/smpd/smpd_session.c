80f5f9412d8149e0f4164d1ae2d8095f446f2632
David <David>
2003-06-19 08:06:28 +0000

    *** empty log message ***

--- src/pm/smpd/smpd_session.c
+++ src/pm/smpd/smpd_session.c
@@ -12,7 +12,10 @@
 #include <signal.h>
 #endif
 #ifdef HAVE_STDLIB_H
-#include <stdlib.h>
+#include <stdlib.h> /* getenv */
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h> /* strrchr */
 #endif
 
 #if 0
