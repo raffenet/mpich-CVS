ddb9d979174fe6732be909091a7034c3a600cb64
robl <robl>
2003-06-27 23:23:16 +0000

    close another small memory leak

--- src/mpi/romio/adio/ad_pvfs2/ad_pvfs2_close.c
+++ src/mpi/romio/adio/ad_pvfs2/ad_pvfs2_close.c
@@ -10,10 +10,10 @@
 
 void ADIOI_PVFS2_Close(ADIO_File fd, int *error_code)
 {
+    ADIOI_Free(fd->fs_ptr);
     /* pvfs2 doesn't have a 'close' */
     *error_code = MPI_SUCCESS;
 }
-
 /* 
  * vim: ts=8 sts=4 sw=4 noexpandtab 
  */
