6bb17fab2f9b600540fb87d044118ccfac0b51cd
robl <robl>
2003-07-02 00:10:42 +0000

    flesh out some more fcntl operations. most of these do not work yet

--- src/mpi/romio/adio/ad_pvfs2/ad_pvfs2_fcntl.c
+++ src/mpi/romio/adio/ad_pvfs2/ad_pvfs2_fcntl.c
@@ -8,17 +8,54 @@
 
 #include "ad_pvfs2.h"
 #include "adio_extern.h"
+#include "ad_pvfs2_common.h"
 
 void ADIOI_PVFS2_Fcntl(ADIO_File fd, int flag, ADIO_Fcntl_t *fcntl_struct, int *error_code)
 {
+    int ret;
+    ADIOI_PVFS2_fs *pvfs_fs;
+    PVFS_sysresp_getattr resp_getattr;
+
+    pvfs_fs = (ADIOI_PVFS2_fs*)fd->fs_ptr;
 
     switch(flag) {
+
     case ADIO_FCNTL_GET_FSIZE:
-    case ADIO_FCNTL_SET_DISKSPACE:
-    case ADIO_FCNTL_SET_IOMODE:
-    case ADIO_FCNTL_SET_ATOMICITY:
-    default:
+	/* PVFS_sys_getattr is all busted w.r.t. size */
+#if 0
+	ret = PVFS_sys_getattr(pvfs_fs->pinode_refn, PVFS_ATTR_SYS_SIZE, 
+		pvfs_fs->credentials, &resp_getattr);
+	if (ret < 0 ) {
+	    ADIOI_PVFS2_pvfs_error_convert(ret, error_code);
+	} else {
 	    *error_code = MPI_SUCCESS;
+	}
+	fcntl_struct->fsize = resp_getattr.attr.
+	return;
+#endif
+	fcntl_struct->fsize = 0;
+	*error_code = MPI_ERR_UNKNOWN;
+	break;
+
+    case ADIO_FCNTL_SET_DISKSPACE:
+	*error_code = MPI_ERR_UNKNOWN;
+	break;
+
+    case ADIO_FCNTL_SET_IOMODE:
+	/* a relic from PFS */
+	if (fd->iomode != fcntl_struct->iomode) {
+	    fd->iomode = fcntl_struct->iomode;
+	    MPI_Barrier(MPI_COMM_WORLD);
+	}
+	*error_code = MPI_SUCCESS;
+	break;
+
+    case ADIO_FCNTL_SET_ATOMICITY:
+	*error_code = MPI_ERR_UNKNOWN;
+	break;
+    default:
+	FPRINTF(stderr, "Unknown flag passed to ADIOI_PVFS2_Fcntl\n");
+	MPI_Abort(MPI_COMM_WORLD, 1);
     }
 }
 
