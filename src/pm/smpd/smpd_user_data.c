df40292cf00daf2aa8800bcc1e39f06366298b78
David <David>
2003-07-03 05:39:31 +0000

    *** empty log message ***

--- src/pm/smpd/smpd_user_data.c
+++ src/pm/smpd/smpd_user_data.c
@@ -5,6 +5,15 @@
  */
 
 #include "smpd.h"
+#ifdef HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
 
 SMPD_BOOL smpd_option_on(const char *option)
 {
