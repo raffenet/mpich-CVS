2874694b48b24e6f6492ad56f9d5904464980b0c
thakur <thakur>
2003-07-18 18:02:17 +0000

    updated pmi.h

--- src/include/pmi.h
+++ src/include/pmi.h
@@ -42,9 +42,20 @@ int PMI_KVS_iter_first(const char *kvsname, char *key, char *val);  /* loop thro
 int PMI_KVS_iter_next(const char *kvsname, char *key, char *val);   /* pairs in the kvs */
 
 /* PMI Process Creation functions */
+
 int PMI_Spawn_multiple(int count, const char *cmds[], const char **argvs[], 
                        const int *maxprocs, const void *info, int *errors, 
                        int *same_domain, const void *preput_info);
+
+/*
+int PMI_Spawn_multiple(int count, const char *cmds[], const char **argvs[], 
+                       const int *maxprocs, const void *info, int *errors, 
+                       int *same_domain, int preput_num,
+		       const char *preput_keys[], const char *preput_vals[]);
+*/
+
+int PMI_Spawn(const char *cmd, const char *argv[], const int maxprocs,
+	      char *spawned_kvsname, const int kvsnamelen );
 
 /* parse PMI implementation specific values into an info object that can then be passed to 
    PMI_Spawn_multiple.  Remove PMI implementation specific arguments from argc and argv */
