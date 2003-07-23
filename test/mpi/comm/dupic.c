99e4cba7fc8c697a3d7969f3ff014b541495334e
toonen <toonen>
2003-07-23 22:59:29 +0000

    removed race conditions from dupic test

--- test/mpi/comm/dupic.c
+++ test/mpi/comm/dupic.c
@@ -12,8 +12,9 @@ int main( int argc, char *argv[] )
 {
     int errs = 0;
     MPI_Comm comm, dupcomm;
-    MPI_Request r1req, r2req;
-    MPI_Status status, status1, status2;
+    MPI_Request rreq[2];
+    MPI_Status status;
+    int index;
     int flag, r1buf, r2buf, s1buf, s2buf;
     int rank;
 
@@ -31,19 +32,18 @@ int main( int argc, char *argv[] )
 	MPI_Comm_rank( comm, &rank );
 	if (rank == 0) {
 	    r1buf = r2buf = -1;
-	    MPI_Irecv( &r1buf, 1, MPI_INT, 0, 0, dupcomm, &r1req );
-	    MPI_Irecv( &r2buf, 1, MPI_INT, 0, 0, comm, &r2req );
+	    MPI_Irecv( &r1buf, 1, MPI_INT, 0, 0, dupcomm, &rreq[0] );
+	    MPI_Irecv( &r2buf, 1, MPI_INT, 0, 0, comm, &rreq[1] );
 	    s2buf = 17;
 	    MPI_Send( &s2buf, 1, MPI_INT, 0, 0, comm );
-	    MPI_Test( &r1req, &flag, &status );
-	    if (flag) {
+	    MPI_Waitany(2, rreq, &index, &status);
+	    if (index != 1) {
 		errs++;
 		printf( "Error in context values for intercomm\n" );
 	    }
 	    s1buf = 456;
 	    MPI_Send( &s1buf, 1, MPI_INT, 0, 0, dupcomm );
-	    MPI_Wait( &r1req, &status1 );
-	    MPI_Wait( &r2req, &status2 );
+	    MPI_Wait( &rreq[0], &status );
 	    if (r1buf != s1buf) {
 		errs++;
 		printf( "Wrong value in communication on dupcomm %d != %d\n",
