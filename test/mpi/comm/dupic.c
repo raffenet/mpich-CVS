287708136bfef2c4d5a6d0e67e0191f92c72b0ea
toonen <toonen>
2003-07-28 23:22:02 +0000

    (test/mpi/comm) fixed a race condition in dupic.c that wasn't quite fixed last time

--- test/mpi/comm/dupic.c
+++ test/mpi/comm/dupic.c
@@ -13,8 +13,8 @@ int main( int argc, char *argv[] )
     int errs = 0;
     MPI_Comm comm, dupcomm;
     MPI_Request rreq[2];
-    MPI_Status status;
-    int index;
+    int count;
+    int indicies[2];
     int flag, r1buf, r2buf, s1buf, s2buf;
     int rank;
 
@@ -31,19 +31,19 @@ int main( int argc, char *argv[] )
 	   unstaisfied communicator should indicate no available message */
 	MPI_Comm_rank( comm, &rank );
 	if (rank == 0) {
+	    s1buf = 456;
+	    s2buf = 17;
 	    r1buf = r2buf = -1;
 	    MPI_Irecv( &r1buf, 1, MPI_INT, 0, 0, dupcomm, &rreq[0] );
 	    MPI_Irecv( &r2buf, 1, MPI_INT, 0, 0, comm, &rreq[1] );
-	    s2buf = 17;
 	    MPI_Send( &s2buf, 1, MPI_INT, 0, 0, comm );
-	    MPI_Waitany(2, rreq, &index, &status);
-	    if (index != 1) {
+	    MPI_Waitsome(2, rreq, &count, indicies, MPI_STATUSES_IGNORE);
+	    if (count == 1 && indicies[0] != 1) {
 		errs++;
 		printf( "Error in context values for intercomm\n" );
 	    }
-	    s1buf = 456;
 	    MPI_Send( &s1buf, 1, MPI_INT, 0, 0, dupcomm );
-	    MPI_Wait( &rreq[0], &status );
+	    MPI_Wait( &rreq[0], MPI_STATUS_IGNORE );
 	    if (r1buf != s1buf) {
 		errs++;
 		printf( "Wrong value in communication on dupcomm %d != %d\n",
