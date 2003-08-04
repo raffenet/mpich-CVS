03b4571a5db3e897c907f4ad4fc960be6a3d8489
toonen <toonen>
2003-08-04 23:10:53 +0000

    (slog2sdk) added copyright notice (is this file autogenerated???)

--- src/mpe2/src/slog2sdk/trace_rlog/acconfig.h
+++ src/mpe2/src/slog2sdk/trace_rlog/acconfig.h
@@ -1,2 +1,8 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*-
+ *
+ *   Copyright (C) 2001 University of Chicago.
+ *   See COPYRIGHT notice in top-level directory.
+ */
+
 /*  define if <jni.h> is found  */
 #undef HAVE_JNI_H
