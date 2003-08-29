65942647015bbd09fe0f2c16cacfafa1125afb20
rross <rross>
2003-08-29 20:22:57 +0000

    added some 4d tests to make things more interesting.

--- test/mpi/datatype/subarray-pack.c
+++ test/mpi/datatype/subarray-pack.c
@@ -8,8 +8,10 @@ static int verbose = 0;
 
 /* tests */
 int subarray_2d_c_test1(void);
+int subarray_4d_c_test1(void);
 int subarray_2d_c_test2(void);
 int subarray_2d_fortran_test1(void);
+int subarray_4d_fortran_test1(void);
 
 /* helper functions */
 static int parse_args(int argc, char **argv);
@@ -40,6 +42,16 @@ int main(int argc, char **argv)
     err = subarray_2d_c_test2();
     if (err && verbose) fprintf(stderr,
 				"%d errors in 2d subarray c test 2.\n", err);
+    errs += err;
+
+    err = subarray_4d_c_test1();
+    if (err && verbose) fprintf(stderr,
+				"%d errors in 4d subarray c test 1.\n", err);
+    errs += err;
+
+    err = subarray_4d_fortran_test1();
+    if (err && verbose) fprintf(stderr,
+				"%d errors in 4d subarray fortran test 1.\n", err);
     errs += err;
 
     /* print message and exit */
@@ -195,6 +207,192 @@ int subarray_2d_c_test2(void)
     MPI_Type_free(&subarray);
     return errs;
 }
+
+/* subarray_4d_c_test1()
+ *
+ * Returns the number of errors encountered.
+ */
+int subarray_4d_c_test1(void)
+{
+    MPI_Datatype subarray;
+    int array[] = {
+	-1111, -1112, -1113, -1114, -1115, -1116,
+	-1121, -1122, -1123, -1124, -1125, -1126,
+	-1131, -1132, -1133, -1134, -1135, -1136,
+	-1211, -1212, -1213, -1214, -1215, -1216,
+	-1221, -1222, -1223, -1224, -1225, -1226,
+	-1231, -1232, -1233, -1234, -1235, -1236,
+	-2111, -2112, -2113, -2114,     1, -2116,
+	-2121, -2122, -2123, -2124,     2, -2126,
+	-2131, -2132, -2133, -2134,     3, -2136,
+	-2211, -2212, -2213, -2214,     4, -2216,
+	-2221, -2222, -2223, -2224,     5, -2226,
+	-2231, -2232, -2233, -2234,     6, -2236
+    };
+    
+    int array_size[4] = {2, 2, 3, 6};
+    int array_subsize[4] = {1, 2, 3, 1};
+    int array_start[4] = {1, 0, 0, 4};
+
+    int i, err, errs = 0, sizeoftype;
+
+    /* set up type */
+    err = MPI_Type_create_subarray(4, /* dims */
+				   array_size,
+				   array_subsize,
+				   array_start,
+				   MPI_ORDER_C,
+				   MPI_INT,
+				   &subarray);
+    if (err != MPI_SUCCESS) {
+	errs++;
+	if (verbose) {
+	    fprintf(stderr,
+		    "error in MPI_Type_create_subarray call; aborting after %d errors\n",
+		    errs);
+	}
+	return errs;
+    }
+
+    MPI_Type_commit(&subarray);
+    MPI_Type_size(subarray, &sizeoftype);
+    if (sizeoftype != 6*sizeof(int)) {
+	errs++;
+	if (verbose) fprintf(stderr, "size of type = %d; should be %d\n",
+			     sizeoftype, 6*sizeof(int));
+	return errs;
+    }
+
+    err = pack_and_unpack((char *) array, 1, subarray, 72*sizeof(int));
+
+    for (i=0; i < 72; i++) {
+	int goodval;
+	switch (i) {
+	    case 40:
+		goodval = 1;
+		break;
+	    case 46:
+		goodval = 2;
+		break;
+	    case 52:
+		goodval = 3;
+		break;
+	    case 58:
+		goodval = 4;
+		break;
+	    case 64:
+		goodval = 5;
+		break;
+	    case 70:
+		goodval = 6;
+		break;
+	    default:
+		goodval = 0;
+		break;
+	}
+	if (array[i] != goodval) {
+	    errs++;
+	    if (verbose) fprintf(stderr, "array[%d] = %d; should be %d\n",
+				 i, array[i], goodval);
+	}
+    }
+
+    MPI_Type_free(&subarray);
+    return errs;
+}
+/* subarray_4d_fortran_test1()
+ *
+ * Returns the number of errors encountered.
+ */
+int subarray_4d_fortran_test1(void)
+{
+    MPI_Datatype subarray;
+    int array[] = {
+	-1111, -1112, -1113, -1114, -1115, -1116,
+	-1121, -1122, -1123, -1124, -1125, -1126,
+	-1131, -1132, -1133, -1134, -1135, -1136,
+	-1211, -1212, -1213, -1214, -1215, -1216,
+	-1221, -1222, -1223, -1224, -1225, -1226,
+	-1231, -1232, -1233, -1234, -1235, -1236,
+	-2111, -2112, -2113, -2114,     1, -2116,
+	-2121, -2122, -2123, -2124,     2, -2126,
+	-2131, -2132, -2133, -2134,     3, -2136,
+	-2211, -2212, -2213, -2214,     4, -2216,
+	-2221, -2222, -2223, -2224,     5, -2226,
+	-2231, -2232, -2233, -2234,     6, -2236
+    };
+    
+    int array_size[4] = {6, 3, 2, 2};
+    int array_subsize[4] = {1, 3, 2, 1};
+    int array_start[4] = {4, 0, 0, 1};
+
+    int i, err, errs = 0, sizeoftype;
+
+    /* set up type */
+    err = MPI_Type_create_subarray(4, /* dims */
+				   array_size,
+				   array_subsize,
+				   array_start,
+				   MPI_ORDER_FORTRAN,
+				   MPI_INT,
+				   &subarray);
+    if (err != MPI_SUCCESS) {
+	errs++;
+	if (verbose) {
+	    fprintf(stderr,
+		    "error in MPI_Type_create_subarray call; aborting after %d errors\n",
+		    errs);
+	}
+	return errs;
+    }
+
+    MPI_Type_commit(&subarray);
+    MPI_Type_size(subarray, &sizeoftype);
+    if (sizeoftype != 6*sizeof(int)) {
+	errs++;
+	if (verbose) fprintf(stderr, "size of type = %d; should be %d\n",
+			     sizeoftype, 6*sizeof(int));
+	return errs;
+    }
+
+    err = pack_and_unpack((char *) array, 1, subarray, 72*sizeof(int));
+
+    for (i=0; i < 72; i++) {
+	int goodval;
+	switch (i) {
+	    case 40:
+		goodval = 1;
+		break;
+	    case 46:
+		goodval = 2;
+		break;
+	    case 52:
+		goodval = 3;
+		break;
+	    case 58:
+		goodval = 4;
+		break;
+	    case 64:
+		goodval = 5;
+		break;
+	    case 70:
+		goodval = 6;
+		break;
+	    default:
+		goodval = 0;
+		break;
+	}
+	if (array[i] != goodval) {
+	    errs++;
+	    if (verbose) fprintf(stderr, "array[%d] = %d; should be %d\n",
+				 i, array[i], goodval);
+	}
+    }
+
+    MPI_Type_free(&subarray);
+    return errs;
+}
+
 
 /* subarray_2d_fortran_test1()
  *
