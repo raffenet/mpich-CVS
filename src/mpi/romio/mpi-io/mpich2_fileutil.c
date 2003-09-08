fe9d8adedac7e8c0a8d4a53c645c50db9bb758e1
gropp <gropp>
2003-09-08 13:34:19 +0000

    Change the ROMIO routines for calling the error handler to interact with the new MPICH2 routines for error handlers

--- src/mpi/romio/mpi-io/mpich2_fileutil.c
+++ src/mpi/romio/mpi-io/mpich2_fileutil.c
@@ -4,19 +4,48 @@
  *      See COPYRIGHT in top-level directory.
  */
 #include "mpioimpl.h"
+#include "adio_extern.h"
 
 #ifdef MPICH2
 
 int MPIR_Err_return_file( MPI_File file_ptr, const char fcname[], int errcode )
 {
+    MPI_Errhandler e;
+    void (*c_errhandler)(MPI_File *, int *, ... );
+    int  kind;   /* Error handler kind (see below) */
+
     /* First, check the nesting level */
     /*if (MPIR_Nest_value()) return errcode;*/ /* Is there any recursion in the MPI_File_... interface? */
 
     /* If the file pointer is not valid, we use the handler on
        MPI_FILE_NULL (MPI-2, section 9.7).  For now, this code assumes that 
-       MPI_FILE_NULL has the default handler (return).  FIXME */
-    if (MPIR_Err_is_fatal(errcode) ||
-	(file_ptr != NULL && file_ptr->err_handler == MPI_ERRORS_ARE_FATAL))
+       MPI_FILE_NULL has the default handler (return).  FIXME.  See
+       below - the set error handler uses ADIOI_DFLT_ERR_HANDLER; 
+    */
+
+    /* First, get the handler and the corresponding function */
+    
+    if (file_ptr == MPI_FILE_NULL) e = ADIOI_DFLT_ERR_HANDLER;
+    else                           e = file_ptr->err_handler;
+
+    /* Actually, e is just the value provide by the MPICH2 routines
+       file_set_errhandler.  This is actually a *pointer* to the
+       errhandler structure.  We don't know that, so we ask
+       the MPICH2 code to translate this object into an error handler.
+       kind = 0: errors are fatal
+       kind = 1: errors return
+       kind = 2: errors call function
+    */
+    if (e == MPI_ERRORS_RETURN || !e) {
+	/* FIXME: This is a hack in case no error handler was set */
+	kind = 1;
+	c_errhandler = 0;
+    }
+    else {
+	MPIR_Get_file_error_routine( e, &c_errhandler, &kind );
+    }
+    
+    if (MPIR_Err_is_fatal(errcode) || kind == 0) 
     {
 	if (MPIR_Err_print_stack_flag)
 	{
@@ -25,16 +54,23 @@ int MPIR_Err_return_file( MPI_File file_ptr, const char fcname[], int errcode )
 	}
 	else
 	{
-	    /* The default handler should try the following: Provide the rank in comm_world.  If the process is not in comm world,
-	       use something else.  If the communicator exists and has a name, provide that name */
+	    /* The default handler should try the following: Provide the rank 
+	       in comm_world.  If the process is not in comm world,
+	       use something else.  If the communicator exists and has a name, 
+	       provide that name */
 	    char msg[MPI_MAX_ERROR_STRING];
 
 	    MPIR_Err_get_string( errcode, msg );
-	    fprintf( stderr, "Fatal error (code 0x%08x) in %s(): %s\n", errcode, fcname, msg);
+	    fprintf( stderr, "Fatal error (code 0x%08x) in %s(): %s\n", 
+		     errcode, fcname, msg);
 	}
 
 	abort(); /* Change this to MPID_Abort */
     }
+    else if (kind == 2) {
+	(*c_errhandler)( &file_ptr, &errcode, 0 );
+    }
+    /* if kind == 1, just return */
 
     return errcode;
 }
@@ -45,12 +81,22 @@ int MPIR_Err_return_file( MPI_File file_ptr, const char fcname[], int errcode )
    includes a reference count.  Not currently used. */
 int MPIR_ROMIO_Set_file_errhand( MPI_File file_ptr, MPI_Errhandler e )
 {
-    file_ptr->err_handler = e;
+    if (file_ptr == MPI_FILE_NULL) ADIOI_DFLT_ERR_HANDLER = e;
+    else if (file_ptr->cookie != ADIOI_FILE_COOKIE) {
+	return MPI_ERR_FILE;
+    }
+    else 
+	file_ptr->err_handler = e;
     return 0;
 }
 int MPIR_ROMIO_Get_file_errhand( MPI_File file_ptr, MPI_Errhandler *e )
 {
-    *e = file_ptr->err_handler;
+    if (file_ptr == MPI_FILE_NULL) *e = ADIOI_DFLT_ERR_HANDLER;
+    else if (file_ptr->cookie != ADIOI_FILE_COOKIE) {
+	return MPI_ERR_FILE;
+    }
+    else 
+	*e = file_ptr->err_handler;
     return 0;
 }
 
