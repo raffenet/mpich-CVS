c0d1f49766022d7f9c188ccad21baa2b6474b9c4
gropp <gropp>
2003-09-12 19:33:44 +0000

    Switch to newer error macros

--- src/mpi/comm/comm_size.c
+++ src/mpi/comm/comm_size.c
@@ -60,17 +60,11 @@ int MPI_Comm_size( MPI_Comm comm, int *size )
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
-            if (!size) {
-                mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_ARG, 
-                            "**nullptr", "**nullptr %s", "size" );
-            } 
+	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
+	    MPIR_ERRTEST_ARGNULL(size,"size",mpi_errno);
             /* Validate comm_ptr */
             MPID_Comm_valid_ptr( comm_ptr, mpi_errno );
-	    /* If comm_ptr is not value, it will be reset to null */
+	    /* If comm_ptr is not valid, it will be reset to null */
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_COMM_SIZE);
                 return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
