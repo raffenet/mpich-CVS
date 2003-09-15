7c17aa77738e1477e2c0b7080c8f932f6467c9ad
toonen <toonen>
2003-09-15 18:11:45 +0000

    (mpi/errhan) updated errcodes.h to use class macros in mpiimpl.h

--- src/mpi/errhan/errcodes.h
+++ src/mpi/errhan/errcodes.h
@@ -37,8 +37,8 @@ void MPIR_Err_delete_class( int );
    is-fatal?: the error is fatal and should not be returned to the user
  */
 
-#define ERROR_CLASS_MASK          0x0000007F
-#define ERROR_CLASS_SIZE          128
+#define ERROR_CLASS_MASK          MPIR_ERR_CLASS_MASK  /* the error class bits are defined in mpiimpl.h */
+#define ERROR_CLASS_SIZE          MPIR_ERR_CLASS_SIZE
 #define ERROR_DYN_MASK            0x00000080
 #define ERROR_DYN_SHIFT           7
 #define ERROR_GENERIC_MASK        0x0007FF00
@@ -52,7 +52,7 @@ void MPIR_Err_delete_class( int );
 #define ERROR_SPECIFIC_SEQ_SIZE   32
 #define ERROR_SPECIFIC_SEQ_SHIFT  26
 #define ERROR_FATAL_MASK          0x80000000
-#define ERROR_GET_CLASS( code ) ((code) & ERROR_CLASS_MASK)
+#define ERROR_GET_CLASS(mpi_errno_) MPIR_ERR_GET_CLASS(mpi_errno_)
 
 /* These must correspond to the masks defined above */
 #define ERROR_MAX_NCLASS ERROR_CLASS_SIZE
