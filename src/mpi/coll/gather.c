31a6bbc27f491d777aae4733aa1041e26299746f
thakur <thakur>
2003-09-19 21:43:31 +0000

    fixed error checks in intercomm gather and scatter

--- src/mpi/coll/gather.c
+++ src/mpi/coll/gather.c
@@ -517,31 +517,51 @@ int MPI_Gather(void *sendbuf, int sendcnt, MPI_Datatype sendtype, void *recvbuf,
                 return MPIR_Err_return_comm( NULL, FCNAME, mpi_errno );
             }
 
-            if (sendbuf != MPI_IN_PLACE) {
-                MPIR_ERRTEST_COUNT(sendcnt, mpi_errno);
-                MPIR_ERRTEST_DATATYPE(sendcnt, sendtype, mpi_errno);
-                if (HANDLE_GET_KIND(sendtype) != HANDLE_KIND_BUILTIN) {
-                    MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
-                    MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
-                }
-            }
-
 	    if (comm_ptr->comm_kind == MPID_INTRACOMM) {
 		MPIR_ERRTEST_INTRA_ROOT(comm_ptr, root, mpi_errno);
-	    }
-	    else {
-		MPIR_ERRTEST_INTER_ROOT(comm_ptr, root, mpi_errno);
-	    }
 
-            rank = comm_ptr->rank;
-            if (rank == root) {
-                MPIR_ERRTEST_COUNT(recvcnt, mpi_errno);
-                MPIR_ERRTEST_DATATYPE(recvcnt, recvtype, mpi_errno);
-                if (HANDLE_GET_KIND(recvtype) != HANDLE_KIND_BUILTIN) {
-                    MPID_Datatype_get_ptr(recvtype, recvtype_ptr);
-                    MPID_Datatype_valid_ptr( recvtype_ptr, mpi_errno );
+                if (sendbuf != MPI_IN_PLACE) {
+                    MPIR_ERRTEST_COUNT(sendcnt, mpi_errno);
+                    MPIR_ERRTEST_DATATYPE(sendcnt, sendtype, mpi_errno);
+                    if (HANDLE_GET_KIND(sendtype) != HANDLE_KIND_BUILTIN) {
+                        MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
+                        MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
+                    }
+                }
+                
+                rank = comm_ptr->rank;
+                if (rank == root) {
+                    MPIR_ERRTEST_COUNT(recvcnt, mpi_errno);
+                    MPIR_ERRTEST_DATATYPE(recvcnt, recvtype, mpi_errno);
+                    if (HANDLE_GET_KIND(recvtype) != HANDLE_KIND_BUILTIN) {
+                        MPID_Datatype_get_ptr(recvtype, recvtype_ptr);
+                        MPID_Datatype_valid_ptr( recvtype_ptr, mpi_errno );
+                    }
                 }
             }
+
+	    if (comm_ptr->comm_kind == MPID_INTERCOMM) {
+		MPIR_ERRTEST_INTER_ROOT(comm_ptr, root, mpi_errno);
+
+                if (root == MPI_ROOT) {
+                    MPIR_ERRTEST_COUNT(recvcnt, mpi_errno);
+                    MPIR_ERRTEST_DATATYPE(recvcnt, recvtype, mpi_errno);
+                    if (HANDLE_GET_KIND(recvtype) != HANDLE_KIND_BUILTIN) {
+                        MPID_Datatype_get_ptr(recvtype, recvtype_ptr);
+                        MPID_Datatype_valid_ptr( recvtype_ptr, mpi_errno );
+                    }
+                }
+                
+                else if (root != MPI_PROC_NULL) {
+                    MPIR_ERRTEST_COUNT(sendcnt, mpi_errno);
+                    MPIR_ERRTEST_DATATYPE(sendcnt, sendtype, mpi_errno);
+                    if (HANDLE_GET_KIND(sendtype) != HANDLE_KIND_BUILTIN) {
+                        MPID_Datatype_get_ptr(sendtype, sendtype_ptr);
+                        MPID_Datatype_valid_ptr( sendtype_ptr, mpi_errno );
+                    }
+                }
+            }
+
             if (mpi_errno != MPI_SUCCESS) {
                 MPID_MPI_COLL_FUNC_EXIT(MPID_STATE_MPI_GATHER);
                 return MPIR_Err_return_comm( comm_ptr, FCNAME, mpi_errno );
