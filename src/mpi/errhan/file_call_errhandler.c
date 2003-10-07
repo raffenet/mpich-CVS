eccfc9c238976416383869299864fcb902dc7ce7
gropp <gropp>
2003-10-07 22:18:44 +0000

    Added file_call_errhandler

--- src/mpi/errhan/file_call_errhandler.c
+++ src/mpi/errhan/file_call_errhandler.c
@@ -46,23 +46,32 @@ int MPI_File_call_errhandler(MPI_File fh, int errorcode)
     static const char FCNAME[] = "MPI_File_call_errhandler";
     int mpi_errno = MPI_SUCCESS;
     MPID_File *file_ptr = NULL;
+    MPID_Errhandler *e;
     MPID_MPI_STATE_DECL(MPID_STATE_MPI_FILE_CALL_ERRHANDLER);
 
     MPID_MPI_FUNC_ENTER(MPID_STATE_MPI_FILE_CALL_ERRHANDLER);
     /* Get handles to MPI objects. */
+#ifndef USE_ROMIO_FILE
     MPID_File_get_ptr( fh, file_ptr );
+#endif
 #   ifdef HAVE_ERROR_CHECKING
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
 	    MPIR_ERRTEST_INITIALIZED(mpi_errno);
 
+#ifndef USE_ROMIO_FILE
             /* Validate file_ptr */
             MPID_File_valid_ptr( file_ptr, mpi_errno );
 	    /* If file_ptr is not value, it will be reset to null */
+#endif
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_FILE_CALL_ERRHANDLER);
+#ifdef USE_ROMIO_FILE
+                return MPIR_Err_return_file( fh, FCNAME, mpi_errno );
+#else
                 return MPIR_Err_return_file( file_ptr, FCNAME, mpi_errno );
+#endif
             }
         }
         MPID_END_ERROR_CHECKS;
@@ -70,19 +79,27 @@ int MPI_File_call_errhandler(MPI_File fh, int errorcode)
 #   endif /* HAVE_ERROR_CHECKING */
 
     /* ... body of routine ...  */
-    switch (file_ptr->errhandler->language) {
+#ifdef USE_ROMIO_FILE
+ {
+     MPIR_ROMIO_Get_file_errhand( fh, (MPI_Errhandler *)&e );
+     if (!e) {
+	 MPID_Errhandler_get_ptr( MPI_ERRORS_RETURN, e );
+     }
+ }
+#else
+    e = file_ptr->errhandler;
+#endif
+    switch (e->language) {
     case MPID_LANG_C:
 #ifdef HAVE_CXX_BINDING
     case MPID_LANG_CXX:
 #endif
-	(*file_ptr->errhandler->errfn.C_File_Handler_function)( 
-	    &file_ptr->handle, &errorcode );
+	(*e->errfn.C_File_Handler_function)( &fh, &errorcode );
 	break;
 #ifdef HAVE_FORTRAN_BINDING
     case MPID_LANG_FORTRAN90:
     case MPID_LANG_FORTRAN:
-	(*file_ptr->errhandler->errfn.F77_Handler_function)( 
-	    (MPI_Fint *)&file_ptr->handle, &errorcode );
+	(*e->errfn.F77_Handler_function)( (MPI_Fint *)&fh, &errorcode );
 	break;
 #endif
     }
