5687751cacf9b40e6b40839b9563423ed92459be
toonen <toonen>
2003-10-16 18:00:10 +0000

    added copyright statement to ./src/pm/util/pmutilconf.h

--- src/pm/util/pmutilconf.h
+++ src/pm/util/pmutilconf.h
@@ -1,1 +1,8 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  $Id$
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 #include "../forker/forkerconf.h"
