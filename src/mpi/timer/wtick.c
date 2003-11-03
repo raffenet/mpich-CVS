860085d8b9fe578d8354618945bce67908c29b73
gropp <gropp>
2003-11-03 13:48:12 +0000

    Cleaned up preinit test

--- src/mpi/timer/wtick.c
+++ src/mpi/timer/wtick.c
@@ -50,10 +50,7 @@ double MPI_Wtick( void )
     {
         MPID_BEGIN_ERROR_CHECKS;
         {
-            if (MPIR_Process.initialized != MPICH_WITHIN_MPI) {
-                mpi_errno = MPIR_Err_create_code( MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER,
-                            "**initialized", 0 );
-            }
+            MPIR_ERRTEST_INITIALIZED(mpi_errno);
             if (mpi_errno) {
                 MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_WTICK);
                 return MPIR_Err_return_comm( 0, FCNAME, mpi_errno );
