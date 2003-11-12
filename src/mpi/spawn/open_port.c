50b388da60207a0d78898a9afd1b692db8c5c24b
ashton <ashton>
2003-11-12 17:21:34 +0000

    updated error codes

--- src/mpi/spawn/open_port.c
+++ src/mpi/spawn/open_port.c
@@ -81,7 +81,7 @@ int MPI_Open_port(MPI_Info info, char *port_name)
     if (mpi_errno != MPI_SUCCESS)
     {
 	mpi_errno = MPIR_Err_create_code(mpi_errno, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER,
-	    "**mpi_open_port", "**mpi_open_port %I %s", info, port_name);
+	    "**mpi_open_port", "**mpi_open_port %I %p", info, port_name);
 	MPID_MPI_FUNC_EXIT(MPID_STATE_MPI_OPEN_PORT);
 	return MPIR_Err_return_comm(0, FCNAME, mpi_errno);
     }
