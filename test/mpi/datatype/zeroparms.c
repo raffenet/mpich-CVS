7b5806f7d20d9b4559e3518b701d970ff5426135
rross <rross>
2003-11-13 21:55:51 +0000

    Added comment regarding functioning of test.

--- test/mpi/datatype/zeroparms.c
+++ test/mpi/datatype/zeroparms.c
@@ -9,12 +9,13 @@ int main( int argc, char *argv[] )
 
     MPI_Init( 0, 0 );
 
+    /* create a legitimate type to see that we don't 
+     * emit spurious errors.
+     */
     MPI_Type_hvector( 0, 1, 10, MPI_DOUBLE, &newtype );
-
     MPI_Type_commit( &newtype );
 
     MPI_Type_indexed( 0, b, d, MPI_DOUBLE, &newtype );
-
     MPI_Type_commit( &newtype );
 
     MPI_Sendrecv( b, 1, newtype, 0, 0, 
