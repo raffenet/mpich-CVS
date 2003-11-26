a84df07fb03bee845c9640b9110ab9f3a76de980
gropp <gropp>
2003-11-26 14:59:21 +0000

    Added warning about missing C++ code

--- src/mpi/attr/attrutil.c
+++ src/mpi/attr/attrutil.c
@@ -161,6 +161,7 @@ int MPIR_Attr_dup_list( int handle, MPID_Attribute *old_attrs,
 	    break;
 #ifdef HAVE_CXX_BINDING
 	case MPID_LANG_CXX: 
+	    /* FIXME:!!!!! No attribute copy for C++ */
 	    break;
 #endif
 #ifdef HAVE_FORTRAN_BINDING
