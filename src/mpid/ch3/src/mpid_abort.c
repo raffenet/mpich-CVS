5b2cf7a02aa6d3d5f49357cf75bc48bc785dde1e
David <David>
2003-12-12 21:17:42 +0000

    added support for non-comm_world rank

--- src/mpid/ch3/src/mpid_abort.c
+++ src/mpid/ch3/src/mpid_abort.c
@@ -12,22 +12,28 @@
 #define FCNAME MPIDI_QUOTE(FUNCNAME)
 int MPID_Abort(MPID_Comm * comm, int mpi_errno, int exit_code)
 {
+    int rank;
     MPIDI_STATE_DECL(MPID_STATE_MPID_ABORT);
 
     MPIDI_FUNC_ENTER(MPID_STATE_MPID_ABORT);
     MPIDI_DBG_PRINTF((10, FCNAME, "entering"));
 
+    if (comm)
+	rank = comm->rank;
+    else
+	rank = MPIR_Process.comm_world->rank;
+
     if (mpi_errno != MPI_SUCCESS)
     {
 	char msg[MPI_MAX_ERROR_STRING];
-	
+
 	MPIR_Err_get_string(mpi_errno, msg);
-	MPIU_Error_printf("ABORT - process %d: %s\n", MPIR_Process.comm_world->rank, msg);
+	MPIU_Error_printf("ABORT - process %d: %s\n", rank, msg);
 	fflush(stderr);
     }
     else
     {
-	MPIU_Error_printf("ABORT - process %d\n", MPIR_Process.comm_world->rank);
+	MPIU_Error_printf("ABORT - process %d\n", rank);
 	fflush(stderr);
     }
     
