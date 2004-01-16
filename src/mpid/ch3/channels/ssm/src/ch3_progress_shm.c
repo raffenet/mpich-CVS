9da81c4364b4b084b9ed891fa0125e039383d1f1
David <David>
2004-01-16 23:57:00 +0000

    *** empty log message ***

--- src/mpid/ch3/channels/ssm/src/ch3_progress_shm.c
+++ src/mpid/ch3/channels/ssm/src/ch3_progress_shm.c
@@ -177,10 +177,6 @@ int MPIDI_CH3I_SHM_write_progress(MPIDI_VC * vc)
 		{
 		    MPIDI_CH3I_SendQ_dequeue(vc);
 		}
-		else
-		{
-		    req->ch.iov_offset = 0;
-		}
 		vc->ch.send_active = MPIDI_CH3I_SendQ_head(vc);
 	    }
 	    else
