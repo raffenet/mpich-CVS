89226bf200bfa3a20520cf92f8564d117dfc7e41
gropp <gropp>
2004-01-16 14:21:22 +0000

    Added a routine to print internal errors involving Unix system calls

--- src/util/msgs/msgprint.c
+++ src/util/msgs/msgprint.c
@@ -77,6 +77,53 @@ int MPIU_Internal_error_printf(char *str, ...)
     return n;
 }
 
+/* Like internal_error_printf, but for the system routine name with 
+   errno errnum.  Str may be null */
+int MPIU_Internal_sys_error_printf(const char *name, int errnum, 
+				   const char *str, ...)
+{
+    int n;
+    va_list list;
+    const char *format_str=0;
+
+    /* Prepend information on the system error */
+#ifdef HAVE_STRERROR
+#ifdef USE_GETTEXT
+    /* Category is LC_MESSAGES */
+    format_str = dgettext( "mpich", "Error in system call %s: %s" );
+#endif
+    if (!format_str) format_str = "Error in system call %s: %s\n";
+
+    fprintf( stderr, format_str, name, strerror(errnum) );
+#else
+#ifdef USE_GETTEXT
+    /* Category is LC_MESSAGES */
+    format_str = dgettext( "mpich", "Error in system call %s errno = %d" );
+#endif
+    if (!format_str) format_str = "Error in system call %s errno = %d\n";
+
+    fprintf( stderr, "Error in %s: errno = %d\n", name, errnum );
+#endif
+
+    /* Now add the message that is specific to this use, if any */
+    if (str) {
+	va_start(list, str);
+#ifdef USE_GETTEXT
+	/* Category is LC_MESSAGES */
+	format_str = dgettext( "mpich", str );
+	if (!format_str) format_str = str;
+#else
+	format_str = str;
+#endif
+	n = vfprintf(stderr, format_str, list);
+	va_end(list);
+    }
+
+    fflush(stderr);
+
+    return n;
+}
+
 int MPIU_Msg_printf(char *str, ...)
 {
     int n;
