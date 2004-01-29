c9ba341c9e65b0541833135d27d48f9d65f7dd2f
thakur <thakur>
2004-01-29 23:41:31 +0000

    fixed bug in ch3_init reported by Cray

--- src/mpid/ch3/channels/sock/src/ch3_init.c
+++ src/mpid/ch3/channels/sock/src/ch3_init.c
@@ -266,7 +266,7 @@ int MPIDI_CH3_Init(int * has_args, int * has_env, int * has_parent)
 		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_FATAL, FCNAME, __LINE__, MPI_ERR_OTHER, "**nomem", 0);
 		return mpi_errno;
 	    }
-	    mpi_errno = PMI_KVS_Get(pg->kvs_name, key, val);
+	    mpi_errno = PMI_KVS_Get(pg->kvs_name, key, val, val_max_sz);
 	    if (mpi_errno != PMI_SUCCESS)
 	    {
 		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_FATAL, FCNAME, __LINE__, MPI_ERR_OTHER, "**pmi_kvs_get", "**pmi_kvs_get %d", mpi_errno);
