a7c427d3a423fec0b7bb9a69c7c81f801a0f2c8d
gropp <gropp>
2004-02-10 19:28:03 +0000

    Added a test for character and removed 2-dimensional array test

--- test/mpi/f90/misc/sizeof.f90
+++ test/mpi/f90/misc/sizeof.f90
@@ -3,9 +3,10 @@
           integer ierr, errs
           integer size1, size2
           real    a
-          real    d(2,2)
+          real    d(20)
           double precision b
           complex c
+          character ch
 
           errs = 0
           call mpi_init(ierr)
@@ -37,6 +38,13 @@
              print *, "complex size is ", size2, " sizeof claims ", size1
           endif
 
+          call mpi_sizeof( ch, size1, ierr )
+          call mpi_type_size( MPI_CHARACTER, size2, ierr )
+          if (size1 .ne. size2) then
+             errs = errs + 1
+             print *, "character size is ", size2, " sizeof claims ", size1
+          endif
+
           call mpi_sizeof( d, size1, ierr )
           call mpi_type_size( MPI_REAL, size2, ierr )
           if (size1 .ne. size2) then
@@ -44,6 +52,11 @@
              print *, "real array size is ", size2, " sizeof claims ", size1
           endif
 
+          if (errs .gt. 0) then
+             print *, ' Found ', errs, ' errors'
+          else
+             print *, ' No Errors'
+          endif
           call mpi_finalize(ierr)
           
         end program main
