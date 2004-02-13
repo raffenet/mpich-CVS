f7de44a7871f5bc225ab4d4f2edd4d0a5987d5e4
gropp <gropp>
2004-02-13 20:30:06 +0000

    Begin adding code to support creating unnamed f90 types

--- src/binding/f90/create_f90_util.c
+++ src/binding/f90/create_f90_util.c
@@ -20,6 +20,9 @@ static F90Predefined f90IntTypes[MAX_F90_TYPES];
 static F90Predefined f90RealTypes[MAX_F90_TYPES];
 static F90Predefined f90ComplexTypes[MAX_F90_TYPES];
 
+static int MPIR_Create_unnamed_predefined( MPI_Datatype old, int combiner, 
+					   int r, int p, MPI_Datatype *new_ptr );
+
 /*
    The simple approach used here is to store (unordered) the precision and 
    range of each type that is created as a result of a user-call to
@@ -65,4 +68,47 @@ int MPIR_Match_f90_int( int range, int length, MPI_Datatype *newtype )
     f90IntTypes[nAllocInteger++].d = *newtype;
     
     return 0;
+}
+
+/* Build a new type */
+static int MPIR_Create_unnamed_predefined( MPI_Datatype old, int combiner, 
+					   int r, int p, MPI_Datatype *new_ptr )
+{
+    int mpi_errno;
+
+    /* Create a contiguous type from one instance of the named type */
+    mpi_errno = MPID_Type_contiguous( 1, old, new_ptr );
+
+    /* Initialize the contents data */
+    if (mpi_errno == MPI_SUCCESS) {
+	MPID_Datatype *new_dtp;
+	int vals[2];
+	int nvals=0;
+
+	switch (combiner) {
+	case MPI_COMBINER_F90_INTEGER:
+	    nvals   = 1;
+	    vals[0] = r;
+	    break;
+
+	case MPI_COMBINER_F90_REAL:
+	case MPI_COMBINER_F90_COMPLEX:
+	    nvals   = 2;
+	    vals[0] = p;
+	    vals[1] = r;
+	    break;
+	}
+
+	MPID_Datatype_get_ptr(*new_ptr, new_dtp);
+	mpi_errno = MPID_Datatype_set_contents(new_dtp,
+					       combiner,
+					       nvals,
+					       0,
+					       0,
+					       vals,
+					       NULL,
+					       NULL );
+    }
+    
+    return mpi_errno;
 }
