72fa5566bb5e8b224bfa554b77afeb46a41ad371
gropp <gropp>
2004-02-20 19:48:26 +0000

    Fix lower case letter in error handling comment

--- src/mpi/coll/reduce.c
+++ src/mpi/coll/reduce.c
@@ -141,7 +141,7 @@ PMPI_LOCAL int MPIR_Reduce (
 	mpi_errno = MPIR_Err_create_code(mpi_errno, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**fail", 0);
 	return mpi_errno;
     }
-    /* --ENd ERROR HANDLING-- */
+    /* --END ERROR HANDLING-- */
     MPID_Datatype_get_extent_macro(datatype, extent);
 
     tmp_buf = MPIU_Malloc(count*(MPIR_MAX(extent,true_extent)));
