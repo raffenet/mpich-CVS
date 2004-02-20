de2968c62e48b19f1b3b8bc8f5f3ada2f18f59bf
David <David>
2004-02-20 19:42:03 +0000

    incorrect library name

--- maint/makegcclibs.bat
+++ maint/makegcclibs.bat
@@ -6,10 +6,10 @@ Release\impgen.exe ..\lib\mpich2.dll > mpich2gcc.def
 Release\impgen.exe ..\lib\mpich2d.dll > mpich2gccd.def
 Release\impgen.exe ..\lib\mpich2fg.dll > mpich2gccf.def
 Release\impgen.exe ..\lib\mpich2fgd.dll > mpich2gccfd.def
-dlltool --dllname ..\lib\mpich2.dll --def mpich2gcc.def --output-lib ..\lib\libmpich2.a
-dlltool --dllname ..\lib\mpich2d.dll --def mpich2gccd.def --output-lib ..\lib\libmpich2d.a
-dlltool --dllname ..\lib\mpich2fg.dll --def mpich2gccf.def --output-lib ..\lib\libmpich2fg.a
-dlltool --dllname ..\lib\mpich2fgd.dll --def mpich2gccfd.def --output-lib ..\lib\libmpich2fgd.a
+dlltool --dllname mpich2.dll --def mpich2gcc.def --output-lib ..\lib\libmpich2.a
+dlltool --dllname mpich2d.dll --def mpich2gccd.def --output-lib ..\lib\libmpich2d.a
+dlltool --dllname mpich2fg.dll --def mpich2gccf.def --output-lib ..\lib\libmpich2fg.a
+dlltool --dllname mpich2fgd.dll --def mpich2gccfd.def --output-lib ..\lib\libmpich2fgd.a
 del mpich2gcc.def
 del mpich2gccd.def
 del mpich2gccf.def
