2e3976177d872f75357264d6b5fbf49899ac34b2
gropp <gropp>
2004-02-23 14:31:51 +0000

    Added Fortran info test

---
+++ test/mpi/f77/info/infotestf.f
@@ -0,0 +1,38 @@
+C Simple info test 
+       program main
+       integer i1, i2
+       integer errs, ierr
+       character*(64) value
+       logical flag
+C
+       errs = 0
+
+       call MTest_Init( ierr )
+
+       call mpi_info_create( i1, ierr )
+       call mpi_info_create( i2, ierr )
+
+       call mpi_info_set( i1, "key1", "value1", ierr )
+       call mpi_info_set( i2, "key2", "value2", ierr )
+
+       call mpi_info_get( i1, "key2", 64, value, flag, ierr )
+       if (flag) then
+          print *, "Found key2 in info1"
+          errs = errs + 1
+       endif
+
+       call MPI_Info_get( i1, "key1", 64, value, flag, ierr )
+       if (.not. flag ) then
+          print *, "Did not find key1 in info1"
+          errs = errs + 1
+       else if (value .ne. "value1") then
+          print *, "Found wrong value (", value, "), expected value1"
+          errs = errs + 1
+       endif
+
+       call mpi_info_free( i1, ierr )
+       call mpi_info_free( i2, ierr )
+
+       call MTest_Finalize( errs )
+       call MPI_Finalize( ierr )
+       end
