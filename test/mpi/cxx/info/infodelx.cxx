f8e51cdc0a396be638f5799b399899b702c0d354
gropp <gropp>
2004-03-16 20:00:50 +0000

    Added info tests for C++

---
+++ test/mpi/cxx/info/infodelx.cxx
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+#include "mpi.h"
+#include "mpitestconf.h"
+#ifdef HAVE_IOSTREAM
+// Not all C++ compilers have iostream instead of iostream.h
+#include <iostream>
+#ifdef HAVE_NAMESPACE_STD
+// Those that do often need the std namespace; otherwise, a bare "cout"
+// is likely to fail to compile
+using namespace std;
+#endif
+#else
+#include <iostream.h>
+#endif
+#include "mpitestcxx.h"
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#define NKEYS 3
+int main( int argc, char *argv[] )
+{
+    int errs = 0;
+    MPI::Info info;
+    char *keys[NKEYS] = { "file", "soft", "host" };
+    char *values[NKEYS] = { "runfile.txt", "2:1000:4,3:1000:7", 
+			    "myhost.myorg.org" };
+    char value[MPI::MAX_INFO_VAL];
+    int i, flag, nkeys;
+
+    MTest_Init( );
+
+    info = MPI::Info::Create();
+    /* Use only named keys incase the info implementation only supports
+       the predefined keys (e.g., IBM) */
+    for (i=0; i<NKEYS; i++) {
+	info.Set( keys[i], values[i] );
+    }
+
+    /* Check that all values are present */
+    for (i=0; i<NKEYS; i++) { 
+	flag = info.Get( keys[i], MPI::MAX_INFO_VAL, value );
+	if (!flag) {
+	    errs++;
+	    cout << "No value for key " <<  keys[i] << "\n";
+	}
+	if (strcmp( value, values[i] )) {
+	    errs++;
+	    cout << "Incorrect value for key " << keys[i] << 
+		", got " << value << " expected " << values[i] << "\n";
+	}
+    }
+
+    /* Now, change one value and remove another, then check again */
+    info.Delete( keys[NKEYS-1] );
+    nkeys = info.Get_nkeys();
+    if (nkeys != NKEYS - 1) {
+	errs++;
+	cout << "Deleting a key did not change the number of keys\n";
+    }
+
+    values[0] = "backfile.txt";
+    info.Set( keys[0], values[0] );
+    for (i=0; i<NKEYS-1; i++) {
+	flag = info.Get( keys[i], MPI::MAX_INFO_VAL, value );
+	if (!flag) {
+	    errs++;
+	    cout << "(after reset) No value for key " << keys[i] << "\n";
+	}
+	if (strcmp( value, values[i] )) {
+	    errs++;
+	    cout << "(after reset) Incorrect value for key " << 
+		keys[i] << ", got " << value << " expected " << values[i] <<
+		"\n";
+	}
+    }
+
+    info.Free();
+
+    MTest_Finalize( errs );
+    MPI::Finalize();
+    return 0;
+  
+}
