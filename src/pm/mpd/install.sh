cac5e6fbeaf7a7a2e78d8f7e8d20f1eddf3ef23e
rbutler <rbutler>
2004-04-13 02:34:53 +0000

    Slightly better version for solaris.

--- src/pm/mpd/install.sh
+++ src/pm/mpd/install.sh
@@ -3,6 +3,11 @@
 # example usage from Makefile.in
 # ${INSTALL} -m 4755 mpdroot ${bindir}/mpdroot ;\
 
+if [ "$1" = "-c" ]
+then
+    shift    # get rid of it
+fi
+
 if [ "$1" != "-m" ]
 then
     echo "-m option (permissions) required for this version of install.sh"
