932f0e2bffcad6e856533c849fa50c11c51f69b4
chan <chan>
2004-04-19 22:29:18 +0000

    Removed configure and acconfig.h

--- src/mpe2/src/slog2sdk/trace_rlog/acconfig.h
+++
@@ -1,8 +0,0 @@
-/* -*- Mode: C; c-basic-offset:4 ; -*-
- *
- *   Copyright (C) 2001 University of Chicago.
- *   See COPYRIGHT notice in top-level directory.
- */
-
-/*  define if <jni.h> is found  */
-#undef HAVE_JNI_H
