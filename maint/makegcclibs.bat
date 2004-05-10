ebd623370e77d90ffcf9b4e07f17ca41a4868d12
David <David>
2004-05-10 19:07:22 +0000

    changed names from mpich2f to fmpich2

--- maint/makegcclibs.bat
+++ maint/makegcclibs.bat
@@ -1,16 +1,16 @@
 del mpich2gcc.def
 del mpich2gccd.def
-del mpich2gccf.def
-del mpich2gccfd.def
+del fmpich2gcc.def
+del fmpich2gccd.def
 Release\impgen.exe ..\lib\mpich2.dll > mpich2gcc.def
 Release\impgen.exe ..\lib\mpich2d.dll > mpich2gccd.def
-Release\impgen.exe ..\lib\mpich2fg.dll > mpich2gccf.def
-Release\impgen.exe ..\lib\mpich2fgd.dll > mpich2gccfd.def
+Release\impgen.exe ..\lib\fmpich2g.dll > fmpich2gcc.def
+Release\impgen.exe ..\lib\fmpich2gd.dll > fmpich2gccd.def
 dlltool --dllname mpich2.dll --def mpich2gcc.def --output-lib ..\lib\libmpich2.a
 dlltool --dllname mpich2d.dll --def mpich2gccd.def --output-lib ..\lib\libmpich2d.a
-dlltool --dllname mpich2fg.dll --def mpich2gccf.def --output-lib ..\lib\libmpich2fg.a
-dlltool --dllname mpich2fgd.dll --def mpich2gccfd.def --output-lib ..\lib\libmpich2fgd.a
+dlltool --dllname fmpich2g.dll --def fmpich2gcc.def --output-lib ..\lib\libfmpich2g.a
+dlltool --dllname fmpich2gd.dll --def fmpich2gccd.def --output-lib ..\lib\libfmpich2gd.a
 del mpich2gcc.def
 del mpich2gccd.def
-del mpich2gccf.def
-del mpich2gccfd.def
+del fmpich2gcc.def
+del fmpich2gccd.def
