c3365b9a4ea545e357efcd1581316c3b503d2892
chan <chan>
2004-09-09 19:28:40 +0000

    Initial deposit of CLOG-2.1: Removed unwanted files.

--- src/mpe2/src/logging/include/clog_time.h
+++
@@ -1,9 +0,0 @@
-/*
-   (C) 2001 by Argonne National Laboratory.
-       See COPYRIGHT in top-level directory.
-*/
-/* where timestamps come from */
-
-void   CLOG_timeinit ( void );
-double CLOG_timestamp ( void );
-
