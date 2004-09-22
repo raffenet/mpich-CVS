3f925a736f1927ebd92b49f6fa4bc0b849a97110
ashton <ashton>
2004-09-22 07:09:32 +0000

    misc

--- src/pm/smpd/mp_parse_oldconfig.c
+++ src/pm/smpd/mp_parse_oldconfig.c
@@ -6,7 +6,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <io.h>
 #include "mpiexec.h"
 #include "smpd.h"
 
--- src/pm/smpd/smpd.h
+++ src/pm/smpd/smpd.h
@@ -11,6 +11,7 @@
 #include <windows.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <io.h>
 #else
 #include "smpdconf.h"
 #endif
