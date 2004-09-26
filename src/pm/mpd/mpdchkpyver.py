8615a611fd6df28e9dc6ba055176b523b5c4ede8
rbutler <rbutler>
2004-09-26 14:32:40 +0000

    Support for new man pg formats.

--- src/pm/mpd/mpdchkpyver.py
+++ src/pm/mpd/mpdchkpyver.py
@@ -4,19 +4,35 @@
 #       See COPYRIGHT in top-level directory.
 #
 
+"""
+This program is not to be executed from the command line.  It is 
+exec'd by mpdroot to verify the version of python before executing
+a 'regular' mpd pgm, e.g. mpdallexit.
+"""
+from time import ctime
+__author__ = "Ralph Butler and Rusty Lusk"
+__date__ = ctime()
+__version__ = "$Revision$"
+__credits__ = ""
+
 from sys     import argv, exit
 from os      import environ, execvpe
 from mpdlib  import mpd_check_python_version
 
-vinfo = mpd_check_python_version()
-if vinfo:
-    print "mpdchkpyver: your python version must be >= 2.2 ; current version is:", vinfo
-    exit(-1)
-if len(argv) > 1:
-    mpdpgm = argv[1] + '.py'
-    # print "CHKPYVER: PGM=:%s: ARGV[1:]=:%s:" % (mpdpgm,argv[1:])
-    try:
-        execvpe(mpdpgm,argv[1:],environ)    # client
-    except Exception, errinfo:
-        print 'mpdchkpyver: failed to exec %s; info=%s' % (mpdpgm,errinfo)
-        exit(-1)
+if __name__ == '__main__':
+    if len(argv) == 1  or  argv[1] == '-h'  or  argv[1] == '--help':
+        print __doc__
+	exit(-1)
+    else:
+        vinfo = mpd_check_python_version()
+        if vinfo:
+            print "mpdchkpyver: your python version must be >= 2.2 ; current version is:", vinfo
+            exit(-1)
+        if len(argv) > 1:
+            mpdpgm = argv[1] + '.py'
+            # print "CHKPYVER: PGM=:%s: ARGV[1:]=:%s:" % (mpdpgm,argv[1:])
+            try:
+                execvpe(mpdpgm,argv[1:],environ)    # client
+            except Exception, errinfo:
+                print 'mpdchkpyver: failed to exec %s; info=%s' % (mpdpgm,errinfo)
+                exit(-1)
