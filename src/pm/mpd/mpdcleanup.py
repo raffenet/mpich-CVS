8b01977c954298a840dd912ef9d50d3c7546eb70
rbutler <rbutler>
2004-09-26 14:37:23 +0000

    Support for new man pgs format.

--- src/pm/mpd/mpdcleanup.py
+++ src/pm/mpd/mpdcleanup.py
@@ -6,6 +6,19 @@
 
 ## NOTE: we do NOT allow this pgm to run via mpdroot
 
+"""
+usage: mpdcleanup', '[-f <hostsfile>] [-r <rshcmd>] [-u <user>] [-c <cleancmd>] or
+   or: mpdcleanup', '[--file=<hostsfile>] [--rsh=<rshcmd>] [-user=<user>] [-clean=<cleancmd>]
+Removes the Unix socket on local (the default) and remote machines
+This is useful in case the mpd crashed badly and did not remove it, which it normally does
+"""
+from time import ctime
+__author__ = "Ralph Butler and Rusty Lusk"
+__date__ = ctime()
+__version__ = "$Revision$"
+__credits__ = ""
+
+
 from sys    import argv, exit
 from os     import environ, system
 from getopt import getopt
@@ -19,7 +32,7 @@ def mpdcleanup():
     try:
 	(opts, args) = getopt(argv[1:], 'hf:r:u:c:', ['help', 'file=', 'rsh=', 'user=', 'clean='])
     except:
-	mpdcleanup_usage()
+	usage()
         mpd_raise('invalid arg(s) specified')
     else:
 	for opt in opts:
@@ -30,11 +43,11 @@ def mpdcleanup():
 	    elif opt[0] == '-f' or opt[0] == '--file':
 		hostsFile = opt[1]
 	    elif opt[0] == '-h' or opt[0] == '--help':
-		mpdcleanup_usage()
+		usage()
 	    elif opt[0] == '-c' or opt[0] == '--clean':
 		cleanCmd = opt[1]
     if args:
-	mpdcleanup_usage()
+	usage()
         mpd_raise('invalid arg(s) specified: ' + ' '.join(args) )
 
     cleanFile = '/tmp/mpd2.console_%s' % (user)
@@ -58,12 +71,9 @@ def mpdcleanup():
 	        # print 'cmd=:%s:' % (cmd)
 	        system(cmd)
 
-def mpdcleanup_usage():
-    print 'usage: mpdcleanup', '[-f <hostsfile>] [-r <rshcmd>] [-u <user>] [-c <cleancmd>] or'
-    print '   or: mpdcleanup', '[--file=<hostsfile>] [--rsh=<rshcmd>] [-user=<user>] [-clean=<cleancmd>]'
-    print 'Removes the Unix socket on local (the default) and remote machines'
-    print 'This is useful in case the mpd crashed badly and did not remove it, which it normally does'
-    exit(0)
+def usage():
+    print __doc__
+    exit(-1)
 
 
 if __name__ == '__main__':
