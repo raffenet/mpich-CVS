d4e4b3c4a7707bf8b4695ce1c8375cca1bafdacf
robl <robl>
2004-10-05 20:09:39 +0000

    move ADIO_GEN_Close to its own file. should make NTFS happier.

--- src/mpi/romio/adio/common/ad_close.c
+++ src/mpi/romio/adio/common/ad_close.c
@@ -88,29 +88,3 @@ void ADIO_Close(ADIO_File fd, int *error_code)
 
     /* memory for fd is freed in MPI_File_close */
 }
-
-void ADIOI_GEN_Close(ADIO_File fd, int *error_code)
-{
-    int err;
-    static char myname[] = "ADIOI_GEN_CLOSE";
-
-#ifdef PROFILE
-    MPE_Log_event(9, 0, "start close");
-#endif
-
-    err = close(fd->fd_sys);
-
-#ifdef PROFILE
-    MPE_Log_event(10, 0, "end close");
-#endif
-
-    fd->fd_sys = -1;
-
-    if (err == -1) {
-	*error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
-					   myname, __LINE__, MPI_ERR_IO,
-					   "**io",
-					   "**io %s", strerror(errno));
-    }
-    else *error_code = MPI_SUCCESS;
-}
---
+++ src/mpi/romio/adio/common/adi_close.c
@@ -0,0 +1,44 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/* 
+ *   $Id$
+ *
+ *   Copyright (C) 1997 University of Chicago. 
+ *   See COPYRIGHT notice in top-level directory.
+ */
+
+#include "adio.h"
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef PROFILE
+#include "mpe.h"
+#endif
+
+
+void ADIOI_GEN_Close(ADIO_File fd, int *error_code)
+{
+    int err;
+    static char myname[] = "ADIOI_GEN_CLOSE";
+
+#ifdef PROFILE
+    MPE_Log_event(9, 0, "start close");
+#endif
+
+    err = close(fd->fd_sys);
+
+#ifdef PROFILE
+    MPE_Log_event(10, 0, "end close");
+#endif
+
+    fd->fd_sys = -1;
+
+    if (err == -1) {
+	*error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
+					   myname, __LINE__, MPI_ERR_IO,
+					   "**io",
+					   "**io %s", strerror(errno));
+    }
+    else *error_code = MPI_SUCCESS;
+}
