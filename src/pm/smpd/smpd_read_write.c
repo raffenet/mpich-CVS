d34430e38e3901f16e55016858743cb8ddcd427a
ashton <ashton>
2004-10-25 06:50:09 +0000

    fixed hex string encoding

--- src/pm/smpd/smpd_read_write.c
+++ src/pm/smpd/smpd_read_write.c
@@ -6,18 +6,128 @@
 
 #include "smpd.h"
 
+static char hexchar(int x)
+{
+    switch (x)
+    {
+    case 0: return '0';
+    case 1: return '1';
+    case 2: return '2';
+    case 3: return '3';
+    case 4: return '4';
+    case 5: return '5';
+    case 6: return '6';
+    case 7: return '7';
+    case 8: return '8';
+    case 9: return '9';
+    case 10: return 'A';
+    case 11: return 'B';
+    case 12: return 'C';
+    case 13: return 'D';
+    case 14: return 'E';
+    case 15: return 'F';
+    }
+    return '0';
+}
+
+static char charhex(char c)
+{
+    switch (c)
+    {
+    case '0': return 0;
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    case 'A': return 10;
+    case 'B': return 11;
+    case 'C': return 12;
+    case 'D': return 13;
+    case 'E': return 14;
+    case 'F': return 15;
+    case 'a': return 10;
+    case 'b': return 11;
+    case 'c': return 12;
+    case 'd': return 13;
+    case 'e': return 14;
+    case 'f': return 15;
+    }
+    return 0;
+}
+
+static void char_to_hex(char ch, char *hex)
+{
+    *hex = hexchar((ch>>4) & 0xF);
+    hex++;
+    *hex = hexchar(ch & 0xF);
+}
+
+static char hex_to_char(const char *hex)
+{
+    unsigned char ch;
+    ch = charhex(*hex) << 4;
+    hex++;
+    ch = ch | charhex(*hex);
+    return (char)ch;
+}
+
+int smpd_encode_buffer(char *dest, int dest_length, const char *src, int src_length, int *num_encoded)
+{
+    int n = 0;
+    while (src_length && (dest_length > 2))
+    {
+	char_to_hex(*src, dest);
+	src++;
+	dest += 2;
+	src_length--;
+	dest_length -= 2;
+	n++;
+    }
+    *dest = '\0';
+    *num_encoded = n;
+    return SMPD_SUCCESS;
+}
+
+int smpd_decode_buffer(const char *str, char *dest, int length, int *num_decoded)
+{
+    int n = 0;
+
+    if (length < 1)
+    {
+	return SMPD_FAIL;
+    }
+
+    while (*str != '\0' && length)
+    {
+	*dest = hex_to_char(str);
+	str += 2;
+	dest++;
+	length--;
+	n++;
+    }
+    *num_decoded = n;
+    return SMPD_SUCCESS;
+}
+
+#if 0
 int smpd_encode_buffer(char *dest, int dest_length, const char *src, int src_length, int *num_encoded)
 {
     int num_used;
     int n = 0;
-    char ch;
+    unsigned char ch;
     while (src_length && dest_length)
     {
 	ch = *src;
-	num_used = snprintf(dest, dest_length, "%02X", (int)*src);
+	num_used = snprintf(dest, dest_length, "%02X", (unsigned int)ch);
 	if (num_used < 0)
 	{
 	    *num_encoded = n;
+	    /* FIXME: why does this return success on truncation? */
 	    return SMPD_SUCCESS;
 	}
 	/*smpd_dbg_printf(" %c = %c%c\n", ch, dest[0], dest[1]);*/
@@ -36,6 +146,7 @@ int smpd_decode_buffer(const char *str, char *dest, int length, int *num_decoded
     char hex[3];
     int value;
     int n = 0;
+    unsigned char *udest = dest;
 
     if (length < 1)
     {
@@ -50,15 +161,23 @@ int smpd_decode_buffer(const char *str, char *dest, int length, int *num_decoded
 	hex[1] = *str;
 	str++;
 	sscanf(hex, "%X", &value);
-	*dest = (char)value;
+	*udest = (unsigned char)value;
 	/*smpd_dbg_printf(" %s = %c\n", hex, *dest);*/
-	dest++;
+	udest++;
 	length--;
 	n++;
     }
+    /* FIXME: Error on truncation?
+    if (*str != '\0')
+    {
+	*num_decoded = n;
+	return SMPD_FAIL;
+    }
+    */
     *num_decoded = n;
     return SMPD_SUCCESS;
 }
+#endif
 
 int smpd_read(MPIDU_Sock_t sock, void *buf, MPIDU_Sock_size_t len)
 {
