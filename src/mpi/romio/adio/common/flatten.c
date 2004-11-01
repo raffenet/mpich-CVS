ff14b6a7594a64ec3c32ea05b5c46189229ef037
rross <rross>
2004-11-01 23:29:47 +0000

    RobL's subarray fix, slightly improved by me.

--- src/mpi/romio/adio/common/flatten.c
+++ src/mpi/romio/adio/common/flatten.c
@@ -535,8 +535,8 @@ void ADIOI_Flatten(MPI_Datatype datatype, ADIOI_Flatlist_node *flat,
 
 /* ADIOI_Count_contiguous_blocks
  *
- * Returns number of contiguous blocks in type, and also saves this value in
- * curr_index.
+ * Returns number of contiguous blocks in type, and also updates
+ * curr_index to reflect the space for the additional blocks.
  *
  * ASSUMES THAT TYPE IS NOT A BASIC!!!
  */
@@ -581,9 +581,16 @@ int ADIOI_Count_contiguous_blocks(MPI_Datatype datatype, int *curr_index)
 	MPI_Type_get_envelope(types[0], &old_nints, &old_nadds,
 			      &old_ntypes, &old_combiner);
 	ADIOI_Datatype_iscontig(types[0], &old_is_contig);
-	if ((old_combiner != MPI_COMBINER_NAMED) && (!old_is_contig))
+
+
+	prev_index = *curr_index;
+	if ((old_combiner != MPI_COMBINER_NAMED) && (!old_is_contig)) {
 	    count = ADIOI_Count_contiguous_blocks(types[0], curr_index);
-	else count = 1;
+	}
+	else {
+	    count = 1;
+	    (*curr_index)++;
+	}
 
 	/* now multiply that by the number of types in the subarray.
 	 *
@@ -594,11 +601,16 @@ int ADIOI_Count_contiguous_blocks(MPI_Datatype datatype, int *curr_index)
 	 * ints[0] - ndims
 	 * ints[ndims+1..2*ndims] - subsizes
 	 */
-	n = 1; /* going to tally up # of types in here */
+	top_count = 1; /* going to tally up # of types in here */
 	for (i=0; i < ints[0]; i++) {
-	    n *= ints[ints[0]+1+i];
+	    top_count *= ints[ints[0]+1+i];
 	}
-	count *= n;
+
+	if (top_count > 1) {
+	    num = *curr_index - prev_index;
+	    count *= top_count;
+	    *curr_index += (top_count - 1) * num;
+	}
 	break;
 #endif
 #ifdef MPIIMPL_HAVE_MPI_COMBINER_DARRAY
@@ -609,11 +621,17 @@ int ADIOI_Count_contiguous_blocks(MPI_Datatype datatype, int *curr_index)
 	    MPI_Type_get_envelope(types[0], &old_nints, &old_nadds,
 				  &old_ntypes, &old_combiner);
 	    ADIOI_Datatype_iscontig(types[0], &old_is_contig);
-	    if ((old_combiner != MPI_COMBINER_NAMED) && (!old_is_contig))
+
+	    prev_index = *curr_index;
+	    if ((old_combiner != MPI_COMBINER_NAMED) && (!old_is_contig)) {
 		count = ADIOI_Count_contiguous_blocks(types[0], curr_index);
-	    else count = 1;
+	    }
+	    else {
+		count = 1;
+		(*curr_index)++;
+	    }
 	    
-	    n = 1;
+	    top_count = 1;
 	    dims = ints[2];
 	    ranks = ADIOI_Malloc(sizeof(int) * dims);
 	    get_darray_position(ints[1],         /* rank */
@@ -628,13 +646,18 @@ int ADIOI_Count_contiguous_blocks(MPI_Datatype datatype, int *curr_index)
 				 ints[dims+3+i],    /* distrib */
 				 ints[2*dims+3+i]); /* darg */
 
-		n *= local_types_in_dim(ints[3+i],        /* gsize */
-					ranks[i],         /* dim rank */
-					ints[3*dims+3+i], /* psize */
-					k);
+		top_count *= local_types_in_dim(ints[3+i],       /* gsize */
+						ranks[i],        /* dim rank */
+						ints[3*dims+3+i],/* psize */
+						k);
 	    }
 	    ADIOI_Free(ranks);
-	    count *= n;
+
+	    if (top_count > 1) {
+		num = *curr_index - prev_index;
+		count *= top_count;
+		*curr_index += (top_count - 1) * num;
+	    }
 	}
 	break;
 #endif
