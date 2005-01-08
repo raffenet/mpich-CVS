0dba1c0da34d3c93c77e86ac3669dac768f8f0dd
ashton <ashton>
2005-01-08 02:25:14 +0000

    added error checking and adjusted the help text

--- src/pm/smpd/wmpiregister/wmpiregister.cs
+++ src/pm/smpd/wmpiregister/wmpiregister.cs
@@ -27,6 +27,7 @@ namespace wmpiregister
 		private System.Windows.Forms.Label example_label;
 		private System.Windows.Forms.TextBox result_textBox;
 		private System.Windows.Forms.Button ok_button;
+		private Label usage3_label;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -66,7 +67,7 @@ namespace wmpiregister
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(wmpiregister));
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(wmpiregister));
 			this.account_label = new System.Windows.Forms.Label();
 			this.account_textBox = new System.Windows.Forms.TextBox();
 			this.password_label = new System.Windows.Forms.Label();
@@ -79,6 +80,7 @@ namespace wmpiregister
 			this.example_label = new System.Windows.Forms.Label();
 			this.result_textBox = new System.Windows.Forms.TextBox();
 			this.ok_button = new System.Windows.Forms.Button();
+			this.usage3_label = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// account_label
@@ -93,7 +95,6 @@ namespace wmpiregister
 			this.account_textBox.Location = new System.Drawing.Point(128, 152);
 			this.account_textBox.Name = "account_textBox";
 			this.account_textBox.TabIndex = 1;
-			this.account_textBox.Text = "";
 			// 
 			// password_label
 			// 
@@ -108,11 +109,10 @@ namespace wmpiregister
 			this.password_textBox.Name = "password_textBox";
 			this.password_textBox.PasswordChar = '*';
 			this.password_textBox.TabIndex = 3;
-			this.password_textBox.Text = "";
 			// 
 			// register_button
 			// 
-			this.register_button.Location = new System.Drawing.Point(8, 216);
+			this.register_button.Location = new System.Drawing.Point(8, 253);
 			this.register_button.Name = "register_button";
 			this.register_button.Size = new System.Drawing.Size(64, 23);
 			this.register_button.TabIndex = 4;
@@ -121,7 +121,7 @@ namespace wmpiregister
 			// 
 			// remove_button
 			// 
-			this.remove_button.Location = new System.Drawing.Point(80, 216);
+			this.remove_button.Location = new System.Drawing.Point(80, 253);
 			this.remove_button.Name = "remove_button";
 			this.remove_button.Size = new System.Drawing.Size(64, 23);
 			this.remove_button.TabIndex = 5;
@@ -130,7 +130,7 @@ namespace wmpiregister
 			// 
 			// cancel_button
 			// 
-			this.cancel_button.Location = new System.Drawing.Point(216, 216);
+			this.cancel_button.Location = new System.Drawing.Point(216, 253);
 			this.cancel_button.Name = "cancel_button";
 			this.cancel_button.Size = new System.Drawing.Size(64, 23);
 			this.cancel_button.TabIndex = 7;
@@ -152,8 +152,8 @@ namespace wmpiregister
 			this.usage2_label.Name = "usage2_label";
 			this.usage2_label.Size = new System.Drawing.Size(224, 48);
 			this.usage2_label.TabIndex = 10;
-			this.usage2_label.Text = "The account provided must be a valid user account available on all nodes particip" +
-				"ating in mpich2 jobs.";
+			this.usage2_label.Text = "The account provided must be a valid user account available on all the nodes that" +
+				" will participate in mpich2 jobs.";
 			// 
 			// example_label
 			// 
@@ -165,27 +165,36 @@ namespace wmpiregister
 			// 
 			// result_textBox
 			// 
-			this.result_textBox.Location = new System.Drawing.Point(8, 248);
+			this.result_textBox.Location = new System.Drawing.Point(8, 285);
 			this.result_textBox.Multiline = true;
 			this.result_textBox.Name = "result_textBox";
 			this.result_textBox.ReadOnly = true;
 			this.result_textBox.Size = new System.Drawing.Size(272, 64);
 			this.result_textBox.TabIndex = 8;
-			this.result_textBox.Text = "";
 			// 
 			// ok_button
 			// 
-			this.ok_button.Location = new System.Drawing.Point(152, 216);
+			this.ok_button.Location = new System.Drawing.Point(152, 253);
 			this.ok_button.Name = "ok_button";
 			this.ok_button.Size = new System.Drawing.Size(56, 23);
 			this.ok_button.TabIndex = 6;
 			this.ok_button.Text = "OK";
 			this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
 			// 
+			// usage3_label
+			// 
+			this.usage3_label.Location = new System.Drawing.Point(24, 210);
+			this.usage3_label.Name = "usage3_label";
+			this.usage3_label.Size = new System.Drawing.Size(224, 37);
+			this.usage3_label.TabIndex = 12;
+			this.usage3_label.Text = "Click register to save the credentials and Remove to delete the credentials for t" +
+				"he current user.";
+			// 
 			// wmpiregister
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(288, 323);
+			this.ClientSize = new System.Drawing.Size(288, 359);
+			this.Controls.Add(this.usage3_label);
 			this.Controls.Add(this.ok_button);
 			this.Controls.Add(this.result_textBox);
 			this.Controls.Add(this.example_label);
@@ -202,6 +211,7 @@ namespace wmpiregister
 			this.Name = "wmpiregister";
 			this.Text = "MPIEXEC -register wrapper";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 		#endregion
@@ -219,48 +229,63 @@ namespace wmpiregister
 		{
 			string mpiexec = "";
 			object obj;
-			RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\MPICH2");
-			obj = key.GetValue("Path");
-			if (obj != null)
+			try
 			{
-				mpiexec = obj.ToString();
-				if (mpiexec.EndsWith(@"\"))
+				RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\MPICH2");
+				if (key != null)
 				{
-					mpiexec = mpiexec + @"bin\mpiexec.exe";
-				}
-				else
-				{
-					mpiexec = mpiexec + @"\bin\mpiexec.exe";
-				}
-				if (!File.Exists(mpiexec))
-				{
-					mpiexec = "";
-				}
-			}
-			if (mpiexec == "")
-			{
-				key = Registry.LocalMachine.OpenSubKey(@"Software\MPICH\SMPD");
-				obj = key.GetValue("binary");
-				if (obj != null)
-				{
-					mpiexec = obj.ToString().Replace("smpd.exe", "mpiexec.exe");
-					if (!File.Exists(mpiexec))
+					obj = key.GetValue("Path");
+					key.Close();
+					if (obj != null)
 					{
-						mpiexec = "";
+						mpiexec = obj.ToString();
+						if (mpiexec.EndsWith(@"\"))
+						{
+							mpiexec = mpiexec + @"bin\mpiexec.exe";
+						}
+						else
+						{
+							mpiexec = mpiexec + @"\bin\mpiexec.exe";
+						}
+						if (!File.Exists(mpiexec))
+						{
+							mpiexec = "";
+						}
 					}
 				}
+				if (mpiexec == "")
+				{
+					key = Registry.LocalMachine.OpenSubKey(@"Software\MPICH\SMPD");
+					if (key != null)
+					{
+						obj = key.GetValue("binary");
+						key.Close();
+						if (obj != null)
+						{
+							mpiexec = obj.ToString().Replace("smpd.exe", "mpiexec.exe");
+							if (!File.Exists(mpiexec))
+							{
+								mpiexec = "";
+							}
+						}
+					}
+				}
+				if (mpiexec == "")
+				{
+					mpiexec = "mpiexec.exe";
+				}
+				mpiexec = mpiexec.Trim();
+				/*
+				if (mpiexec.IndexOf(' ') != -1)
+				{
+					mpiexec = "\"" + mpiexec + "\"";
+				}
+				*/
 			}
-			if (mpiexec == "")
+			catch (Exception)
 			{
 				mpiexec = "mpiexec.exe";
 			}
-			mpiexec = mpiexec.Trim();
-			/*
-			if (mpiexec.IndexOf(' ') != -1)
-			{
-				mpiexec = "\"" + mpiexec + "\"";
-			}
-			*/
 			return mpiexec;
 		}
 
@@ -268,43 +293,57 @@ namespace wmpiregister
 		{
 			string output;
 			//string error;
-			Process process = new Process();
-			process.StartInfo.FileName = get_mpiexec();
-			process.StartInfo.Arguments = "-register";
-			process.StartInfo.RedirectStandardInput = true;
-			process.StartInfo.RedirectStandardOutput = true;
-			//process.StartInfo.RedirectStandardError = true;
-			process.StartInfo.RedirectStandardError = false;
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.CreateNoWindow = true;
-			process.Start();
-			process.StandardInput.WriteLine(account_textBox.Text);
-			process.StandardInput.WriteLine(password_textBox.Text);
-			process.StandardInput.WriteLine(password_textBox.Text);
-			//error = process.StandardError.ReadToEnd();
-			output = process.StandardOutput.ReadToEnd();
-			process.WaitForExit();
-			result_textBox.Text = output; // + "\n" + error;
+			try
+			{
+				Process process = new Process();
+				process.StartInfo.FileName = get_mpiexec();
+				process.StartInfo.Arguments = "-register";
+				process.StartInfo.RedirectStandardInput = true;
+				process.StartInfo.RedirectStandardOutput = true;
+				//process.StartInfo.RedirectStandardError = true;
+				process.StartInfo.RedirectStandardError = false;
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.CreateNoWindow = true;
+				process.Start();
+				process.StandardInput.WriteLine(account_textBox.Text);
+				process.StandardInput.WriteLine(password_textBox.Text);
+				process.StandardInput.WriteLine(password_textBox.Text);
+				//error = process.StandardError.ReadToEnd();
+				output = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+				result_textBox.Text = output; // + "\n" + error;
+			}
+			catch (Exception x)
+			{
+				result_textBox.Text = "Unable to run \"mpiexec -register\"\r\nError: " + x.Message;
+			}
 		}
 
 		private void remove_button_Click(object sender, System.EventArgs e)
 		{
 			string output;
 			//string error;
-			Process process = new Process();
-			process.StartInfo.FileName = get_mpiexec();
-			process.StartInfo.Arguments = "-remove";
-			process.StartInfo.RedirectStandardInput = false;
-			process.StartInfo.RedirectStandardOutput = true;
-			//process.StartInfo.RedirectStandardError = true;
-			process.StartInfo.RedirectStandardError = false;
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.CreateNoWindow = true;
-			process.Start();
-			//error = process.StandardError.ReadToEnd();
-			output = process.StandardOutput.ReadToEnd();
-			process.WaitForExit();
-			result_textBox.Text = output;// + "\n" + error;
+			try
+			{
+				Process process = new Process();
+				process.StartInfo.FileName = get_mpiexec();
+				process.StartInfo.Arguments = "-remove";
+				process.StartInfo.RedirectStandardInput = false;
+				process.StartInfo.RedirectStandardOutput = true;
+				//process.StartInfo.RedirectStandardError = true;
+				process.StartInfo.RedirectStandardError = false;
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.CreateNoWindow = true;
+				process.Start();
+				//error = process.StandardError.ReadToEnd();
+				output = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+				result_textBox.Text = output;// + "\n" + error;
+			}
+			catch (Exception x)
+			{
+				result_textBox.Text = "Unable to run \"mpiexec -remove\"\r\nError: " + x.Message;
+			}
 		}
 
 		private void cancel_button_Click(object sender, System.EventArgs e)
