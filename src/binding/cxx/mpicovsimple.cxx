baee2e527633ab7c0701f88b552f4f100d252cd6
gropp <gropp>
2005-01-21 16:14:12 +0000

    Improved copyright statement

--- src/binding/cxx/mpicovsimple.cxx
+++ src/binding/cxx/mpicovsimple.cxx
@@ -1,6 +1,6 @@
 //-*- Mode: C++; c-basic-offset:4 ; -*- */
 //
-//  (C) 2004 by Argonne National Laboratory.
+//  Copyright (C) 2004 by Argonne National Laboratory.
 //      See COPYRIGHT in top-level directory.
 //
 // This is a *very* simple tool for basic coverage analysis.  
