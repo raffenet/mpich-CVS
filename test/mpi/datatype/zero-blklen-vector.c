5575d7b095f01587081a73b9e3dee68dca154cc1
rross <rross>
2005-01-28 16:46:25 +0000

    Fixed missing copyright.

--- test/mpi/datatype/zero-blklen-vector.c
+++ test/mpi/datatype/zero-blklen-vector.c
@@ -1,3 +1,10 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */     
+
 #include <mpi.h>
 #include <stdio.h>
 int main(int argc, char* argv[])
