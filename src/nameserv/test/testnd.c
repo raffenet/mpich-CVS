bc8725375765797cb489fe88823fdcfc232c0587
gropp <gropp>
2005-01-31 16:12:15 +0000

    Missing copyright added

--- src/nameserv/test/testnd.c
+++ src/nameserv/test/testnd.c
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*  
+ *  (C) 2003 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 /* 
  * A simple test program for name service routines.  This does not 
  * require the MPI library
