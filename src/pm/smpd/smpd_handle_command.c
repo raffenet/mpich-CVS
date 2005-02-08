0a8deb06b0689f2c4fa90d59d8c7b500e4c25d73
ashton <ashton>
2005-02-08 22:02:03 +0000

    added extra parameter checking

--- src/pm/smpd/smpd_handle_command.c
+++ src/pm/smpd/smpd_handle_command.c
@@ -304,7 +304,7 @@ int smpd_handle_close_stdin_command(smpd_context_t *context)
     piter = smpd_process.process_list;
     while (piter)
     {
-	if (piter->rank == 0 || smpd_process.stdin_toall)
+	if ((piter->rank == 0 || smpd_process.stdin_toall) && piter->in != NULL)
 	{
 	    piter->in->state = SMPD_CLOSING;
 	    result = MPIDU_Sock_post_close(piter->in->sock);
