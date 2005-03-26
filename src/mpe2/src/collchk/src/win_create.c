1e86de1d988ad7644a32742c23bd58d475c1936f
chan <chan>
2005-03-26 02:03:20 +0000

    Fixed bug in passing MPI_Win* as MPI_Win.

--- src/mpe2/src/collchk/src/win_create.c
+++ src/mpe2/src/collchk/src/win_create.c
@@ -7,7 +7,7 @@
 int MPI_Win_create(void *base, MPI_Aint size, int disp_unit,
                    MPI_Info info, MPI_Comm comm, MPI_Win *win)
 {
-    int g2g = 1, i;
+    int g2g = 1, ierr;
     char call[25];
 
     sprintf(call, "WIN_CREATE");
@@ -20,10 +20,12 @@ int MPI_Win_create(void *base, MPI_Aint size, int disp_unit,
         CollChk_same_call(comm, call);
 
         /* make the call */
-        i = PMPI_Win_create(base, size, disp_unit, info, comm, win); 
+        ierr = PMPI_Win_create(base, size, disp_unit, info, comm, win); 
 
         /* add the comm to the window */
-        CollChk_add_win(win, comm);
+        CollChk_add_win(*win, comm);
+
+        return ierr;
     }
     else {
         /* init not called */
