5d046159fa7e9f9cf92f7ff274ec8ce9c100d703
rbutler <rbutler>
2005-03-26 05:46:59 +0000

    Better parsing of output

--- src/pm/mpd/mpdgdbdrv.py
+++ src/pm/mpd/mpdgdbdrv.py
@@ -22,6 +22,7 @@ from signal import signal, SIGUSR1, SIGINT, SIGKILL
 from errno  import EINTR
 from select import select, error
 from re     import findall, sub
+from mpdlib import mpd_set_my_id, mpd_print
 
 global appPid, gdbPID
 
@@ -47,7 +48,9 @@ def sig_handler(signum,frame):
 if __name__ == '__main__':    # so I can be imported by pydoc
     signal(SIGINT,sig_handler)
     signal(SIGUSR1,sig_handler)
+    mpd_set_my_id('mpdgdbdrv')
     
+    # print "RMB:GDBDRV: ARGS=", argv
     if len(argv) > 2:
         args = ' '.join(argv[2:])
     else:
@@ -58,40 +61,57 @@ if __name__ == '__main__':    # so I can be imported by pydoc
     gdb_sin = gdb_info.tochild
     gdb_sin_fileno = gdb_sin.fileno()
     gdb_sout_serr = gdb_info.fromchild
+    gdb_sout_serr_fileno = gdb_sout_serr.fileno()
     write(gdb_sin_fileno,'set prompt (gdb)\\n\n')
-    line = gdb_sout_serr.readline() #; print "LINE1=|%s|" % (line.rstrip()) ; stdout.flush()
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE1=|%s|" % (gdb_line.rstrip()))
     write(gdb_sin_fileno,'set confirm off\n')
-    line = gdb_sout_serr.readline() #; print "LINE2=|%s|" % (line.rstrip()) ; stdout.flush()
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE2=|%s|" % (gdb_line.rstrip()))
     write(gdb_sin_fileno,'handle SIGUSR1 nostop noprint\n')
-    line = gdb_sout_serr.readline() #; print "LINE3=|%s|" % (line.rstrip()) ; stdout.flush()
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE3=|%s|" % (gdb_line.rstrip()))
     write(gdb_sin_fileno,'handle SIGPIPE nostop noprint\n')
-    line = gdb_sout_serr.readline() #; print "LINE4=|%s|" % (line.rstrip()) ; stdout.flush()
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE4=|%s|" % (gdb_line.rstrip()))
     write(gdb_sin_fileno,'set confirm on\n')
-    line = gdb_sout_serr.readline() #; print "LINE5=|%s|" % (line.rstrip()) ; stdout.flush()
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE5=|%s|" % (gdb_line.rstrip()))
     write(gdb_sin_fileno,'echo hi1\n')
-    line = gdb_sout_serr.readline() #; print "LINE6=|%s|" % (line.rstrip()) ; stdout.flush()
-    i = 0
-    while i < 8  and  not line.startswith('hi1'):
-        line = gdb_sout_serr.readline() #; print "LINEx=|%s|" % (line.rstrip());stdout.flush()
-        i += 1
-    if i >= 8:
-        print 'failed to drain valid input from gdb'
-        exit(-1)
+    gdb_line = gdb_sout_serr.readline() 
+    mpd_print(0000, "LINE6=|%s|" % (gdb_line.rstrip()))
+    gdb_line = ''
+    while not gdb_line.startswith('hi1'):
+        gdb_line = gdb_sout_serr.readline() 
+        mpd_print(0000, "LINEx=|%s|" % (gdb_line.rstrip()))
     
     write(gdb_sin_fileno,'b main\n')
-    gdb_line = gdb_sout_serr.readline()  # drain breakpoint response
+    gdb_line = ''
+    while not gdb_line.startswith('Breakpoint'):
+        try:
+            (readyFDs,unused1,unused2) = select([gdb_sout_serr_fileno],[],[],3)
+        except error, data:
+            if data[0] == EINTR:    # interrupted by timeout for example
+                continue
+            else:
+                print 'mpdgdb_drv: main loop: select error: %s' % strerror(data[0])
+        if not readyFDs:
+            mpd_print(1, 'timed out waiting for initial Breakpoint response')
+            exit(-1)
+        gdb_line = gdb_sout_serr.readline()  # drain breakpoint response
+        mpd_print(0000, "gdb_line=|%s|" % (gdb_line.rstrip()))
     if not gdb_line.startswith('Breakpoint'):
-        print 'mpdgdbdrv: expecting "Breakpoint", got :%s:' % (gdb_line)
+        mpd_print(1, 'expecting "Breakpoint", got :%s:' % (gdb_line) )
         exit(-1)
     gdb_line = gdb_sout_serr.readline()  # drain prompt
+    mpd_print(0000, "gdb_line=|%s|" % (gdb_line.rstrip()))
     if not gdb_line.startswith('(gdb)'):
-        print 'mpdgdbdrv: expecting "(gdb)", got :%s:' % (gdb_line)
+        mpd_print(1, 'expecting "(gdb)", got :%s:' % (gdb_line) )
         exit(-1)
     
     print '(gdb)\n', ; stdout.flush()    # initial prompt to user
     
     user_fileno = stdin.fileno()
-    gdb_sout_serr_fileno = gdb_sout_serr.fileno()
     while 1:
         try:
             (readyFDs,unused1,unused2) = select([user_fileno,gdb_sout_serr_fileno],[],[],1)
@@ -99,7 +119,7 @@ if __name__ == '__main__':    # so I can be imported by pydoc
             if data[0] == EINTR:    # interrupted by timeout for example
                 continue
             else:
-                print 'select error: %s' % strerror(data[0])
+                mpd_print(1, 'mpdgdb_drv: main loop: select error: %s' % strerror(data[0]))
         # print "READY=", readyFDs ; stdout.flush()
         for readyFD in readyFDs:
             if readyFD == gdb_sout_serr_fileno:
@@ -112,15 +132,16 @@ if __name__ == '__main__':    # so I can be imported by pydoc
             elif readyFD == user_fileno:
                 user_line = stdin.readline()
                 # print "USERLINE=", user_line, ; stdout.flush()
-                if not line:
-                    print 'mpdgdbdrv: problem: expected user input but got none'
+                if not user_line:
+                    mpd_print(1, 'mpdgdbdrv: problem: expected user input but got none')
                     exit(-1)
                 if user_line.startswith('r'):
                     # we have already set breakpoint 1 in main
                     write(gdb_sin_fileno,user_line)
                     gdb_line = gdb_sout_serr.readline()  # drain starting msg
                     if not gdb_line.startswith('Starting program'):
-                        print 'mpdgdbdrv: expecting "Starting program", got :%s:' % (gdb_line)
+                        mpd_print(1, 'expecting "Starting program", got :%s:' % \
+                                  (gdb_line))
                         exit(-1)
                     while 1:    # drain to a prompt
                         gdb_line = gdb_sout_serr.readline()  # drain one line
@@ -134,11 +155,12 @@ if __name__ == '__main__':    # so I can be imported by pydoc
                         elif gdb_line.find('Thread') >= 0:
                             appPid = findall(r'Using .* image of child .* \(LWP (\d+)\).',gdb_line)
                         else:
-                            print 'mpdgdbdrv: expecting process or thread line, got :%s:' % (gdb_line)
+                            mpd_print(1, 'expecting process or thread line, got :%s:' % \
+                                      (gdb_line))
                             exit(-1)
                         appPid = int(appPid[0])
                     else:
-                        print 'mpdgdbdrv: expecting line with "Using"; got :%s:' % (gdb_line)
+                        mpd_print(1, 'expecting line with "Using"; got :%s:' % (gdb_line))
                         exit(-1)
                     while 1:    # drain to a prompt
                         gdb_line = gdb_sout_serr.readline()  # drain one line
