e625223b3aad6ac113004133eafbc28cf6edd068
thakur <thakur>
2005-05-23 18:30:16 +0000

    updated comments

--- src/mpi/romio/adio/ad_nfs/ad_nfs_wait.c
+++ src/mpi/romio/adio/ad_nfs/ad_nfs_wait.c
@@ -25,7 +25,7 @@ void ADIOI_NFS_ReadComplete(ADIO_Request *request, ADIO_Status *status,
     }
     
 #ifdef ROMIO_HAVE_AIO_SUSPEND_TWO_ARGS
-/* IBM */
+/* old IBM */
     if ((*request)->queued) {
 	do {
 	    err = aio_suspend(1, (struct aiocb **) &((*request)->handle));
@@ -108,7 +108,7 @@ void ADIOI_NFS_ReadComplete(ADIO_Request *request, ADIO_Status *status,
     }
 
 #else
-/* HP, FreeBSD, Linux */
+/* no aio */
 
 #ifdef HAVE_STATUS_SET_BYTES
     MPIR_Status_set_bytes(status, (*request)->datatype, (*request)->nbytes);
