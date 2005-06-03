7bddd1c4557dc322363d72b2032f6fe4a8a742a0
rbutler <rbutler>
2005-06-03 16:05:38 +0000

    Add check for presence of spawn pgms

--- src/pm/mpd/test/testall.sh
+++ src/pm/mpd/test/testall.sh
@@ -4,6 +4,11 @@ echo
 echo "RMB: tests between magpie and cluster hosts"
 echo 
 
+if [ ! -x $HOME/mpich2/test/mpi/spawn/spawn1 ] ; then
+    echo '***  build the spawn pgms first'
+    exit -1
+fi
+
 test1.py
 test2.py
 test3.py
