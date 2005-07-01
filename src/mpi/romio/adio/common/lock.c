3d33a654f1cc555e552f2219d86592c82d71b3e1
ashton <ashton>
2005-07-01 02:12:10 +0000

    updated the file locks for ntfs to handle asynchronous locking and produce error messages

--- src/mpi/romio/adio/common/lock.c
+++ src/mpi/romio/adio/common/lock.c
@@ -8,46 +8,79 @@
 #include "adio.h"
 
 #ifdef ROMIO_NTFS
+/* This assumes that lock will always remain in the common directory and 
+ * that the ntfs directory will always be called ad_ntfs. */
+#include "..\ad_ntfs\ad_ntfs.h"
 int ADIOI_Set_lock(FDTYPE fd, int cmd, int type, ADIO_Offset offset, int whence,
 	     ADIO_Offset len) 
 {
-    int ret_val, error_code;
-	OVERLAPPED Overlapped;
-	DWORD dwFlags;
-	
-	dwFlags = type;
+    static char myname[] = "ADIOI_Set_lock";
+    int ret_val, error_code = MPI_SUCCESS;
+    OVERLAPPED Overlapped;
+    DWORD dwFlags;
 
-	Overlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    dwFlags = type;
+
+    Overlapped.hEvent = /*0;*/CreateEvent(NULL, TRUE, FALSE, NULL);
 #ifdef HAVE_INT64
-	Overlapped.Offset = ( (DWORD) ( offset & (__int64) 0xFFFFFFFF ) );
-	Overlapped.OffsetHigh = ( (DWORD) ( (offset >> 32) & (__int64) 0xFFFFFFFF ) );
+    Overlapped.Offset = ( (DWORD) ( offset & (__int64) 0xFFFFFFFF ) );
+    Overlapped.OffsetHigh = ( (DWORD) ( (offset >> 32) & (__int64) 0xFFFFFFFF ) );
 
-	if (cmd == ADIOI_LOCK_CMD)
-		ret_val = LockFileEx(fd, dwFlags, 0, 
-			( (DWORD) ( len & (__int64) 0xFFFFFFFF ) ), 
-			( (DWORD) ( (len >> 32) & (__int64) 0xFFFFFFFF ) ), 
-			&Overlapped);
-	else
-		ret_val = UnlockFileEx(fd, 0, 
-			( (DWORD) ( len & (__int64) 0xFFFFFFFF ) ), 
-			( (DWORD) ( (len >> 32) & (__int64) 0xFFFFFFFF ) ), 
-			&Overlapped);
+    if (cmd == ADIOI_LOCK_CMD)
+    {
+	/*printf("locking %d\n", (int)fd);fflush(stdout);*/
+	ret_val = LockFileEx(fd, dwFlags, 0, 
+	( (DWORD) ( len & (__int64) 0xFFFFFFFF ) ), 
+	( (DWORD) ( (len >> 32) & (__int64) 0xFFFFFFFF ) ), 
+	&Overlapped);
+    }
+    else
+    {
+	/*printf("unlocking %d\n", (int)fd);fflush(stdout);*/
+	ret_val = UnlockFileEx(fd, 0, 
+	( (DWORD) ( len & (__int64) 0xFFFFFFFF ) ), 
+	( (DWORD) ( (len >> 32) & (__int64) 0xFFFFFFFF ) ), 
+	&Overlapped);
+    }
 #else
-	Overlapped.Offset = offset;
-	Overlapped.OffsetHigh = 0;
+    Overlapped.Offset = offset;
+    Overlapped.OffsetHigh = 0;
 
-	if (cmd == ADIOI_LOCK_CMD)
-		ret_val = LockFileEx(fd, dwFlags, 0, len, 0, &Overlapped);
-	else
-		ret_val = UnlockFileEx(fd, 0, len, 0, &Overlapped);
+    if (cmd == ADIOI_LOCK_CMD)
+    {
+	/*printf("locking %d\n", (int)fd);fflush(stdout);*/
+	ret_val = LockFileEx(fd, dwFlags, 0, len, 0, &Overlapped);
+    }
+    else
+    {
+	/*printf("unlocking %d\n", (int)fd);fflush(stdout);*/
+	ret_val = UnlockFileEx(fd, 0, len, 0, &Overlapped);
+    }
 #endif
 
-    if (!ret_val) {
+    if (!ret_val)
+    {
+	/*
 	FPRINTF(stderr, "File locking failed in ADIOI_Set_lock.\n");
 	MPI_Abort(MPI_COMM_WORLD, 1);
+	*/
+	ret_val = GetLastError();
+	if (ret_val == ERROR_IO_PENDING)
+	{
+	    DWORD dummy;
+	    ret_val = GetOverlappedResult(fd, &Overlapped, &dummy, TRUE);
+	    if (ret_val)
+	    {
+		CloseHandle(Overlapped.hEvent);
+		return MPI_SUCCESS;
+	    }
+	    ret_val = GetLastError();
+	}
+	error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, myname, __LINE__,
+	    MPI_ERR_IO, "**io", "**io %s", ADIOI_NTFS_Strerror(ret_val));
     }
+    CloseHandle(Overlapped.hEvent);
 
-    error_code = (ret_val) ? MPI_SUCCESS : MPI_ERR_UNKNOWN;
     return error_code;
 }
 #else
