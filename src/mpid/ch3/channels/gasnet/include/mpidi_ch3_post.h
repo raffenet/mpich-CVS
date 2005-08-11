a582b636447fbe185cf6d21bea0edb5c56911b18
thakur <thakur>
2005-08-11 01:33:01 +0000

    changes for the style checker

--- src/mpid/ch3/channels/gasnet/include/mpidi_ch3_post.h
+++ src/mpid/ch3/channels/gasnet/include/mpidi_ch3_post.h
@@ -21,9 +21,9 @@
 
 #define MPIDI_CH3_Request_release_ref(req, req_ref_count)	\
 {								\
-    assert(HANDLE_GET_MPI_KIND(req->handle) == MPID_REQUEST);	\
+    MPIU_Assert(HANDLE_GET_MPI_KIND(req->handle) == MPID_REQUEST);	\
     MPIU_Object_release_ref(req, req_ref_count);		\
-    assert(req->ref_count >= 0);				\
+    MPIU_Assert(req->ref_count >= 0);				\
 }
 
 /*
