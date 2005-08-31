4de1c5e96d4dbf21262a2380ff562dfd8087de7a
gropp <gropp>
2005-08-31 13:47:24 +0000

    Added check for blank padding in result

--- test/mpi/f77/info/infotestf.f
+++ test/mpi/f77/info/infotestf.f
@@ -7,8 +7,10 @@ C Simple info test
        program main
        include 'mpif.h'
        integer i1, i2
-       integer errs, ierr
-       character*(64) value
+       integer i, errs, ierr
+       integer valuelen
+       parameter (valuelen=64)
+       character*(valuelen) value
        logical flag
 C
        errs = 0
@@ -21,7 +23,7 @@ C
        call mpi_info_set( i1, "key1", "value1", ierr )
        call mpi_info_set( i2, "key2", "value2", ierr )
 
-       call mpi_info_get( i1, "key2", 64, value, flag, ierr )
+       call mpi_info_get( i1, "key2", valuelen, value, flag, ierr )
        if (flag) then
           print *, "Found key2 in info1"
           errs = errs + 1
@@ -31,9 +33,19 @@ C
        if (.not. flag ) then
           print *, "Did not find key1 in info1"
           errs = errs + 1
-       else if (value .ne. "value1") then
-          print *, "Found wrong value (", value, "), expected value1"
-          errs = errs + 1
+       else 
+          if (value .ne. "value1") then
+             print *, "Found wrong value (", value, "), expected value1"
+             errs = errs + 1
+          else
+C     check for trailing blanks             
+             do i=7,valuelen
+                if (value(i:i) .ne. " ") then
+                   print *, "Found non blank in info value"
+                   errs = errs + 1
+                endif
+             enddo
+          endif
        endif
 
        call mpi_info_free( i1, ierr )
