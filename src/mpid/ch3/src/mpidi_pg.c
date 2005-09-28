216dbf6bd4f82ade8af7c96e215d0c926ab2cea9
ashton <ashton>
2005-09-28 11:38:49 +0000

    corrected error case

--- src/mpid/ch3/src/mpidi_pg.c
+++ src/mpid/ch3/src/mpidi_pg.c
@@ -323,12 +323,24 @@ int MPIDI_PG_To_string(MPIDI_PG_t *pg_ptr, char **str_ptr)
 		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**fail", 0);
 		goto fn_exit;
 	    }
+	    mpi_errno = MPIU_Str_add_string(&cur_pos, &cur_len, key);
+	    if (mpi_errno != MPIU_STR_SUCCESS)
+	    {
+		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**fail", 0);
+		goto fn_exit;
+	    }
 	}
 	mpi_errno = MPIU_Str_add_string(&cur_pos, &cur_len, val);
 	if (mpi_errno != MPIU_STR_SUCCESS)
 	{
 	    mpi_errno = MPIDI_Allocate_more(&str, &cur_pos, &cur_len);
 	    if (mpi_errno != MPI_SUCCESS)
+	    {
+		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**fail", 0);
+		goto fn_exit;
+	    }
+	    mpi_errno = MPIU_Str_add_string(&cur_pos, &cur_len, val);
+	    if (mpi_errno != MPIU_STR_SUCCESS)
 	    {
 		mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE, FCNAME, __LINE__, MPI_ERR_OTHER, "**fail", 0);
 		goto fn_exit;
