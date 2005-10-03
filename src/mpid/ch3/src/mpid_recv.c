98133b31ff617775e4616d1baea52941fbcfa4e7
gropp <gropp>
2005-10-03 20:49:41 +0000

    Added fixmes

--- src/mpid/ch3/src/mpid_recv.c
+++ src/mpid/ch3/src/mpid_recv.c
@@ -40,6 +40,12 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype, int rank, int tag, M
     }
     /* --END ERROR HANDLING-- */
 
+    /* FIXME: in the common case, we want to simply complete the message
+       and make as few updates as possible.
+       Note in addition that this routine is used only by MPI_Recv (a
+       blocking routine; the intent of the interface (which returns 
+       a request) was to simplify the handling of the case where the
+       message was not found in the unexpected queue. */
     rreq->comm = comm;
     MPIR_Comm_add_ref(comm);
     rreq->dev.user_buf = buf;
@@ -50,6 +56,11 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype, int rank, int tag, M
     {
 	MPIDI_VC_t * vc;
 
+	/* FIXME: We do not need the vc unless we are performing
+	   a rendezvous (or synchronous eager(!!!)) send.  This code should 
+	   instead be optimized for the low-latency case of an eager 
+	   message, already received first, and then handle the more
+	   expensive case of rendezvous messages separately. */
 	MPIDI_Comm_get_vc(comm, rreq->dev.match.rank, &vc);
 
 	/* Message was found in the unexepected queue */
@@ -62,6 +73,17 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype, int rank, int tag, M
 	    /* This is an eager message. */
 	    MPIDI_DBG_PRINTF((15, FCNAME, "eager message in the request"));
 
+	    /* FIXME: There should be no synchronous eager messages.  It
+	       is not worth the extra complexity to support these separate
+	       from the rendezvous case (we did this early in MPICH-1 and
+	       took it out, we should *not* commit the same mistake in MPICH-2)
+	    */
+	    /* FIXME: This appears to be identical code to that in MPID_Isend;
+	       that is a maintenance problem (duplicate code should rarely be
+	       used; if it is necessary to duplicate code, then it is vital 
+	       that all locations of the duplicate code contain a complete
+	       list of locations where the same code appears, so that any
+	       changes may be uniformly applied */
 	    /* If this is a eager synchronous message, then we need to send an acknowledgement back to the sender. */
 	    if (MPIDI_Request_get_sync_send_flag(rreq))
 	    {
@@ -121,6 +143,11 @@ int MPID_Recv(void * buf, int count, MPI_Datatype datatype, int rank, int tag, M
 	else if (MPIDI_Request_get_msg_type(rreq) == MPIDI_REQUEST_RNDV_MSG)
 	{
 	    /* A rendezvous request-to-send (RTS) message has arrived.  We need to send a CTS message to the remote process. */
+	    /* FIXME: We should decide who is responsible for the rendezvous
+	       and require them to perform the operation.  It makes 
+	       the most sense for the channel to do this, since it may want to
+	       optimize the particular handling of the operation.  Common
+	       code can be made available to the channels separately */
 #ifdef MPIDI_CH3_CHANNEL_RNDV
 		/* The channel will be performing the rendezvous */
 
