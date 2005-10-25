46e60535ec996fd179d03b0c0f4b101ae20dd09e
chan <chan>
2005-10-25 00:28:06 +0000

    Updated clog format version to 2.4.  Added an unique range for eventIDs of CLOG_USER_SOLE_EVENT which corresponds to CLOG_Stream->user_solo_eventID.  Added comment to clarify the limitation and separations for various eventIDs and stateIDs.

--- src/mpe2/src/logging/include/clog.h
+++ src/mpe2/src/logging/include/clog.h
@@ -11,8 +11,10 @@
                 wasted disk space.
    Version 2.2: Added CLOG internal profiling state: CLOG_Buffer_write2disk
    Version 2.3: Added support of MPI_Comm.
+   Version 2.4: Added support of user-defined event drawable/category.
+                backward compatible with version 2.3.
 */
-#define CLOG_VERSION          "CLOG-02.30"
+#define CLOG_VERSION          "CLOG-02.40"
 
 #include "clog_buffer.h"
 #include "clog_sync.h"
@@ -20,21 +22,27 @@
 #include "clog_record.h"
 
 /*
-   keep this less than user event IDs;
-   it is for MPI implementation, i.e., src/wrapper/log_mpi_core.c
+   This is for MPI implementation, i.e., src/wrapper/log_mpi_core.c
+   CLOG_KNOWN_EVENTID_START < CLOG_USER_EVENTID_START
 */
 #define CLOG_KNOWN_EVENTID_START 0
 
 /*
-   keep this less than user state IDs;
-   it is for MPI implementation, i.e., src/wrapper/log_mpi_core.c
+   This is for users: CLOG_KNOWN_EVENTID_START < CLOG_USER_EVENTID_START
+   CLOG_USER_EVENTID_START = 2 * CLOG_USER_STATEID_START
+*/
+#define CLOG_USER_EVENTID_START 600
+
+/* This is for users: CLOG_USER_EVENTID_START < CLOG_USER_SOLE_EVENTID_START */
+#define CLOG_USER_SOLO_EVENTID_START 5000
+
+/*
+   This is for MPI implementation, i.e., src/wrapper/log_mpi_core.c
+   CLOG_KNOWN_STATEID_START < CLOG_USER_STATEID_START
 */
 #define CLOG_KNOWN_STATEID_START 0
 
-/* keep this larger than predefined event IDs; it is for users */
-#define CLOG_USER_EVENTID_START 600
-
-/* keep this larger than predefined state IDs; it is for users */
+/* This is for users: CLOG_KNOWN_STATEID_START < CLOG_USER_STATEID_START */
 #define CLOG_USER_STATEID_START 300
 
 #define CLOG_COMM_NULL          -1
@@ -47,6 +55,7 @@ typedef struct {
     int                known_stateID;
     int                user_eventID;
     int                user_stateID;
+    int                user_solo_eventID;
 } CLOG_Stream_t;
 
 CLOG_Stream_t *CLOG_Open( void );
@@ -57,13 +66,15 @@ void CLOG_Local_init( CLOG_Stream_t *stream, const char *local_tmpfile_name );
 
 void CLOG_Local_finalize( CLOG_Stream_t *stream );
 
+int  CLOG_Get_known_eventID( CLOG_Stream_t *stream );
+
 int  CLOG_Get_user_eventID( CLOG_Stream_t *stream );
 
-int  CLOG_Get_user_stateID( CLOG_Stream_t *stream );
+int  CLOG_Get_user_solo_eventID( CLOG_Stream_t *stream );
 
 int  CLOG_Get_known_stateID( CLOG_Stream_t *stream );
 
-int  CLOG_Get_known_eventID( CLOG_Stream_t *stream );
+int  CLOG_Get_user_stateID( CLOG_Stream_t *stream );
 
 int  CLOG_Check_known_stateID( CLOG_Stream_t *stream, int stateID );
 
