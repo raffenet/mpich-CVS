aab4019ddf2bebaada76acee9feb95cba51c6ecf
gropp <gropp>
2005-10-28 15:14:16 +0000

    Move business card code into connection code

--- src/mpid/ch3/util/sock/ch3u_connect_sock.c
+++ src/mpid/ch3/util/sock/ch3u_connect_sock.c
@@ -8,11 +8,38 @@
 #include "pmi.h"
 
 #include "mpidu_sock.h"
+
+/* FIXME: Describe what these routines do */
+
+/* Partial description: 
+   This file contains the routines that are used to create socket connections,
+   including the routines used to encode/decode the description of a connection
+   into/out of the "business card".
+
+   ToDo: change the "host description" to an "interface address" so that
+   socket connections are targeted at particularly interfaces, not
+   compute nodes, and that the address is in ready-to-use IP address format, 
+   and does not require a gethostbyname lookup.
+ */
+
+/*
+ * Manage the connection information that is exported to other processes
+ * 
+ */
+#define MPIDI_CH3I_HOST_DESCRIPTION_KEY  "description"
+#define MPIDI_CH3I_HOST_KEY              "host"
+#define MPIDI_CH3I_PORT_KEY              "port"
+
+/* FIXME: All of the listener port routines should be in one place.
+   It looks like this should be a socket utility function called by
+   ch3_progress.c in sock and ssm (and essm if that is still valid), 
+   as part of the progress init function.  Note that those progress
+   engines also set the port to 0 when shutting down the progress engine,
+   though it doesn't look like the port is closed. */
+
+int MPIDI_CH3I_listener_port = 0;
 /* Required for (socket version) upcall to Connect_to_root (see FIXME) */
 extern MPIDU_Sock_set_t MPIDI_CH3I_sock_set;
-
-/* FIXME: Move this prototype into the header file */
-int MPIDI_CH3I_Connection_alloc(MPIDI_CH3I_Connection_t **);
 
 /* Allocates a connection and the pg_id field for a connection only.
    Does not initialize any connection fields other than pg_id.
@@ -73,13 +100,11 @@ int MPIDI_CH3I_Connect_to_root_sock(const char * port_name,
     MPIU_DBG_MSG_S(CH3_CONNECT,VERBOSE,"Connect to root with portstring %s",
 		   port_name );
 
-    mpi_errno = MPIU_Str_get_string_arg(port_name, MPIDI_CH3I_HOST_DESCRIPTION_KEY, host_description, MAX_HOST_DESCRIPTION_LEN);
-    if (mpi_errno != MPIU_STR_SUCCESS) {
-	MPIU_ERR_SETANDJUMP(mpi_errno,MPI_ERR_OTHER, "**argstr_hostd");
-    }
-    mpi_errno = MPIU_Str_get_int_arg(port_name, MPIDI_CH3I_PORT_KEY, &port);
-    if (mpi_errno != MPIU_STR_SUCCESS) {
-	MPIU_ERR_SETANDJUMP(mpi_errno,MPI_ERR_OTHER, "**argstr_port");
+    mpi_errno = MPIDU_Sock_get_conninfo_from_bc( port_name, host_description,
+						 sizeof(host_description),
+						 &port );
+    if (mpi_errno) {
+	MPIU_ERR_POP(mpi_errno);
     }
     mpi_errno = MPIDI_GetTagFromPort(port_name, &port_name_tag);
     if (mpi_errno != MPIU_STR_SUCCESS) {
@@ -159,3 +184,104 @@ int MPIDI_CH3I_Connect_to_root_sock(const char * port_name,
     MPIU_CHKPMEM_REAP();
     goto fn_exit;
 }
+
+/* ------------------------------------------------------------------------- */
+/* Business card management.  These routines insert or extract connection
+   information when using sockets from the business card */
+/* ------------------------------------------------------------------------- */
+
+/* FIXME: These are small routines; we may want to bring them together 
+   into a more specific post-connection-for-sock */
+/*
+int MPIDI_CH3I_Connection_with_sock( const char *bc, 
+				     MPIDI_CH3I_Connection_t **conn )
+*/
+
+/* The host_description should be of length MAX_HOST_DESCRIPTION_LEN */
+
+#undef FUNCNAME
+#define FUNCNAME MPIDU_Sock_get_conninfo_from_bc
+#undef FCNAME
+#define FCNAME MPIDI_QUOTE(FUNCNAME)
+int MPIDU_Sock_get_conninfo_from_bc( const char *bc, 
+				     char *host_description, int maxlen,
+				     int *port )
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    mpi_errno = MPIU_Str_get_string_arg(bc, MPIDI_CH3I_HOST_DESCRIPTION_KEY, 
+				 host_description, maxlen);
+    if (mpi_errno != MPIU_STR_SUCCESS) {
+	MPIU_ERR_SETANDJUMP(mpi_errno,MPI_ERR_OTHER, "**argstr_hostd");
+    }
+    mpi_errno = MPIU_Str_get_int_arg(bc, MPIDI_CH3I_PORT_KEY, port);
+    if (mpi_errno != MPIU_STR_SUCCESS) {
+	MPIU_ERR_SETANDJUMP(mpi_errno,MPI_ERR_OTHER, "**argstr_port");
+    }
+ fn_exit:
+    return mpi_errno;
+ fn_fail:
+    goto fn_exit;
+}
+
+
+/*  MPIDI_CH3U_Get_business_card_sock - does socket specific portion of 
+ *  getting a business card
+ *  
+ *  Parameters:
+ *     bc_val_p     - business card value buffer pointer, updated to the next 
+ *                    available location or freed if published.
+ *     val_max_sz_p - ptr to maximum value buffer size reduced by the number 
+ *                    of characters written
+ *                               
+ */
+
+#undef FUNCNAME
+#define FUNCNAME MPIDI_CH3U_Get_business_card_sock
+#undef FCNAME
+#define FCNAME MPIDI_QUOTE(FUNCNAME)
+int MPIDI_CH3U_Get_business_card_sock(char **bc_val_p, int *val_max_sz_p)
+{
+    int mpi_errno;
+    int port;
+    char host_description[MAX_HOST_DESCRIPTION_LEN];
+
+    mpi_errno = MPIDU_Sock_get_host_description(host_description, MAX_HOST_DESCRIPTION_LEN);
+    /* --BEGIN ERROR HANDLING-- */
+    if (mpi_errno != MPI_SUCCESS) {
+	MPIU_ERR_SET(mpi_errno,MPI_ERR_OTHER, "**init_description");
+	return mpi_errno;
+    }
+    /* --END ERROR HANDLING-- */
+
+    port = MPIDI_CH3I_listener_port;
+    mpi_errno = MPIU_Str_add_int_arg(bc_val_p, val_max_sz_p, MPIDI_CH3I_PORT_KEY, port);
+    /* --BEGIN ERROR HANDLING-- */
+    if (mpi_errno != MPIU_STR_SUCCESS)
+    {
+	if (mpi_errno == MPIU_STR_NOMEM) {
+	    MPIU_ERR_SET(mpi_errno,MPI_ERR_OTHER, "**buscard_len");
+	}
+	else {
+	    MPIU_ERR_SET(mpi_errno,MPI_ERR_OTHER, "**buscard");
+	}
+	return mpi_errno;
+    }
+    /* --END ERROR HANDLING-- */
+    
+    mpi_errno = MPIU_Str_add_string_arg(bc_val_p, val_max_sz_p, MPIDI_CH3I_HOST_DESCRIPTION_KEY, host_description);
+    /* --BEGIN ERROR HANDLING-- */
+    if (mpi_errno != MPIU_STR_SUCCESS)
+    {
+	if (mpi_errno == MPIU_STR_NOMEM) {
+	    MPIU_ERR_SET(mpi_errno,MPI_ERR_OTHER, "**buscard_len");
+	}
+	else {
+	    MPIU_ERR_SET(mpi_errno,MPI_ERR_OTHER, "**buscard");
+	}
+	return mpi_errno;
+    }
+    /* --END ERROR HANDLING-- */
+    return MPI_SUCCESS;
+}
+
