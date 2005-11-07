171d56aea74865c3f041884f5771a5830ad9f612
thakur <thakur>
2005-11-07 22:25:47 +0000

    return error if req create fails in mpid_recv_init

--- src/mpid/ch3/src/mpid_startall.c
+++ src/mpid/ch3/src/mpid_startall.c
@@ -293,6 +293,9 @@ int MPID_Recv_init(void * buf, int count, MPI_Datatype datatype, int rank, int t
     /* --BEGIN ERROR HANDLING-- */
     if (rreq == NULL)
     {
+	/* --BEGIN ERROR HANDLING-- */
+	mpi_errno = MPIR_Err_create_code(MPI_SUCCESS, MPIR_ERR_FATAL, FCNAME, __LINE__, MPI_ERR_OTHER, "**nomem", 0);
+	/* --END ERROR HANDLING-- */
 	goto fn_exit;
     }
     /* --END ERROR HANDLING-- */
