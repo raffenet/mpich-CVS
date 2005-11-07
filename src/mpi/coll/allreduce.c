85fb75bff7657680b9b7ffae49f9e0dd00a683b6
thakur <thakur>
2005-11-07 22:54:20 +0000

    added buffer alias error check

--- src/mpi/coll/allreduce.c
+++ src/mpi/coll/allreduce.c
@@ -651,6 +651,10 @@ int MPI_Allreduce ( void *sendbuf, void *recvbuf, int count,
             }
 	    if (mpi_errno != MPI_SUCCESS) goto fn_fail;
         }
+
+	MPIR_ERRTEST_ALIAS(sendbuf, recvbuf, mpi_errno);
+	if (mpi_errno != MPI_SUCCESS) goto fn_fail;
+
         MPID_END_ERROR_CHECKS;
     }
 #   endif /* HAVE_ERROR_CHECKING */
--- src/mpi/coll/reduce.c
+++ src/mpi/coll/reduce.c
@@ -797,6 +797,10 @@ int MPI_Reduce(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype,
             }
             if (mpi_errno != MPI_SUCCESS) goto fn_fail;
         }
+
+	MPIR_ERRTEST_ALIAS(sendbuf, recvbuf, mpi_errno);
+	if (mpi_errno != MPI_SUCCESS) goto fn_fail;
+
         MPID_END_ERROR_CHECKS;
     }
 #   endif /* HAVE_ERROR_CHECKING */
