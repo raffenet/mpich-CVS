e742b8b2fd7aabf7dc64dd94ce8d754b76bb08cb
mercierg <mercierg>
2006-01-30 21:44:13 +0000

    Changed routine def : switch to generic one

--- src/mpid/ch3/channels/nemesis/nemesis/src/mpid_nem_network_poll.c
+++ src/mpid/ch3/channels/nemesis/nemesis/src/mpid_nem_network_poll.c
@@ -1,44 +1,9 @@
 #include "mpid_nem.h"
-#include "gm_module.h"
-#include "tcp_module.h"
-
-/*static int count = 0; */
+#include "mpid_nem_nets.h"
 
 void
 MPID_nem_network_poll (int in_or_out)
 {
-    switch (MPID_NEM_NET_MODULE)
-    {
-    case MPID_NEM_GM_MODULE:
-        if (in_or_out)
-            gm_module_send_poll();
-        else
-            gm_module_recv_poll();
-        break;
-    case MPID_NEM_TCP_MODULE:
-            tcp_module_poll (in_or_out);
-        break;
-    default:
-        break;
-    }
+  net_module_poll(in_or_out);
 }
-
-/* void */
-/* MPID_nem_rel_network_poll( int in_or_out ) */
-/* { */
-/*     switch (MPID_NEM_NET_MODULE) */
-/*     { */
-/*     case MPID_NEM_GM_MODULE: */
-/*         if (in_or_out) */
-/*             gm_module_send_poll(); */
-/*         else */
-/*             gm_module_recv_poll(); */
-/*         break; */
-/*     case MPID_NEM_TCP_MODULE: */
-/*             tcp_module_poll ( in_or_out ); */
-/*         break; */
-/*     default: */
-/*         break; */
-/*     } */
-/* } */
 
