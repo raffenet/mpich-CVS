ddd00f7c0f884c67acc49bd1da47288f01d29f30
ashton <ashton>
2006-02-13 18:25:27 +0000

    unreferenced arg

--- src/mpid/ch3/channels/sock/src/ch3_init.c
+++ src/mpid/ch3/channels/sock/src/ch3_init.c
@@ -73,7 +73,8 @@ int MPIDI_CH3_PortFnsInit( MPIDI_PortFns *portFns )
 /* This function simply tells the CH3 device to use the defaults for the 
    MPI-2 RMA functions */
 int MPIDI_CH3_RMAFnsInit( MPIDI_RMAFns *a ) 
-{ 
+{
+    MPIU_UNREFERENCED_ARG(a);
     return 0;
 }
 
