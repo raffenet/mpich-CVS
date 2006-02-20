578213949c53a64e6f0acf1c7172fe3da30c8f28
chan <chan>
2006-02-20 21:16:44 +0000

    added Irecv/Send/Wait in intercommunicator and the merged intracommunicator.

--- src/mpe2/src/wrappers/test/comm2_connect_accept.c
+++ src/mpe2/src/wrappers/test/comm2_connect_accept.c
@@ -12,36 +12,33 @@
 #include "mpi.h"
 
 
-void handle_error(int errcode, char *str);
-MPI_Comm server_routine(MPI_Comm comm);
-MPI_Comm client_routine(MPI_Comm comm);
-void usage(char * name);
-int parse_args(int argc, char ** argv);
 
 
-int is_server=0;
-int is_client=0;
+static int is_server = 0;
+static int is_client = 0;
 
-void handle_error(int errcode, char *str)
+void handle_error( int errcode, char *str );
+void handle_error( int errcode, char *str )
 {
-    char msg[MPI_MAX_ERROR_STRING];
+    char msg[ MPI_MAX_ERROR_STRING ];
     int  resultlen;
-    MPI_Error_string(errcode, msg, &resultlen);
-    fprintf(stderr, "%s: %s\n", str, msg);
+    MPI_Error_string( errcode, msg, &resultlen );
+    fprintf( stderr, "%s: %s\n", str, msg );
 }
 
 /*
    open a port, waiting for a connection from a client,
 */
-MPI_Comm server_routine(MPI_Comm comm)
+MPI_Comm server_init( MPI_Comm comm );
+MPI_Comm server_init( MPI_Comm comm )
 {
-    char port_name[MPI_MAX_PORT_NAME];
-    MPI_Comm newcomm;
+    char      port_name[ MPI_MAX_PORT_NAME ];
+    MPI_Comm  newcomm;
 
-    MPI_Open_port(MPI_INFO_NULL, port_name);
-    printf("server: port opened at %s\n", port_name);
-    MPI_Publish_name("mpe_port_name", MPI_INFO_NULL, port_name);
-    MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, comm, &newcomm);
+    MPI_Open_port( MPI_INFO_NULL, port_name );
+    fprintf( stdout, "server: port opened at %s\n", port_name );
+    MPI_Publish_name( "mpe_port_name", MPI_INFO_NULL, port_name );
+    MPI_Comm_accept( port_name, MPI_INFO_NULL, 0, comm, &newcomm );
 
     return newcomm;
 }
@@ -49,34 +46,37 @@ MPI_Comm server_routine(MPI_Comm comm)
 /*
    look up the available port, then connect to the server with the port name.
 */
-MPI_Comm client_routine(MPI_Comm comm)
+MPI_Comm client_init( MPI_Comm comm );
+MPI_Comm client_init( MPI_Comm comm )
 {
-    MPI_Comm newcomm;
-    int ret;
-    char port_name[MPI_MAX_PORT_NAME];
+    MPI_Comm  newcomm;
+    int       ret;
+    char      port_name[ MPI_MAX_PORT_NAME ];
 
-    ret = MPI_Lookup_name("mpe_port_name", MPI_INFO_NULL, port_name);
-    if (ret != MPI_SUCCESS) {
-        handle_error(ret, "MPI_Lookup_name");
+    ret = MPI_Lookup_name( "mpe_port_name", MPI_INFO_NULL, port_name );
+    if ( ret != MPI_SUCCESS ) {
+        handle_error( ret, "MPI_Lookup_name" );
         return 0;
     }
-    printf("client: found open port at %s\n", port_name);
-    MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, comm, &newcomm);
+    fprintf( stdout, "client: found open port at %s\n", port_name );
+    MPI_Comm_connect( port_name, MPI_INFO_NULL, 0, comm, &newcomm );
 
     return newcomm;
 }
 
-void usage(char * name)
+void usage( char * name );
+void usage( char * name )
 {
-    fprintf(stderr, "usage: %s [-s|-c]\n", name);
-    fprintf(stderr, "      specify one and only one of -s or -c\n");
-    exit (-1);
+    fprintf( stderr, "usage: %s [-s|-c]\n", name );
+    fprintf( stderr, "      specify one and only one of -s or -c\n" );
+    exit( -1 );
 }
 
-int parse_args(int argc, char ** argv)
+int parse_args( int argc, char ** argv );
+int parse_args( int argc, char ** argv )
 {
     int c;
-    while ( (c = getopt(argc, argv, "csp:")) != -1 ) {
+    while ( (c = getopt( argc, argv, "csp:" ) ) != -1 ) {
         switch (c) {
             case 's':
                 is_server = 1;
@@ -90,18 +90,21 @@ int parse_args(int argc, char ** argv)
                 usage(argv[0]);
         }
     }
-    if ( (is_client == 0 ) && (is_server == 0)) {
-        usage(argv[0]);
+    if ( (is_client == 0 ) && (is_server == 0) ) {
+        usage( argv[0] );
     }
     return 0;
 }
 
 
-int main (int argc, char ** argv)
+int main ( int argc, char ** argv )
 {
-    int rank, size;
-    MPI_Comm intercomm, intracomm;
-    int *usize, aflag;
+    MPI_Comm     intercomm, intracomm;
+    MPI_Request  request;
+    MPI_Status   status;
+    int          ibuffer;
+    int          neighbor_rank, rank, size;
+    int         *usize, aflag;
     
 
 
@@ -116,32 +119,62 @@ int main (int argc, char ** argv)
     intercomm = MPI_COMM_NULL;
 
 #if defined( SERVER )
-    intercomm = server_routine(MPI_COMM_WORLD);
+    is_server = 1;
+    intercomm = server_init( MPI_COMM_WORLD );
 #elif defined( CLIENT )
-    intercomm = client_routine(MPI_COMM_WORLD);
+    is_client = 1;
+    intercomm = client_init( MPI_COMM_WORLD );
 #else
-    parse_args(argc, argv);
-    if (is_server) {
-        intercomm = server_routine(MPI_COMM_WORLD);
+    parse_args( argc, argv );
+    if ( is_server ) {
+        intercomm = server_init( MPI_COMM_WORLD );
     }
-    else if (is_client) {
-        intercomm = client_routine(MPI_COMM_WORLD);
+    else if ( is_client ) {
+        intercomm = client_init( MPI_COMM_WORLD );
     }
 #endif
 
-    if (intercomm == MPI_COMM_NULL ) {
+    if ( intercomm == MPI_COMM_NULL ) {
+        if ( is_server ) {
+            fprintf( stderr, "Server returns NULL intercommunicator!" );
+        }
+        else if ( is_client ) {
+            fprintf( stderr, "Client returns NULL intercommunicator!" );
+        }
+        else {
+            fprintf( stderr, "Unknown server/client: NULL intercommunicator!" );
+        }
         return -1;
     }
 
+    MPI_Comm_rank( intercomm, &rank );
 
-    MPI_Intercomm_merge(intercomm, 0, &intracomm);
-    MPI_Comm_rank(intracomm, &rank);
-    MPI_Comm_size(intracomm, &size);
+    if ( rank == 0 ) {
+        MPI_Irecv( &ibuffer, 1, MPI_INT, 0,
+                   9999, intercomm, &request );
+        MPI_Send( &rank, 1, MPI_INT, 0, 9999, intercomm );
+        MPI_Wait( &request, &status );
+    }
 
-    printf("[%d/%d] after Intercomm_merge\n", rank, size);
 
-    MPI_Comm_free(&intracomm);
-    MPI_Comm_disconnect(&intercomm);
+    MPI_Intercomm_merge( intercomm, 0, &intracomm );
+    MPI_Comm_rank( intracomm, &rank );
+    MPI_Comm_size( intracomm, &size );
+
+    fprintf( stdout, "[%d/%d] after Intercomm_merge()\n", rank, size );
+
+    if ( rank == size - 1 )
+        neighbor_rank = 0;
+    else
+        neighbor_rank = rank + 1;
+
+    MPI_Irecv( &ibuffer, 1, MPI_INT, MPI_ANY_SOURCE,
+               999, intracomm, &request );
+    MPI_Send( &rank, 1, MPI_INT, neighbor_rank, 999, intracomm );
+    MPI_Wait( &request, &status );
+
+    MPI_Comm_free( &intracomm );
+    MPI_Comm_disconnect( &intercomm );
     MPI_Finalize();
     return 0;
 }
