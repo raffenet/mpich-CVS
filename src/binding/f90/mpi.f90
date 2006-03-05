66ce6fd997eb023a13e0438f87399c16bf6bcaf6
gropp <gropp>
2006-03-05 23:35:26 +0000

    gfortran requires these interface specs to go with the EXTERNALS in mpif.h ;  without them, we get unresolved symbols (the externals appear to be prepended with mpi__ from the module name)

--- src/binding/f90/mpi.f90
+++ src/binding/f90/mpi.f90
@@ -5,4 +5,37 @@
        MODULE MPI
        IMPLICIT NONE
        INCLUDE 'mpif.h'
+       INTERFACE
+        SUBROUTINE MPI_INIT(IERROR)
+        INTEGER IERROR
+        END SUBROUTINE MPI_INIT
+        
+        SUBROUTINE MPI_COMM_NULL_DELETE_FN(a,b,c,d,e)
+          INTEGER a,b,c,d,e
+        END SUBROUTINE MPI_COMM_NULL_DELETE_FN
+        SUBROUTINE MPI_COMM_DUP_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_COMM_DUP_FN
+        SUBROUTINE MPI_COMM_NULL_COPY_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_COMM_NULL_COPY_FN
+        SUBROUTINE MPI_TYPE_NULL_DELETE_FN(a,b,c,d,e)
+          INTEGER a,b,c,d,e
+        END SUBROUTINE MPI_TYPE_NULL_DELETE_FN
+        SUBROUTINE MPI_TYPE_DUP_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_TYPE_DUP_FN
+        SUBROUTINE MPI_TYPE_NULL_COPY_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_TYPE_NULL_COPY_FN
+        SUBROUTINE MPI_WIN_NULL_DELETE_FN(a,b,c,d,e)
+          INTEGER a,b,c,d,e
+        END SUBROUTINE MPI_WIN_NULL_DELETE_FN
+        SUBROUTINE MPI_WIN_DUP_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_WIN_DUP_FN
+        SUBROUTINE MPI_WIN_NULL_COPY_FN(a,b,c,d,e,f,g)
+         INTEGER a,b,c,d,e,f,g
+        END SUBROUTINE MPI_WIN_NULL_COPY_FN
+       END INTERFACE
        END MODULE MPI
