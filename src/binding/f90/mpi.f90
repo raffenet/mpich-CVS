a6b285a158aef1dfcf64ecf772dd17c8793260a3
gropp <gropp>
2006-03-08 20:10:59 +0000

    Update handling of external for gfortran F90 compiler

--- src/binding/f90/mpi.f90
+++ src/binding/f90/mpi.f90
@@ -4,7 +4,7 @@
 !
        MODULE MPI
        IMPLICIT NONE
-       INCLUDE 'mpif.h'
+       INCLUDE 'mpifnoext.h'
        INTERFACE
         SUBROUTINE MPI_INIT(IERROR)
         INTEGER IERROR
@@ -13,29 +13,44 @@
         SUBROUTINE MPI_COMM_NULL_DELETE_FN(a,b,c,d,e)
           INTEGER a,b,c,d,e
         END SUBROUTINE MPI_COMM_NULL_DELETE_FN
+
         SUBROUTINE MPI_COMM_DUP_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_COMM_DUP_FN
+
         SUBROUTINE MPI_COMM_NULL_COPY_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_COMM_NULL_COPY_FN
+
         SUBROUTINE MPI_TYPE_NULL_DELETE_FN(a,b,c,d,e)
           INTEGER a,b,c,d,e
         END SUBROUTINE MPI_TYPE_NULL_DELETE_FN
+
         SUBROUTINE MPI_TYPE_DUP_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_TYPE_DUP_FN
+
         SUBROUTINE MPI_TYPE_NULL_COPY_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_TYPE_NULL_COPY_FN
+
         SUBROUTINE MPI_WIN_NULL_DELETE_FN(a,b,c,d,e)
           INTEGER a,b,c,d,e
         END SUBROUTINE MPI_WIN_NULL_DELETE_FN
+
         SUBROUTINE MPI_WIN_DUP_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_WIN_DUP_FN
+
         SUBROUTINE MPI_WIN_NULL_COPY_FN(a,b,c,d,e,f,g)
-         INTEGER a,b,c,d,e,f,g
+         INTEGER a,b,c,d,e,g
+         LOGICAL f
         END SUBROUTINE MPI_WIN_NULL_COPY_FN
+
        END INTERFACE
        END MODULE MPI
