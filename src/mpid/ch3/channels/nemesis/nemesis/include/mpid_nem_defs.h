aabd8b53b8faac8498de7f7de640130099307089
mercierg <mercierg>
2006-06-27 09:09:37 +0000

    MX Module added

--- src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_defs.h
+++ src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_defs.h
@@ -30,7 +30,8 @@ extern char MPID_nem_hostname[MAX_HOSTNAME_LEN];
 /* Network module defines */
 #define MPID_NEM_NO_MODULE    0
 #define MPID_NEM_GM_MODULE    1
-#define MPID_NEM_TCP_MODULE   2
+#define MPID_NEM_TCP_MODULE   2 
+#define MPID_NEM_MX_MODULE    3
 
 typedef enum {MPID_NEM_POLL_IN, MPID_NEM_POLL_OUT} MPID_nem_poll_dir_t;
 
