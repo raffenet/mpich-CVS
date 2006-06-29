0513f50afb3cc88fc75cbab22afe28b438468d2b
mercierg <mercierg>
2006-06-29 11:42:59 +0000

    Changed prototypes for error checking

--- src/mpid/ch3/channels/nemesis/nemesis/include/mx_module.h
+++ src/mpid/ch3/channels/nemesis/nemesis/include/mx_module.h
@@ -13,8 +13,8 @@ int MPID_nem_mx_module_init (MPID_nem_queue_ptr_t proc_recv_queue, MPID_nem_queu
 		    MPIDI_PG_t *pg_p, int pg_rank, char **bc_val_p, int *val_max_sz_p);
 int MPID_nem_mx_module_finalize (void);
 int MPID_nem_mx_module_ckpt_shutdown (void);
-void MPID_nem_mx_module_poll(MPID_nem_poll_dir_t in_or_out);
-void MPID_nem_mx_module_send (MPIDI_VC_t *vc, MPID_nem_cell_ptr_t cell, int datalen);
+int MPID_nem_mx_module_poll(MPID_nem_poll_dir_t in_or_out);
+int MPID_nem_mx_module_send (MPIDI_VC_t *vc, MPID_nem_cell_ptr_t cell, int datalen);
 int MPID_nem_mx_module_get_business_card (char **bc_val_p, int *val_max_sz_p);
 int MPID_nem_mx_module_connect_to_root (const char *business_card, MPIDI_VC_t *new_vc);
 int MPID_nem_mx_module_vc_init (MPIDI_VC_t *vc, const char *business_card);
