b4e18d976c013aaace2285b395eac7e3d4dc64e8
mercierg <mercierg>
2006-07-03 15:27:18 +0000

    Back to normal for cell size : the MX case in handled in Mx module now

--- src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_datatypes.h
+++ src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_datatypes.h
@@ -24,23 +24,19 @@
 #include <mpidi_ch3i_nemesis_conf.h>
 
 #define MPID_NEM_OFFSETOF(struc, field) ((int)(&((struc *)0)->field))
-#define MPID_NEM_CACHE_LINE_LEN     64
-#define MPID_NEM_NUM_CELLS          32
-#if (MPID_NEM_NET_MODULE == MPID_NEM_MX_MODULE)
-#warning ">>>>>>>>>>>> Nemesis' MX module requires 32 KBytes cells !"
-#define MPID_NEM_CELL_LEN           (32*1024)
-#else
-#define MPID_NEM_CELL_LEN           (64*1024)
-#endif
+#define MPID_NEM_CACHE_LINE_LEN 64
+#define MPID_NEM_NUM_CELLS      32
+
+#define MPID_NEM_CELL_LEN      (64*1024)
 
 #define MPID_NEM_CELL_PAYLOAD_LEN   (MPID_NEM_CELL_LEN - sizeof(void *)) 
 
 #define MPID_NEM_ALIGNED(addr, bytes) ((((unsigned long)addr) & (((unsigned long)bytes)-1)) == 0)
 
-#define MPID_NEM_PKT_UNKNOWN 0
-#define MPID_NEM_PKT_CKPT 1
+#define MPID_NEM_PKT_UNKNOWN     0
+#define MPID_NEM_PKT_CKPT        1
 #define MPID_NEM_PKT_CKPT_REPLAY 2
-#define MPID_NEM_PKT_MPICH2 3
+#define MPID_NEM_PKT_MPICH2      3
 #define MPID_NEM_PKT_MPICH2_HEAD 4
 
 #define MPID_NEM_FBOX_SOURCE(cell) (MPID_nem_mem_region.local_procs[(cell)->pkt.mpich2.source])
@@ -128,8 +124,8 @@ typedef MPID_nem_abs_cell_t *MPID_nem_abs_cell_ptr_t;
 #define MPID_NEM_MIN_PACKET_LEN (sizeof (MPID_nem_pkt_header_t))
 #define MPID_NEM_MAX_PACKET_LEN (sizeof (MPID_nem_pkt_t))
 
-#define MPID_NEM_OPT_LOAD 16 
-#define MPID_NEM_OPT_SIZE ((MPID_NEM__MPICH2_HEADER_LEN) + (MPID_NEM_OPT_LOAD))
+#define MPID_NEM_OPT_LOAD     16 
+#define MPID_NEM_OPT_SIZE     ((MPID_NEM__MPICH2_HEADER_LEN) + (MPID_NEM_OPT_LOAD))
 #define MPID_NEM_OPT_HEAD_LEN ((MPID_NEM_MPICH2_HEAD_LEN) + (MPID_NEM_OPT_SIZE))
 
 #define MPID_NEM_PACKET_OPT_LEN(pkt) \
