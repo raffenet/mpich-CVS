13f627b4afda2164e843f906669c7e3878c5a530
buntinas <buntinas>
2006-07-21 21:06:43 +0000

    nemesis: added copyrights

--- src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_post.h
+++ src/mpid/ch3/channels/nemesis/nemesis/include/mpid_nem_post.h
@@ -1,3 +1,9 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2006 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
 #ifndef MPID_NEM_POST_H
 #define MPID_NEM_POST_H
 
