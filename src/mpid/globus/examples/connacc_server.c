73173b0173763a41a740ba4a330fe4be21beab24
toonen <toonen>
2006-07-27 22:39:25 +0000

    improved the connect/accept example programs to perform multiple connect/accept operations, and change which processes send and receive data on each iteration

--- src/mpid/globus/examples/connacc_server.c
+++ src/mpid/globus/examples/connacc_server.c
@@ -12,36 +12,62 @@
 #include <stdio.h>
 #include <mpi.h>
 
+const int num_iter = 2;
+
 int main(int argc, char **argv)
 {
-    int my_id;
+    int i;
+    int my_rank;
+    int local_size;
     char port_name[MPI_MAX_PORT_NAME];
     MPI_Comm newcomm;
+    int remote_rank;
+    int remote_size;
     int passed_num;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &local_size);
 
-    passed_num = 111;
-
-    if (my_id == 0)
+    if (my_rank == 0)
     {
 	MPI_Open_port(MPI_INFO_NULL, port_name);
-	printf("%s\n\n", port_name); fflush(stdout);
+	printf("\n%s\n\n", port_name); fflush(stdout);
     }
 
-    MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &newcomm); 
-
-    if (my_id == 0)
+    for (i = 0; i < num_iter; i++)
     {
-	MPI_Send(&passed_num, 1, MPI_INT, 0, 0, newcomm);
-	printf("server 0: after sending passed_num %d\n", passed_num); fflush(stdout);
-    }
+	MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &newcomm); 
+	MPI_Comm_remote_size(newcomm, &remote_size);
+	printf("server %d: remote size is %d\n", my_rank, remote_size);
 
-    MPI_Bcast(&passed_num, 1, MPI_INT, 0, newcomm);
-    printf("server %d: after receiving broadcast of passed_num %d\n", my_id, passed_num); fflush(stdout);
+	if (my_rank == (i + 4) % local_size)
+	{
+	    remote_rank = (i + 2) % remote_size;
+	    passed_num = i;
+	    MPI_Send(&passed_num, 1, MPI_INT, remote_rank, i, newcomm);
+	    printf("server %d: after sending passed_num=%d to %d\n", my_rank, passed_num, remote_rank);
+	    fflush(stdout);
+	}
+
+	remote_rank = (i + 4) % remote_size;
+	MPI_Bcast(&passed_num, 1, MPI_INT, remote_rank, newcomm);
+	printf("server %d: after receiving broadcast of passed_num=%d from %d\n", my_rank, passed_num, remote_rank);
+	fflush(stdout);
+
+	if (my_rank == (i + 1) %local_size)
+	{
+	    MPI_Status status;
+	    remote_rank = i % remote_size;
+	    MPI_Recv(&passed_num, 1, MPI_INT, remote_rank, i, newcomm, &status);
+	    printf("server %d: after receiving passed_num=%d to %d\n", my_rank, passed_num, remote_rank);
+	    fflush(stdout);
+	}
+
+	MPI_Comm_disconnect(&newcomm);
+    }
     
-    if (my_id == 0)
+    if (my_rank == 0)
     {
 	MPI_Close_port(port_name);
     }
