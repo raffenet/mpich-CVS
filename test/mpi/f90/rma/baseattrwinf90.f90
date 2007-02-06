010613df11480cbdfecfc0b092dbba859b615917
thakur <thakur>
2007-02-06 20:33:13 +0000

    fixed aint

--- test/mpi/f90/rma/baseattrwinf90.f90
+++ test/mpi/f90/rma/baseattrwinf90.f90
@@ -7,23 +7,25 @@
       program main
       use mpi
       integer (kind=MPI_ADDRESS_KIND) extrastate, valin, valout, val
-
       logical flag
       integer ierr, errs
       integer base(1024)
       integer disp
       integer win
       integer commsize
+! Include addsize defines aint as an address-sized integer
+      integer (kind=MPI_ADDRESS_KIND) aint
+
       errs = 0
+      call mtest_init( ierr )
       
-      call mpi_init( ierr )
       call mpi_comm_size( MPI_COMM_WORLD, commsize, ierr )
 
 ! Create a window; then extract the values 
-      n    = 1024
+      aint    = 1024
       disp = 4
-      call MPI_Win_create( base, n, disp, MPI_INFO_NULL, MPI_COMM_WORLD,  &
-      &  win, ierr )
+      call MPI_Win_create( base, aint, disp, MPI_INFO_NULL, &
+      &  MPI_COMM_WORLD, win, ierr )
 !
 ! In order to check the base, we need an address-of function.
 ! We use MPI_Get_address, even though that isn't strictly correct
@@ -52,10 +54,10 @@
          errs = errs + 1
          print *, "Could not get WIN_SIZE"
       else
-        if (valout .ne. n) then
+        if (valout .ne. aint) then
             errs = errs + 1
             print *, "Got incorrect value for WIN_SIZE (", valout,  &
-      &        ", should be ", n, ")"
+      &        ", should be ", aint, ")"
          endif
       endif
 
@@ -71,14 +73,9 @@
          endif
       endif
 
-      ! Check for errors
-      if (errs .eq. 0) then
-         print *, " No Errors"
-      else
-         print *, " Found ", errs, " errors"
-      endif
-
       call MPI_Win_free( win, ierr )
+
+      call mtest_finalize( errs )
 
       call MPI_Finalize( ierr )
 
