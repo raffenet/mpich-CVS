3fb5435ddcef007ade07a402b96d5079dd1a6288
buntinas <buntinas>
2007-03-09 21:44:47 +0000

    nemesis: newtcp module changes to send routines

--- src/mpid/ch3/channels/nemesis/nemesis/net_mod/newtcp_module/newtcp_module_impl.h
+++ src/mpid/ch3/channels/nemesis/nemesis/net_mod/newtcp_module/newtcp_module_impl.h
@@ -62,6 +62,11 @@ int MPID_nem_newtcp_module_is_sock_connected(int fd);
 int MPID_nem_newtcp_module_disconnect (struct MPIDI_VC *const vc);
 int MPID_nem_newtcp_module_state_listening_handler(pollfd_t *const l_plfd, sockconn_t *const l_sc);
 
+int MPID_nem_newtcp_iSendContig(MPIDI_VC_t *vc, MPID_Request *sreq, void *hdr, MPIDI_msg_sz_t hdr_sz,
+                                void *data, MPIDI_msg_sz_t data_sz);
+int MPID_nem_newtcp_iStartContigMsg(MPIDI_VC_t *vc, void *hdr, MPIDI_msg_sz_t hdr_sz, void *data, MPIDI_msg_sz_t data_sz,
+                                    MPID_Request **sreq_ptr);
+
 /* Macros */
 
 /* system call wrapper -- This retries the syscall each time it is interrupted.  
