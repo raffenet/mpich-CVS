51ee913d3a5f284ce1ef4ef2396a61e082ae0ffa
rross <rross>
2007-03-22 13:03:24 +0000

    rough, uncompiled version of dataloops in romio. no makefile yet, so no compile errors :).

---
+++ src/mpi/romio/common/dataloop/romio_dataloop.c
@@ -0,0 +1,359 @@
+#include <mpi.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "romio_dataloop.h"
+#include "typesize_support.h"
+
+typedef struct MPIO_Datatype_s {
+    int   valid, refct;
+    int   dloop_size; /* size of actual dataloop structure */
+    int   dloop_depth;
+    DLOOP_Offset size, extent; /* size and extent of type */
+    void *dloop;
+} MPIO_Datatype;
+
+static int MPIO_Datatype_keyval = MPI_KEYVAL_INVALID;
+
+static MPIO_Datatype *MPIO_Datatype_allocate(MPI_Datatype type);
+static void MPIO_Datatype_set_szext(MPI_Datatype type, MPIO_Datatype *dtp);
+
+#define MPIO_DATATYPE_VALID_PTR       1
+#define MPIO_DATATYPE_VALID_SIZE      2
+#define MPIO_DATATYPE_VALID_DEPTH     4
+#define MPIO_DATATYPE_VALID_TYPESZEXT 8
+
+int MPIO_Datatype_initialize(void);
+void MPIO_Datatype_finalize(void);
+
+void MPIO_Datatype_get_loopptr(MPI_Datatype type, void **ptr_p, int flag);
+void MPIO_Datatype_get_loopsize(MPI_Datatype type, int *size_p, int flag);
+void MPIO_Datatype_get_loopdepth(MPI_Datatype type, int *depth_p, int flag);
+void MPIO_Datatype_set_loopptr(MPI_Datatype type, void *ptr, int flag);
+void MPIO_Datatype_set_loopsize(MPI_Datatype type, int size, int flag);
+void MPIO_Datatype_set_loopdepth(MPI_Datatype type, int depth, int flag);
+void MPIO_Datatype_get_size(MPI_Datatype type, MPI_Offset *size_p);
+void MPIO_Datatype_get_extent(MPI_Datatype type, MPI_Offset *extent_p);
+
+int MPIO_Datatype_copy_attr_function(MPI_Datatype type, int type_keyval,
+				     void *extra_state, void *attribute_val_in,
+				     void *attribute_val_out, int *flag);
+int MPIO_Datatype_delete_attr_function(MPI_Datatype type, int type_keyval,
+				       void *attribute_val, void *extra_state);
+
+#if 0
+int main(int argc, char *argv[])
+{
+    int mpi_errno;
+    MPI_Datatype type, duptype;
+
+    MPI_Init(&argc, &argv);
+
+    MPIO_Datatype_initialize();
+
+    mpi_errno = MPI_Type_contiguous(2, MPI_INT, &type);
+    assert(mpi_errno == MPI_SUCCESS);
+
+    MPIO_Datatype_set_loopptr(type, (void *) 7, 0);
+
+    mpi_errno = MPI_Type_dup(type, &duptype);
+    assert(mpi_errno == MPI_SUCCESS);
+
+    mpi_errno = MPI_Type_free(&type);
+
+    mpi_errno = MPI_Type_free(&duptype);
+
+    foo_finalize();
+
+    MPI_Finalize();
+    return 0;
+}
+#endif
+
+int MPIO_Datatype_copy_attr_function(MPI_Datatype type,
+				     int type_keyval,
+				     void *extra_state,
+				     void *attribute_val_in,
+				     void *attribute_val_out,
+				     int *flag)
+{
+    MPIO_Datatype *dtp = (MPIO_Datatype *) attribute_val_in;
+
+    printf("copy fn. called\n");
+
+    assert(dtp->refct);
+
+    dtp->refct++;
+
+    * (MPIO_Datatype **) attribute_val_out = dtp;
+    *flag = 1;
+
+    printf("inc'd refct.\n");
+
+    return MPI_SUCCESS;
+}
+
+int MPIO_Datatype_delete_attr_function(MPI_Datatype type,
+				       int type_keyval,
+				       void *attribute_val,
+				       void *extra_state)
+{
+    MPIO_Datatype *dtp = (MPIO_Datatype *) attribute_val;
+
+    printf("delete fn. called\n");
+
+    assert(dtp->refct);
+
+    printf("dec'd refct\n");
+    
+    dtp->refct--;
+    if (dtp->refct == 0) {
+	free(dtp);
+	printf("freed attr structure\n");
+    }
+
+    return MPI_SUCCESS;
+}
+
+int MPIO_Datatype_initialize(void)
+{
+    int mpi_errno;
+
+    assert(MPIO_Datatype_keyval == MPI_KEYVAL_INVALID);
+
+    /* create keyval for use later */
+    mpi_errno = MPI_Type_create_keyval(MPIO_Datatype_copy_attr_function,
+				       MPIO_Datatype_delete_attr_function,
+				       &MPIO_Datatype_keyval,
+				       NULL);
+    assert(mpi_errno == MPI_SUCCESS);
+
+    printf("created keyval\n");
+
+    return 0;
+}
+
+void MPIO_Datatype_finalize(void)
+{
+    int mpi_errno;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    /* remove keyval */
+    mpi_errno = MPI_Type_free_keyval(&MPIO_Datatype_keyval);
+    assert(mpi_errno == MPI_SUCCESS);
+
+    printf("freed keyval\n");
+    
+    return;
+}
+
+void MPIO_Datatype_get_size(MPI_Datatype type, MPI_Offset *size_p)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    
+    if (!attrflag) {
+	dtp = MPIO_Datatype_allocate(type);
+    }
+
+    if (!(dtp->valid & MPIO_DATATYPE_VALID_SZEXT)) {
+	MPIO_Datatype_set_szext(type, dtp);
+    }
+
+    *size_p = dtp->size;
+    return;
+}
+
+void MPIO_Datatype_get_extent(MPI_Datatype type, MPI_Offset *extent_p)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    
+    if (!attrflag) {
+	dtp = MPIO_Datatype_allocate(type);
+    }
+
+    if (!(dtp->valid & MPIO_DATATYPE_VALID_SZEXT)) {
+	MPIO_Datatype_set_szext(type, dtp);
+    }
+
+    *extent_p = dtp->extent;
+    return;
+}
+
+void MPIO_Datatype_get_loopptr(MPI_Datatype type, void **ptr_p, int flag)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    assert(dtp->valid & MPIO_DATATYPE_VALID_PTR);
+
+    *ptr_p = dtp->dloop;
+    return;
+}
+
+void MPIO_Datatype_get_loopsize(MPI_Datatype type, int *size_p, int flag)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    assert(dtp->valid & MPIO_DATATYPE_VALID_SIZE);
+
+    *size_p = dtp->dloop_size;
+    return;
+}
+
+void MPIO_Datatype_get_loopdepth(MPI_Datatype type, int *depth_p, int flag)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    assert(dtp->valid & MPIO_DATATYPE_VALID_DEPTH);
+
+    *depth_p = dtp->dloop_depth;
+    return;
+}
+
+void MPIO_Datatype_set_loopptr(MPI_Datatype type, void *ptr, int flag)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    if (!attrflag) {
+	dtp = MPIO_Datatype_allocate(type);
+    }
+
+    printf("set loopptr = %x\n", (int) ptr);
+
+    dtp->dloop  = ptr;
+    dtp->valid |= MPIO_DATATYPE_VALID_PTR;
+    return;
+}
+
+void MPIO_Datatype_set_loopsize(MPI_Datatype type, int size, int flag)
+{
+    int mpi_errno, attrflag;
+    MPIO_Datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    if (!attrflag) {
+	dtp = MPIO_Datatype_allocate(type);
+    }
+
+    dtp->dloop_size  = size;
+    dtp->valid      |= MPIO_DATATYPE_VALID_SIZE;
+    return;
+}
+
+void MPIO_Datatype_set_loopdepth(MPI_Datatype type, int depth, int flag)
+{
+    int mpi_errno, attrflag;
+    foo_datatype *dtp;
+
+    assert(MPIO_Datatype_keyval != MPI_KEYVAL_INVALID);
+
+    mpi_errno = MPI_Type_get_attr(type, MPIO_Datatype_keyval, &dtp, &attrflag);
+    assert(mpi_errno == MPI_SUCCESS);
+    if (!attrflag) {
+	dtp = MPIO_Datatype_allocate(type);
+    }
+
+    dtp->dloop_depth  = depth;
+    dtp->valid       |= MPIO_DATATYPE_VALID_DEPTH;
+    return;
+}
+
+/* internal functions */
+
+static MPIO_Datatype *MPIO_Datatype_allocate(MPI_Datatype type)
+{
+    int mpi_errno;
+    MPIO_Datatype *dtp;
+
+    dtp = (MPIO_Datatype *) malloc(sizeof(MPIO_Datatype));
+    assert(dtp != NULL);
+    dtp->valid       = 0;
+    dtp->refct       = 1;
+    dtp->dloop       = NULL;
+    dtp->dloop_size  = -1;
+    dtp->dloop_depth = -1;
+    
+    mpi_errno = MPI_Type_set_attr(type, MPIO_Datatype_keyval, dtp);
+    assert(mpi_errno == MPI_SUCCESS);
+
+    printf("allocated attr struct\n");
+
+    return dtp;
+}
+
+/* MPIO_Datatype_set_szext()
+ *
+ * Calculates size and extent of type, fills in values in MPIO_Datatype
+ * structure, and sets valid flag.
+ *
+ * Note: This code currently checks for compatible variable sizes at
+ *       runtime, while this check could instead be performed at configure
+ *       time to save a few instructions. This seems like micro-optimization,
+ *       so I skipped it for now. -- RobR, 03/22/2007
+ */
+static void MPIO_Datatype_set_szext(MPI_Datatype type, MPIO_Datatype *dtp)
+{
+    int mpi_errno;
+
+    if (sizeof(int) == sizeof(MPI_Offset) &&
+	sizeof(MPI_Aint) == sizeof(MPI_Offset))
+    {
+	int size;
+	MPI_Aint lb, extent;
+	
+	mpi_errno = MPI_Type_size(type, &size);
+	assert(mpi_errno == MPI_SUCCESS);
+	
+	mpi_errno = MPI_Type_get_extent(type, &lb, &extent);
+	assert(mpi_errno == MPI_SUCCESS);
+	
+	dtp->size   = (MPI_Offset) size;
+	dtp->extent = (MPI_Offset) extent;
+    }
+    else {
+	MPIO_Type_footprint tfp;
+	
+	MPIO_Type_calc_footprint(type, &tfp);
+	dtp->size   = tfp.size;
+	dtp->extent = tfp.extent;
+    }
+
+    dtp->valid |= MPIO_DATATYPE_VALID_SZEXT;
+    return;
+}
---
+++ src/mpi/romio/common/dataloop/romio_dataloop.h
@@ -0,0 +1,114 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#ifndef ROMIO_DATALOOP_H
+#define ROMIO_DATALOOP_H
+
+#include <mpi.h>
+
+/* Note: this is where you define the prefix that will be prepended on
+ * all externally visible generic dataloop and segment functions.
+ */
+#define PREPEND_PREFIX(fn) MPIO_ ## fn
+
+/* accessor functions */
+void MPIO_Datatype_get_loopptr(MPI_Datatype type, void **ptr_p, int flag);
+void MPIO_Datatype_get_loopsize(MPI_Datatype type, int *size_p, int flag);
+void MPIO_Datatype_get_loopdepth(MPI_Datatype type, int *depth_p, int flag);
+void MPIO_Datatype_set_loopptr(MPI_Datatype type, void *ptr, int flag);
+void MPIO_Datatype_set_loopsize(MPI_Datatype type, int size, int flag);
+void MPIO_Datatype_set_loopdepth(MPI_Datatype type, int depth, int flag);
+
+int MPIO_Datatype_copy_attr_function(MPI_Datatype type, int type_keyval,
+				     void *extra_state, void *attribute_val_in,
+				     void *attribute_val_out, int *flag);
+int MPIO_Datatype_delete_attr_function(MPI_Datatype type, int type_keyval,
+				       void *attribute_val, void *extra_state);
+
+/* These following dataloop-specific types will be used throughout the DLOOP
+ * instance:
+ */
+#define DLOOP_Offset     MPI_Offset
+#define DLOOP_Count      int
+#define DLOOP_Handle     MPI_Datatype
+#define DLOOP_Type       MPI_Datatype
+#define DLOOP_Buffer     void *
+#define DLOOP_VECTOR     MPID_IOV
+#define DLOOP_VECTOR_LEN MPID_IOV_LEN
+#define DLOOP_VECTOR_BUF MPID_IOV_BUF
+
+/* The following accessor functions must also be defined:
+ *
+ * DLOOP_Handle_extent()
+ * DLOOP_Handle_size()
+ * DLOOP_Handle_loopptr()
+ * DLOOP_Handle_loopdepth()
+ * DLOOP_Handle_hasloop()
+ *
+ */
+
+/* USE THE NOTATION THAT BILL USED IN MPIIMPL.H AND MAKE THESE MACROS */
+
+/* NOTE: put get size into mpiimpl.h; the others go here until such time
+ * as we see that we need them elsewhere.
+ */
+#define DLOOP_Handle_get_loopdepth_macro(handle_,depth_,flag_) \
+    MPIO_Datatype_get_loopdepth(handle_,&(depth_),flag_)
+
+#define DLOOP_Handle_get_loopsize_macro(handle_,size_,flag_) \
+    MPIO_Datatype_get_loopsize(handle_,&(size_),flag_)
+
+#define DLOOP_Handle_set_loopptr_macro(handle_,lptr_,flag_) \
+    MPIO_Datatype_set_loopptr(handle_,lptr_,flag_)
+
+#define DLOOP_Handle_set_loopdepth_macro(handle_,depth_,flag_) \
+    MPIO_Datatype_set_loopdepth(handle_,depth_,flag_)
+
+#define DLOOP_Handle_set_loopsize_macro(handle_,size_,flag_) \
+    MPIO_Datatype_set_loopsize(handle_,size_,flag_)
+
+#define DLOOP_Handle_get_loopptr_macro(handle_,lptr_,flag_) \
+    MPIO_Datatype_get_loopptr(handle_,&(lptr_),flag_)
+
+#define DLOOP_Handle_get_size_macro(handle_,size_) \
+    MPIO_Datatype_get_size(handle_,size_)
+
+#define DLOOP_Handle_get_basic_type_macro(handle_,eltype_) \
+    MPID_Datatype_get_basic_type(handle_, eltype_)
+
+#define DLOOP_Handle_get_extent_macro(handle_,extent_) \
+    MPID_Datatype_get_extent_macro(handle_,extent_)
+
+#define DLOOP_Handle_hasloop_macro(handle_)                           \
+    ((HANDLE_GET_KIND(handle_) == HANDLE_KIND_BUILTIN) ? 0 : 1)
+
+/* allocate and free functions must also be defined. */
+#define DLOOP_Malloc MPIU_Malloc
+#define DLOOP_Free   MPIU_Free
+
+/* debugging output function */
+#define DLOOP_dbg_printf MPIU_dbg_printf
+
+/* assert function */
+#define DLOOP_Assert MPIU_Assert
+
+/* Include dataloop_parts.h at the end to get the rest of the prototypes
+ * and defines, in terms of the prefixes and types above.
+ */
+#include "dataloop/dataloop_parts.h"
+#include "dataloop/dataloop_create.h"
+
+/* These values are defined by DLOOP code.
+ *
+ * Note: DLOOP_DATALOOP_ALL_BYTES not currently used in MPICH2.
+ */
+#define MPID_DATALOOP_HETEROGENEOUS DLOOP_DATALOOP_HETEROGENEOUS
+#define MPID_DATALOOP_HOMOGENEOUS   DLOOP_DATALOOP_HOMOGENEOUS
+
+#include <mpiimpl.h>
+
+#endif
