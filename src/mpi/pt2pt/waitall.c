aca8cb70fa2cfc83424f76237680cbbd31203b3a
robl <robl>
2007-05-25 14:45:22 +0000

    here's a multiple-operation wait function that appears to do something good for some workloads.

--- src/mpi/pt2pt/waitall.c
+++ src/mpi/pt2pt/waitall.c
@@ -75,6 +75,8 @@ int MPI_Waitall(int count, MPI_Request array_of_requests[],
     MPID_Request * request_ptr_array[MPID_REQUEST_PTR_ARRAY_SIZE];
     MPID_Request ** request_ptrs = request_ptr_array;
     MPI_Status * status_ptr;
+    void ** state_ptrs = NULL;
+    MPIX_Grequest_wait_function *wait_fn;
     MPID_Progress_state progress_state;
     int i, j;
     int n_completed, n_native, n_greq, n_classes;
@@ -161,59 +163,48 @@ int MPI_Waitall(int count, MPI_Request array_of_requests[],
     
     MPID_Progress_start(&progress_state);
 
-    n_native=0;
-    n_greq=0;
-    for (i = 0; i< count; i++ )
+    state_ptrs = malloc(sizeof(void*)*count);
+    for(;;)
     {
-	    /* we have to avoid calling the channel poll if all we have are
-	     * generalized requests.  will block indefinitely */
-	    if (request_ptrs[i] != NULL)
-	    {
-		    if ( request_ptrs[i]->kind == MPID_UREQUEST)
-			    n_greq += 1;
-		    else 
-			    n_native += 1;
-	    }
-    }
-#if 0
-    /* examine requests for both generalized requests and generalized request
-     * classes */
-    for (i=0, j=0, n_classes=1; i< count; i++)
-    {
-        if(request_ptrs[i] != NULL)
+	for (i=0, j=0, n_classes=1, n_native=0, n_greq=0; i< count; i++)
 	{
-	    if (request_ptrs[i]->kind == MPID_UREQUEST)
+	    if(request_ptrs[i] == NULL) continue;
+            if(*request_ptrs[i]->cc_ptr != 0)
 	    {
-	        state_ptrs[j] = request_ptrs[i]->grequest_extra_state;
-		j++;
-		if (i+1 < count) {
-		    if (request_ptrs[i]->greq_class != 
-				    request_ptrs[i+1]->greq_class )
-		        n_classes+= 1;
+	        if (request_ptrs[i]->kind == MPID_UREQUEST)
+		{
+		    n_greq += 1;
+		    wait_fn = request_ptrs[i]->wait_fn;
+		    state_ptrs[j] = request_ptrs[i]->grequest_extra_state;
+		    j++;
+		    if (i+1 < count) {
+		        if (request_ptrs[i]->greq_class != 
+					request_ptrs[i+1]->greq_class)
+			    n_classes += 1;
+		    }
+		} else {
+		    n_native += 1;
 		}
 	    }
 	}
-    }
-#endif
-
-    for(;;)
-    {
-	for (i = 0; i < count; i++)
-	{
-	    if (request_ptrs[i] != NULL && 
-			    request_ptrs[i]->kind == MPID_UREQUEST &&
-			    request_ptrs[i]->wait_fn != NULL)
+	/* might be able to wait on multiple greqs */
+	/* wait on multiple greqs outside of the iterative loop */
+	/* means we need to rebuild state_ptrs each time */
+	/* maybe resolve greq class and call wait_fn that way */
+	if (j > 0 && n_classes == 1 && wait_fn != NULL) {
+	    (wait_fn)(j, state_ptrs, 0, &(array_of_statuses[i]));
+	} else {
+	    for (i = 0; i< count; i++)
 	    {
-	        if (request_ptrs[i]->wait_fn != NULL) {
-		    (request_ptrs[i]->wait_fn)(1, 
-		        &(request_ptrs[i]->grequest_extra_state), 
-			0, &(array_of_statuses[i]));
-		} else {
-		    (request_ptrs[i]->poll_fn)(request_ptrs[i]->grequest_extra_state, 
-				&(array_of_statuses[i]));
+		if (request_ptrs[i]->kind == MPID_UREQUEST && 
+				*request_ptrs[i]->cc_ptr != 0) {
+		    (request_ptrs[i]->poll_fn)(request_ptrs[i]->grequest_extra_state, &(array_of_statuses[i]));
 		}
 	    }
+	}
 
+	for (i = 0; i < count; i++)
+	{
 	    if (request_ptrs[i] != NULL && *request_ptrs[i]->cc_ptr == 0)
 	    {
 		status_ptr = (array_of_statuses != MPI_STATUSES_IGNORE) ? &array_of_statuses[i] : MPI_STATUS_IGNORE;
@@ -282,6 +273,8 @@ int MPI_Waitall(int count, MPI_Request array_of_requests[],
     { 
 	MPIU_CHKLMEM_FREEALL();
     }
+
+    if (state_ptrs != NULL) free (state_ptrs);
 
     MPID_MPI_PT2PT_FUNC_EXIT(MPID_STATE_MPI_WAITALL);
     MPIU_THREAD_SINGLE_CS_EXIT("pt2pt");
