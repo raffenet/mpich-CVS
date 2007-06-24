a3b7c86766fa761eb01f62ad488a980d524a2556
balaji <balaji>
2007-06-24 17:34:20 +0000

    Added a test to reproduce the performance problem with performing gather with a non-zero root.

---
+++ test/mpi/perf/non_zero_root.c
@@ -0,0 +1,59 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define SIZE 100000
+#define ITER 100
+
+int main(int argc, char* argv[])
+{
+	char *sbuf, *rbuf;
+	int i, j;
+	double t1, t2, t, ts;
+	int rank, size;
+	MPI_Status status;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (getenv("MPITEST_VERBOSE")) verbose = 1;
+
+	/* Allocate memory regions to communicate */
+	sbuf = (char*) malloc(SIZE);
+	rbuf = (char*) malloc(size * SIZE);
+
+	/* Touch the buffers to make sure they are allocated */
+	for (i = 0; i < SIZE; i++) sbuf[i] = '0';
+	for (i = 0; i < SIZE * size; i++) rbuf[i] = '0';
+
+	/* Time when rank 0 gathers the data */
+	MPI_Barrier(MPI_COMM_WORLD);
+	t1 = MPI_Wtime();
+	for (i = 0; i < ITER; i++)
+		MPI_Gather(sbuf, SIZE, MPI_BYTE, rbuf, SIZE, MPI_BYTE, 0, MPI_COMM_WORLD);
+	t2 = MPI_Wtime();
+	t = (t2-t1)/ITER;
+
+	/* Time when rank 1 gathers the data */
+	MPI_Barrier(MPI_COMM_WORLD);
+	t1 = MPI_Wtime();
+	for (j = 0; j < ITER; j++)
+		MPI_Gather(sbuf, SIZE, MPI_BYTE, rbuf, SIZE, MPI_BYTE, 1, MPI_COMM_WORLD);
+	t2 = MPI_Wtime();
+	ts = (t2-t1)/ITER;
+
+	/* Print out the results */
+	if (!rank) {
+		if ((t / ts) > 1.1) { /* If the difference is more than 10%, it's an error */
+			printf("%.3f\t%.3f\n", 1000000.0 * t, 1000000.0 * ts);
+			printf("Too much difference in performance\n");
+		}
+		else printf(" No Errors\n");
+	}
+	
+	MPI_Finalize();
+
+	return 0;
+}
