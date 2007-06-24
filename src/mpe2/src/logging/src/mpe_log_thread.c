3ffcaf238b9945a1a236e0b9c93ccd4843506fd1
chan <chan>
2007-06-24 04:02:53 +0000

    Added a thread-specific MPE logging on/off switch and its related support. This should enable thread logging with --enable-safePMPI in MPI logging.

--- src/mpe2/src/logging/src/mpe_log_thread.c
+++ src/mpe2/src/logging/src/mpe_log_thread.c
@@ -20,15 +20,15 @@
 
 /* MPE coarse-grained lock support mechanism */
 pthread_mutex_t  MPE_Thread_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_key_t    MPE_ThreadID_key;
+pthread_key_t    MPE_ThreadStm_key;
 MPE_ThreadID_t   MPE_Thread_count = 0;
 
-void MPE_ThreadID_free( void *thdID_ptr );
-void MPE_ThreadID_free( void *thdID_ptr )
+void MPE_ThreadStm_free( void *thdstm );
+void MPE_ThreadStm_free( void *thdstm )
 {
-    if ( thdID_ptr != NULL ) {
-        free( thdID_ptr );
-        thdID_ptr = NULL;
+    if ( thdstm != NULL ) {
+        free( thdstm );
+        thdstm = NULL;
     }
 }
 
@@ -37,7 +37,7 @@ void MPE_Log_thread_init( void )
 {
     int   thd_fn_rc;
     MPE_Thread_count = 0;
-    thd_fn_rc = pthread_key_create( &MPE_ThreadID_key, MPE_ThreadID_free );
+    thd_fn_rc = pthread_key_create( &MPE_ThreadStm_key, MPE_ThreadStm_free );
     if ( thd_fn_rc != 0 ) {
         perror( "pthread_key_create() fails!" );
         pthread_exit( NULL );
@@ -46,8 +46,8 @@ void MPE_Log_thread_init( void )
 
 #else
 
-void MPE_ThreadID_free( void *thdID_ptr );
-void MPE_ThreadID_free( void *thdID_ptr )
+void MPE_ThreadStm_free( void *thdstm );
+void MPE_ThreadStm_free( void *thdstm )
 {}
 
 void MPE_Log_thread_init( void );
