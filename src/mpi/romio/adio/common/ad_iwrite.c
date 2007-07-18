04b81315e77c1160571c09b767fbc8e20f1ce983
robl <robl>
2007-07-18 16:26:57 +0000

    whoops. debugging slipped in on last commit.

--- src/mpi/romio/adio/common/ad_iwrite.c
+++ src/mpi/romio/adio/common/ad_iwrite.c
@@ -201,7 +201,7 @@ int ADIOI_GEN_aio_poll_fn(void *extra_state, MPI_Status *status)
 	    int n = aio_return(aio_req->aiocbp);
 	    aio_req->nbytes = n;
 	    MPIR_Nest_incr();
-	    errcode = MPI_Grequest_complete(aio_req->req);
+	    errcode = MPI_Grequest_complete(*(aio_req->req));
 	    /* --BEGIN ERROR HANDLING-- */
 	    if (errcode != MPI_SUCCESS) {
 		    errcode = MPIO_Err_create_code(MPI_SUCCESS,
@@ -249,7 +249,7 @@ int ADIOI_GEN_aio_wait_fn(int count, void ** array_of_states,
 		    	int n = aio_return(aio_reqlist[i]->aiocbp);
 			aio_reqlist[i]->nbytes = n;
 			MPIR_Nest_incr();
-			errcode = MPI_Grequest_complete(aio_reqlist[i]->req);
+			errcode = MPI_Grequest_complete(*(aio_reqlist[i]->req));
 			if (errcode != MPI_SUCCESS) {
 				errcode = MPIO_Err_create_code(MPI_SUCCESS,
 						MPIR_ERR_RECOVERABLE,
