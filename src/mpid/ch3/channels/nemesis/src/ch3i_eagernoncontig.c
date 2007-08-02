9878414eff23aaaccad6479cb05b1aad439e46fd
balaji <balaji>
2007-08-02 17:48:06 +0000

    Some more compilation warnings.

--- src/mpid/ch3/channels/nemesis/src/ch3i_eagernoncontig.c
+++ src/mpid/ch3/channels/nemesis/src/ch3i_eagernoncontig.c
@@ -75,7 +75,7 @@ int MPIDI_CH3I_SendEagerNoncontig( MPIDI_VC_t *vc, MPID_Request *sreq, void *hea
     {
         MPIU_Assert(MPIDI_Request_get_type(sreq) != MPIDI_REQUEST_TYPE_GET_RESP);
         MPIDI_CH3U_Request_complete(sreq);
-        MPIU_DBG_MSG_D(CH3_CHANNEL, VERBOSE, ".... complete %d bytes", sreq->dev.segment_size);
+        MPIU_DBG_MSG_D(CH3_CHANNEL, VERBOSE, ".... complete %d bytes", (int) (sreq->dev.segment_size));
     }
     else
     {
@@ -84,7 +84,7 @@ int MPIDI_CH3I_SendEagerNoncontig( MPIDI_VC_t *vc, MPID_Request *sreq, void *hea
         if (mpi_errno) MPIU_ERR_POP(mpi_errno);
         MPIU_Assert(complete); /* all data has been sent, we should always complete */
         
-        MPIU_DBG_MSG_D(CH3_CHANNEL, VERBOSE, ".... complete %d bytes", sreq->dev.segment_size);
+        MPIU_DBG_MSG_D(CH3_CHANNEL, VERBOSE, ".... complete %d bytes", (int) (sreq->dev.segment_size));
     }
     
  fn_exit:
