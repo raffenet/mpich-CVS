32b65f5852be7be0181147e344ac1768c330e971
jayesh <jayesh>
2007-08-13 16:32:28 +0000

    1) Spawned processes inherit working directory from spawner if the spawner does not specify it explicitly in MPI_Info

--- src/pm/smpd/smpd_launch_process.c
+++ src/pm/smpd/smpd_launch_process.c
@@ -1802,14 +1802,6 @@ int smpd_launch_process(smpd_process_t *process, int priorityClass, int priority
     {
 	actual_exe = process->exe;
     }
-    /* Set the working directory of executable if not already set */
-    if(process->dir[0] == '\0')
-    {
-        if(!getcwd(process->dir, SMPD_MAX_EXE_LENGTH))
-        {
-            process->dir[0]='\0';
-	}
-    }
     
     /* create argv from the command */
     i = 0;
